51726177f57f15492686ed99088e8f2880b82172
maksim-shmat <maksim.shmat@gmail.com>
2020-12-05 10:55:32 +0300

    Add python code for rectangle pronlem.

---
+++ xclass_rectangle.py
@@ -0,0 +1,12 @@
+class Rectangle:
+    def __init__(self):
+        self.height = 1
+        self.width = 2
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.height = height
+        self.wigth = wifth
+    
+    def area(self):
+        return self.height * self.wigth
