425ec28817f7142fdc9ae0f9a667d2cd08c9e206
maksim-shmat <maksim.shmat@gmail.com>
2020-12-11 14:39:10 +0300

    Add three verion reload data module.

---
+++ python/reload_all.py
@@ -0,0 +1,42 @@
+"""
+reloadall.py: transitions reload inserted modules. Call reload_all with one and
+more objects imported modules.
+"""
+
+import types
+from imp import reload
+
+def status(module):
+    print('reloading' + module.__name__)
+
+def tryreload(module):
+    try:
+        reload(module)
+    except:
+        print('FAILED: %s' % module)
+        
+def transitive_reload(module, visited):
+    if not module in visited:
+        status(module)
+        tryreload(module)
+        visited[module]
+        for attrobj in module.__dict__.values():
+            if type(attrobj) == types.ModuleType:
+
+                transitive_reload(attrobj, visited)
+
+def reload_all(*args):
+    visited = {}
+    for arg in args:
+        if type(arg) == types.ModuleType:
+            transitive_reload(arg, visited)
+
+def tester(reloader, modname):
+    import importlib, sys
+    if len(sys.argv) > 1: modname = sys.argv[1]
+
+    module = importlib.import_module(modname)
+
+    reloader(module)
+if __name__ == '__main__':
+    tester(reload_all, 'reloadall')
