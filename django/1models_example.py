10ee75dcc81389c650486a624264be463debb58f
maksim-shmat <maksim.shmat@gmail.com>
2020-12-12 20:41:56 +0300

    Testing tests

--- django/1models_example.py
+++ django/1models_example.py
@@ -49,4 +49,10 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
+##########
+# after tests he talk change it
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+##########
 
