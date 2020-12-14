d0ad024065bbe47627f2e9902cd61ab09bae9b6f
maksim-shmat <maksim.shmat@gmail.com>
2020-12-14 14:05:28 +0300

    Sphinx is not easy, their documentation is garbage. But it is for docs.

--- django/sphinx.py
+++ django/sphinx.py
@@ -20,4 +20,22 @@ $ docker run -it --rm -v /path/to/document:/docx sphinxdoc/sphinx sphinx-quickst
 $ docker run --rm -v /path/to/document:/docs sphinxdoc/sphinx make html
 (make html)
 
+This is Head
+============
+
+This is Second Head
+-------------------
+
+This is list.
+* One element
+* Two element
+
+This is autonumeric list
+#. Element 1
+#. Element 2
+
+*cursive text*
+**Fat text**
+``code``
+
 
