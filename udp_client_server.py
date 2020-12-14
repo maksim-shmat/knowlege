0ce0ae9377b8c280846c7b2c2da79cc688bdc900
maksim-shmat <maksim.shmat@gmail.com>
2020-12-14 20:42:29 +0300

    May work this server but not a ansver.

---
+++ udp_client_server.py
@@ -0,0 +1,26 @@
+""" Simple udp echo client-server."""
+
+import sys
+import socket
+
+def server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(("127.0.0.1", 5500))
+    print("listenning at {}".format(s.getsockname()))
+    while True:
+        data,address = s.recovfrom(1024)
+        print("Client at {} says {}".format(address,data))
+        s.sendto(data,address)
+
+def client():
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    msg = input("enter your message :").encode("ascii")
+    s.sendto(msg,("127.0.0.1", 5500))
+    data,address = s.recvfrom(1024)
+    print("Server Says : {}".format(data))
+
+if __name__ == "__main__":
+    if '-s' in sys.argv:
+        server()
+    else:
+        client()
