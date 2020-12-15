ab601f39d53ecd5d18742e20d6adf8c67dfe8567
maksim-shmat <maksim.shmat@gmail.com>
2020-12-15 17:08:14 +0300

    Ok I'm make artificient inteligence withot if, great work.

---
+++ python/game2.py
@@ -0,0 +1,18 @@
+""" Game how is ask a question and a joke to answer."""
+
+print('What is be, that you defecate on the table?')
+input()
+print('The great deal.')
+print()
+print('What bitches ask a \'black whole\'?')
+input()
+print('Kariosus!')
+print()
+print('Knock-knock.')
+input()
+print("Where is that?")
+input()
+print('Dumb bitch.')
+input()
+print('Dumb bitch?', end='')
+print('-MOOOO!')
