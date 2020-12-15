f3c600d6774309edb259c2777bde564afc98ff3f
maksim-shmat <maksim.shmat@gmail.com>
2020-12-15 20:46:52 +0300

    Little bit eng phrases.

---
+++ notes/eng.py
@@ -0,0 +1,30 @@
+""" Eng phrases."""
+
+What I want to speak about is 
+То, о чём я хочу поговорить сейчас, - это...
+
+Let me inform you about
+Позвольте сообщить вам
+
+My speech will be about
+Речь пойдёт о
+
+As far as I remember/know
+Насколько я помню/знаю
+
+If I'm not mistaken
+Если я не ошибаюсь
+
+It's needless to say
+Нет нужды говорить
+
+It's common knowledge that
+Это общеизвесетно, что
+
+There is no doubt that
+Нет сомнений в том, что
+
+Let's discuss pro and contra
+Давайте обсудим за и против
+
+
