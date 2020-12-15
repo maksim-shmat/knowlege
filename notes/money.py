b72b99b458d0ebf3d49886611be1ce90b746bc18
maksim-shmat <maksim.shmat@gmail.com>
2020-12-15 20:17:22 +0300

    Nice lecton man, about money.

---
+++ notes/money.py
@@ -0,0 +1,9 @@
+""" About money."""
+
+1. Formula of gross:
+    - How many years your experience? * $500  # for dev
+                                      * $400  # for testman
+
+    - How many people under your wing? before 15 * $150  # for manager
+                                       great  15 * $100
+                                       after  50 * ??? but down
