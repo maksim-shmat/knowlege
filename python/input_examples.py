3ab2280f09d56d8f0252f4266b846f24c2b0782e
maksim-shmat <maksim.shmat@gmail.com>
2020-12-16 18:01:31 +0300

    Make an interraction question.

--- python/input_examples.py
+++ python/input_examples.py
@@ -13,10 +13,21 @@ print("Hello",firstname, surname)
 print("What do you call a bear with no teeth?\nA gummy bear!")
 
 # 004
-num1 = int(input("Please enter your first name: "))
-num2 = int(input("Please enter your surname: "))
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
 answer = num1 + num2
 print("The answer is", answer)
 
 # 005
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+num3 = int(input("Please enter your third number: "))
+answer = (num1 + num2) * num3
+print("The answer is", answer)
+
+# 006
+startNum = int(input("Enter the number of slices of pizza you started with: "))
+endNum = int(input("How many slices have you eaten? "))
+slicesLeft = startNum - endNum
+print("You have", slicesLeft, "slices remaining")
 
