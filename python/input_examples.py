6b80e300638f7e5fc5b13b2cb0ab591735e1619e
maksim-shmat <maksim.shmat@gmail.com>
2020-12-16 15:53:59 +0300

    More names for God of Names.

--- python/input_examples.py
+++ python/input_examples.py
@@ -1,10 +1,22 @@
 """ From tose book for dummy."""
 
+# 001
 firstname = input("Please inter your first name: ")
 print("Hello",firstname)
 
+# 002
 firstname = input("Please enter your first name: ")
 surname = input("Please enter your surname: ")
 print("Hello",firstname, surname)
 
+# 003
+print("What do you call a bear with no teeth?\nA gummy bear!")
+
+# 004
+num1 = int(input("Please enter your first name: "))
+num2 = int(input("Please enter your surname: "))
+answer = num1 + num2
+print("The answer is", answer)
+
+# 005
 
