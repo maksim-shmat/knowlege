3879af64b541f0ef283731bd4f63bd6ce6fbbb15
maksim-shmat <maksim.shmat@gmail.com>
2020-12-16 15:42:57 +0300

    First name, last name routine.

--- python/input_examples.py
+++ python/input_examples.py
@@ -3,4 +3,8 @@
 firstname = input("Please inter your first name: ")
 print("Hello",firstname)
 
+firstname = input("Please enter your first name: ")
+surname = input("Please enter your surname: ")
+print("Hello",firstname, surname)
+
 
