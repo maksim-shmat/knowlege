ef1275b0ae8b87a6031cb53c6a5c866d9028deaa
maksim-shmat <maksim.shmat@gmail.com>
2020-12-17 14:56:31 +0300

    Print leters from input word with change different numeric diapason.

--- python/input_examples.py
+++ python/input_examples.py
@@ -175,3 +175,14 @@ print(name)
 
 # 023
 
+phrase = input("Enter the first line of a nursery rhyme: ")
+length = len(phrase)
+print("This has", length, "letters in it")
+start = int(input("Enter a starting number: "))
+end = int(input("Enter an end number: "))
+part = (phrase[start:end])
+print(part)
+
+# 024
+
+
