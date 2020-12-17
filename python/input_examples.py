5aad1f836baf54e26af9794936f9e0a1926f67a3
maksim-shmat <maksim.shmat@gmail.com>
2020-12-17 15:11:28 +0300

    Pig Latin takes the first consonant of a word,
    moves it to the end of the word and adds on an "ay". If a word begins
    with a vowel you just add "way" to the end. For example, pig becomes
    igpay, banana becomes ananabay, and aadvorak becomes aadvarkway.
    Create a program that will ask the user to enter a word and change it
    into Pig Latin. Make sure the new word is displayed in lower case.

--- python/input_examples.py
+++ python/input_examples.py
@@ -199,5 +199,14 @@ if len(name) < 5:
 else:
     print(name.lower())
 
-# 025
+# 026
 
+word = input("Please enter a word: ")
+first = word[0]
+length = len(word)
+rest = word[1:length]
+if first != "a" and first != "e" and first != "i" and first != "o" and first != "u":
+    newword = rest + first + "ay"
+else:
+    newword = word + "way"
+print(newword.lower())
