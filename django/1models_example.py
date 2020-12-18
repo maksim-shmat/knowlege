6a04b6a6489a3a9f933460ed3d6407d1fd375c68
maksim-shmat <maksim.shmat@gmail.com>
2020-12-18 15:24:33 +0300

    Add custom methods.

--- django/1models_example.py
+++ django/1models_example.py
@@ -179,4 +179,25 @@ class Ox(models.Model):
         verbose_name_plural = "oxen"
 
 ########
+# add custom methods
+from django.db import models
 
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+
+    def baby_boomer_status(self):
+        "Return the person's baby_boomer status."
+        import datetime
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre_boomer"
+        elif self.birth_date < datetime.date(1965, 1, 1):
+            return "Baby boomer"
+        else:
+            return "Post-boomer"
+
+    @property
+    def full_name(self):
+        "Returns the person's full name."
+        return '%s %s' % (self.first_name, self.last_name)
