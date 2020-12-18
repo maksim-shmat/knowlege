1fb501f21e941956f46a83b5220a709a89272906
maksim-shmat <maksim.shmat@gmail.com>
2020-12-18 18:31:09 +0300

    Add ssh-key to github

--- django/shop/ssh_notes.py
+++ django/shop/ssh_notes.py
@@ -10,10 +10,23 @@ Enter same passphrase again: ********
 Your identification has been saved with the new passphrase.
 
 # Make a new ssh-key from github tutorial
-$ ssh-keygen -t ed25519 -C "maksim.shmat@gmail.com"
+$ ssh-keygen -t ed25519 -C "***@gmail.com"
 # or ssh-keygen -t rsa -b 4096 -C "mail@example.com"  for old system
 > Generating public/private ed25519 key pair.
 > Enter a file in which to save the key(/User/you/.ssh/id_ed25519): [Press enter]
 > Enter passphrase (empty for no passphrase): [Type a passphrase]
 > Enter same passphrase again: [Type passphrase again]
+
+# Check ssh how you have
+$ ls -al ~/.ssh  # list the files in your .ssh directory, if they exist
+# Check public keys. Default:
+                            id_rsa.pub
+                            id_ecdsa.pub
+                            id_ed25519.pub
+# Add ssh-key to ssh-agent.
+$ eval "$(ssh-agent -s)"
+> Agent pid ******
+$ ssh-add ~/.ssh/maksim-shmat_github_openssh
+>Enter passphrase for /home/jack/.ssh/maksim-shmat_github_openssh: 
+Identity added: /home/jack/.ssh/maksim-shmat_github_openssh (***@gmail.com)
 
