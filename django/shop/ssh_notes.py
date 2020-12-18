4b14f9a88231c9adfdf304f26ed26243c53aba0a
maksim-shmat <maksim.shmat@gmail.com>
2020-12-18 17:55:56 +0300

    Make a ssh-keygen and passphrase.

--- django/shop/ssh_notes.py
+++ django/shop/ssh_notes.py
@@ -9,3 +9,11 @@ Enter new passphrase (empty for no passphrase): ******** lock&key in your door
 Enter same passphrase again: ********
 Your identification has been saved with the new passphrase.
 
+# Make a new ssh-key from github tutorial
+$ ssh-keygen -t ed25519 -C "maksim.shmat@gmail.com"
+# or ssh-keygen -t rsa -b 4096 -C "mail@example.com"  for old system
+> Generating public/private ed25519 key pair.
+> Enter a file in which to save the key(/User/you/.ssh/id_ed25519): [Press enter]
+> Enter passphrase (empty for no passphrase): [Type a passphrase]
+> Enter same passphrase again: [Type passphrase again]
+
