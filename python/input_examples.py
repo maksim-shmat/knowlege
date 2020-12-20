cbcb4ee8ea611c73bfd82afc8b8a071777535351
maksim-shmat <maksim.shmat@gmail.com>
2020-12-20 13:20:39 +0300

    Create a variable called compnum and set the value to 50. Ask
    the user to enter a number. While their guess is not the same as the
    compnum value, tell them if their guess is too low or too high and
    ask them to have another guess. If they enter the same value as
    compnum, display the message "Well done, you took [count] attempts".

--- python/input_examples.py
+++ python/input_examples.py
@@ -428,7 +428,7 @@ while again == "y":
     total = total + num2
     again = input("Do you want to add another number? (y/n)")
     print("The total is ", total)
-"""
+
 # 048
 
 # Ask for the name of somebody the use wants to invite to a party. After this,
@@ -445,7 +445,27 @@ while again =="y":
     count = count + 1
     again = input("Do you want to invite somebody else? (y/n) ")
 print("You have", count, "people coming to your party")
-
+"""
 # 049
+
+# Create a variable called compnum and set the value to 50. Ask the user to
+# enter a number. While their guess is not the same as the compnum value,
+# tell them if their guess is too low or too high and ask them to have
+# another guess. If they enter the same value as compnum, display the
+# message "Well done, you took [count] attempts".
+
+compnum = 50
+guess = int(input("Can you guess the number I am thinking of? "))
+count = 1
+while guess != compnum:
+    if guess < compnum:
+        print("Too low")
+    else:
+        print("Too high")
+    count = count+1
+    guess = int(input("Have another guess: "))
+print("Well done, you took", count, "attempts")
+
+# 050
 
 
