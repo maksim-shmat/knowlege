36afabc8cef70c98bd5268b730a26145f3dec3d6
maksim-shmat <maksim.shmat@gmail.com>
2020-12-20 15:27:23 +0300

    Randomly choose a number between 1 and 5. Ask the user to
    pick a number. If they guess correctly, display the message "Well
    done", otherwise tell them if they are too high ot too low and ask
    them to pick a second number. If they guess correctly on their
    second guess, display "Correct", otherwise display "You lose.

--- python/random_examples.py
+++ python/random_examples.py
@@ -62,4 +62,31 @@ else:
     print("It was tails")
 
 # 055
+# Randomly choose a number between 1 and 5. Ask the user to pick a number. 
+# If they guess correctly, display the message "Well done", otherwise tell
+# them if they are too high or too low and them to pick a second number. If
+# they guess correctly on their second guess, display "Correct", otherwise
+# display "You lose".
+
+import random
+num = random.randint(1,5)
+guess = int(input("Enter a number: "))
+if guess == num:
+    print("Well done")
+elif guess > num:
+    print("Too high")
+    guess = int(input("Guess again: "))
+    if guess == num:
+        print("Correct")
+    else:
+        print("You lose")
+elif guess < num:
+    print("Too low")
+    guess = int(input("Guess again: "))
+    if guess == num:
+        print("Correct")
+    else:
+        print("You lose")
+
+# 056
 
