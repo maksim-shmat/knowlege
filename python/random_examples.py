91bd704a0f07e0bfb06c240301dc06f176919eb9
maksim-shmat <maksim.shmat@gmail.com>
2020-12-20 15:14:12 +0300

    Randomly choose either heads or tails("h" or "t"). Ask the user
    to make their choice. If their choice is the same as the randomly
    selected value, display the message "You win", otherwise display "Bad
    luck". At the end, tell the user if the computer selected heads or
    tails.

--- python/random_examples.py
+++ python/random_examples.py
@@ -44,4 +44,22 @@ fruit = random.choice(['apple', 'orange', 'grape', 'bannana', 'strawberry'])
 print(fruit)
 
 # 054
+# Randomly choose either heads or tails ("h" or "t"). Ask the user to make
+# their choice. If their choice is the same as the randomly selected value,
+# display the message "You win", otherwise display "Bad luck". At the end,
+# tell the user if the computer selected heads or tails.
+
+import random
+coin = random.choice(["h", "t"])
+guess = input("Enter (h)eads or (t)ails: ")
+if guess == coin:
+    print("You win")
+else:
+    print("Bad luck")
+if coin == "h":
+    print("It was heads")
+else:
+    print("It was tails")
+
+# 055
 
