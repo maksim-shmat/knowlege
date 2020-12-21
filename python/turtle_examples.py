9a46fed4287dfd59c7eee6d329fab44d655766f3
maksim-shmat <maksim.shmat@gmail.com>
2020-12-21 14:37:37 +0300

    Draw three squares in a row with a gap between each. Fill them
    using three different colours.

--- python/turtle_examples.py
+++ python/turtle_examples.py
@@ -27,7 +27,7 @@ for i in range(0, 3):
     turtle.forward(100)
     turtle.left(120)
 turtle.exitonclick()
-"""
+
 # 062
 # Draw a circle.
 
@@ -37,4 +37,38 @@ for i in range(0, 360):
     turtle.forward(1)
     turtle.left(1)
 turtle.exitonclick()
+"""
 
+# 063
+# Draw three squares in a row with a gap between each. Fill them using three
+# different colours.
+
+import turtle
+
+turtle.color("black", "red")
+turtle.begin_fill()
+for i in range(0, 4):
+    turtle.forward(70)
+    turtle.right(90)
+turtle.penup()
+turtle.end_fill()
+turtle.forward(100)
+
+turtle.pendown()
+turtle.color("black", "yellow")
+turtle.begin_fill()
+for i in range(0,4):
+    turtle.forward(70)
+    turtle.right(90)
+turtle.penup()
+turtle.end_fill()
+turtle.forward(100)
+
+turtle.pendown()
+turtle.color("black", "green")
+turtle.begin_fill()
+for i in range(0, 4):
+    turtle.forward(70)
+    turtle.right(90)
+turtle.end_fill()
+turtle.exitonclick()
