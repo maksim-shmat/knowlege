ad6b9b927d3cfaf1e3f3246f2be05af580ffe953
maksim-shmat <maksim.shmat@gmail.com>
2020-12-21 14:14:58 +0300

    Turtle make five corners 72 degree.

---
+++ python/turtle_examples.py
@@ -0,0 +1,12 @@
+""" Graphical exercises."""
+
+import turtle
+
+for i in range(0, 4):
+    turtle.forward(100)
+    turtle.right(90)
+
+for i in range(0,5):
+    turtle.forward(100)
+    turtle.right(72)
+turtle.exitonclick()
