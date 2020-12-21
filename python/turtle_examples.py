a384e470333431478bbded2521184f76a87999c8
maksim-shmat <maksim.shmat@gmail.com>
2020-12-21 14:21:19 +0300

    Make a multi fivecorners.

--- python/turtle_examples.py
+++ python/turtle_examples.py
@@ -2,11 +2,21 @@
 
 import turtle
 
+# fivecorners of 72 degrees
 for i in range(0, 4):
     turtle.forward(100)
     turtle.right(90)
-
+#turtle.exitonclick()  click on screen with turtle or print quit() in terminal
+# square
 for i in range(0,5):
     turtle.forward(100)
     turtle.right(72)
-turtle.exitonclick()
+
+# multi fivecorners
+for i in range(0, 10):
+    turtle.right(36)
+    for i in range(0, 5):
+        turtle.forward(100)
+        turtle.right(72)
+
+#
