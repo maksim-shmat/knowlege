070f59dcb825974713c83c19899f0894330f19bb
maksim-shmat <maksim.shmat@gmail.com>
2020-12-22 14:33:48 +0300

    Create a list of two sports. Ask the user what their favourite sport
    is and add this to the end of the list. Sort the list and display it.

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -12,7 +12,7 @@ print(country_tuple)
 print()
 country = input("Please enter one of the countries from above: ")
 print(country, "has index number", country_tuple.index(country))
-"""
+
 # 070
 # Add to program 069 to ask the user to enter a number and display the 
 # country in that position.
@@ -25,3 +25,15 @@ print(country, "has index number", country_tuple.index(country))
 print()
 num = int(input("Enter a number between 0 and 4: "))
 print(country_tuple[num])
+"""
+# 071
+# Create a list of two sports. Ask the user what their favourite sport is
+# and add this to the end of the list. Sort the list and display it.
+
+sports_list = ["tennis", "football"]
+sports_list.append(input("What is your favourite sport? "))
+sports_list.sort()
+print(sports_list)
+
+# 072
+
