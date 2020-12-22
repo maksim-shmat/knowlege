f796b900a4869896e50c3cabd5e356500e16a390
maksim-shmat <maksim.shmat@gmail.com>
2020-12-22 14:07:30 +0300

    Ask the user to enter a number and display the country in that position.

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -1,5 +1,6 @@
 """ Tuples, lists and dictionaries."""
 
+"""
 # 069
 # Create a tuple containing the names of five countries and display the whole
 # tuple. Ask the user to enter one of the countries that have been shown to 
@@ -11,6 +12,16 @@ print(country_tuple)
 print()
 country = input("Please enter one of the countries from above: ")
 print(country, "has index number", country_tuple.index(country))
-
+"""
 # 070
+# Add to program 069 to ask the user to enter a number and display the 
+# country in that position.
 
+country_tuple = ("France", "England", "Spain", "Germany", "Australia")
+print(country_tuple)
+print()
+country = input("Please enter one of the countries from above: ")
+print(country, "has index number", country_tuple.index(country))
+print()
+num = int(input("Enter a number between 0 and 4: "))
+print(country_tuple[num])
