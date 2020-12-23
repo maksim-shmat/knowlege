8130b7277164a6886bbaeb4a5fce75e2abb5734b
maksim-shmat <maksim.shmat@gmail.com>
2020-12-23 15:47:52 +0300

    Ask the user to enter the names of three people they want to
    invite to a party and store them in a list. After they have entered
    all three names, ask them if they want to add another. If they do,
    allow them to add more names until they answer "no". When they answer
    "no", display how many people they have invited to the party.

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -77,7 +77,7 @@ colours = ["red", "blue", "green", "black", "white", "pink", "grey",
 start = int(input("Enter a starting number(0-4): "))
 end = int(input("Enter an end number (5-9): "))
 print(colours[start: end])
-"""
+
 # 075
 # Create a list of four three-digit numbers. Display the list to the user,
 # showing each item from the list on a separate line. Ask the user to enter a
@@ -93,6 +93,23 @@ if selection in nums:
     print(selection, "is in position", nums.index(selection))
 else:
     print("That is not in the list")
-
+"""
 # 076
+# Ask the user to enter the names of three people they want to invite
+# to a party and store them in a list. After they have entered all three
+# names, ask them if they want to add another. If they do, allow them to
+# add more names until they answer "no". When they answer "no", display how
+# many people they have invited to the party.
+
+name1 = input("Enter a name of somebody you want to invite to your party: ")
+name2 = input("Enter another name: ")
+name3 = input("Enter a third name: ")
+party = [name1, name2, name3]
+another = input("Do you want to invite another (y/n): ")
+while another == "y":
+    newname = party.append(input("Enter another name: "))
+    another = input("Do you want to invite another (y/n): ")
+print("You have", len(party), "people coming to your party")
+
+# 077
 
