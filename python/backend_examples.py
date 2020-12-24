aa71ca2d5603135f448706147e5f9391fc22befb
maksim-shmat <maksim.shmat@gmail.com>
2020-12-24 11:22:51 +0300

    Create an array which contains five numbers (two of which should be
    repeated). Display the whole array to the user. Ask the user to enter
    one of the numbers from the array and then display a message saying
    how many times that number appears in the list.

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -317,4 +317,21 @@ for i in nums:
     print(i)
 
 # 091
+# Create an array which contains five numbers(two of which should be
+# repeated). Display the whole array to the user. Ask the user to enter
+# one of the numbers from the array and then display a message saying how
+# many times that number appears in the list.
+
+from array import *
+
+nums = array('i', [5, 7, 9, 2, 9])
+for i in nums:
+    print(i)
+num = int(input("Enter a number: "))
+if nums.count(num) == 1:
+    print(num, "is in the list once")
+else:
+    print(num, "is in the list", nums.count(num), "times")
+
+# 092
 
