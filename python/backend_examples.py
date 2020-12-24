f7438029deb7b2b2e0ce18328d3a0c29ebdfe267
maksim-shmat <maksim.shmat@gmail.com>
2020-12-24 08:02:22 +0300

    Ask the user to type in their name and then tell them how many
    vowels are in their name.

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -227,4 +227,16 @@ start = postcode[0:2]
 print(start.upper())
 
 # 085
-#
+# Ask the user to type in their name and then tell them how many vowels are
+# in their name.
+
+name = input("Enter your name: ")
+count = 0
+name = name.lower()
+for x in name:
+    if x == "a" or x == "e" or x == "i" or x == "o" or x == "u":
+        count = count + 1
+print("Vowels =", count)
+
+# 086
+
