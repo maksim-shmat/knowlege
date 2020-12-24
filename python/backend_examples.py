5ff8c6d36219bc6d5f3dddb582668d72529d7abe
maksim-shmat <maksim.shmat@gmail.com>
2020-12-24 08:16:13 +0300

    Ask the user to type in a word and then display it backwards on
    separate lines. For instance, if they type in "Hello" it should
    display as shown below a reverse column.

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -254,4 +254,15 @@ else:
     print("Incorrect")
 """
 # 087
-# 
+# Ask the user to type in a word and then display it backwards on separate
+# lines. For instance, if they type in "Hello" it should display as shown
+# below in reverse column.
+
+word = input("Enter a word: ")
+length = len(word)
+num = 1
+for x in word:
+    position = length - num
+    letter = word[position]
+    print(letter)
+    num = num + 1
