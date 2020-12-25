409ecd941e08b949a4d8a320a50e08e1ff4eae2d
maksim-shmat <maksim.shmat@gmail.com>
2020-12-25 12:59:40 +0300

    Create the following using a 2D dictionary showing the sales each
    person has made in the different geographical regions.

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -433,7 +433,7 @@ while tryagain == True:
 for i in range(0, 5):
     ans = nums[i]/num
     print(round(ans, 2))
-"""
+
 # 096
 # Create the following using a simple 2D list using the standard Python
 # indexig
@@ -478,6 +478,15 @@ if change == "y":
     newvalue = int(input("Enter new value: "))
     list[row][col] = newvalue
 print(list[row])
-
+"""
 # 100
+# Create the folloeing using a 2D dictionary showing th sales each person
+# has made in the different geographical regions:
+
+sales = {"John": {"N": 3056, "S": 8463, "E": 8441, "W": 2694},
+        "Tom": {"N": 4832, "S": 6786, "E": 4737, "W":3612},
+        "Anne": {"N": 5239, "S": 4802, "E": 5820, "W": 1859},
+        "Fiona": {"N": 3904, "S": 3645, "E": 8821, "W": 2451}}
+
+# 101
 
