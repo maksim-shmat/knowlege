f2e1c7a38d44d38be077255d92cddec0b2565af4
maksim-shmat <maksim.shmat@gmail.com>
2020-12-25 09:31:38 +0300

    Display an array of five numbers. Ask the user to select one of the
    numbers. Once they have selected a number, display the position of
    that item in the array. If they enter something that is not in the
    array. If they enter something that is not in the array, ask them
    to try again until they select a relevant item.

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -388,4 +388,28 @@ else:
     print("That is not a value in the array")
 
 # 094
+# Display an array of five numbers. Ask the user to select one of the 
+# numbers. Once they have selected a number, display the position of that
+# item in the array. If they enter something that is not in the array, ask
+# them to try again until they select a relevant item.
+
+from array import *
+
+nums = array('i', [4, 6, 8, 2, 5])
+
+for i in nums:
+    print(i)
+
+num = int(input("Select one of the numbers: "))
+
+tryagain = True
+while tryagain == True:
+    if num in nums:
+        print("This is in position", nums.index(num))
+        tryagain = False
+    else:
+        print("Not in array")
+        num = int(input("Select one of the numbers: "))
+
+# 095
 
