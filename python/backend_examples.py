a1e18c0be77255735f0a726057f845fa4ddccbe1
maksim-shmat <maksim.shmat@gmail.com>
2020-12-25 13:44:15 +0300

    After gathering the four names. ages and shoe sizes, ask the user to
    enter the name of the person they want to remove from the list.
    Delete this row from the data and display the other rows
    on separate lines.

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -504,7 +504,7 @@ print(sales[person][region])
 newdata = int(input("Enter new data: "))
 sales[person][region] = newdata
 print(sales[person])
-"""
+
 # 102
 # Ask the user to enter the name, age and shoe size for four people. Ask for
 # the name of one of the people in the list and display their age and shoe
@@ -533,6 +533,22 @@ for in in range(0, 4):
 
 for name in list:
     print((name), list[name]["Age"])
-
+"""
 # 104
+# After gathering the four names, ages and shoe sizes, ask the user to enter
+# the name of the person they want to remove from the list. Delete this row
+# from the data and display the other rows on separate lines.
+
+list = {}
+for i in range(0, 4):
+    name = input("Enter name: ")
+    age = int(input("Enter age: "))
+    shoe = int(input("Enter shoe size: "))
+    list[name] = {"Age": age, "Shoe size": shoe}
+
+getrid = input("Who do you want to remove from the list? ")
+del list[getrid]
+
+for name in list:
+    print((name), list[name]["Age"], list[name]["Shoe size"])
 
