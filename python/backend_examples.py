f5893fae207481a979c8ab08c82578ff73667453
maksim-shmat <maksim.shmat@gmail.com>
2020-12-26 11:40:08 +0300

    Using the Names.txt file you created earlier, display the list of
    names in Python. Ask the user to type in one of the names and then
    save all the names except the one they entered into a new file called
    Names2.txt

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -603,7 +603,7 @@ file = open("Names.txt", "r")
 print(file.read())
 file.close
 
-"""
+
 
 # 109
 # Display the following menu to the user:
@@ -642,5 +642,27 @@ elif selection == 3:
 else:
     print("Invalid option")
 
+"""
 # 110
+# Using the Names.txt file you created earlier, display the list of names in
+# Python. Ask the user to type in one of the names and then save all the
+# names except the one they entered into a new file called Names2.txt
+
+file = open("Names.txt", "r")
+print(file.read())
+file.close()
+
+file = open("Names.txt", "r")
+selectedname = input("Enter a name from the list: ")
+selectedname = selectedname + "\n"
+for row in file:
+    if row in file:
+        if row != slectedname:
+            file = open("Names2.txt", "a")
+            newrecord = row
+            file.write(newrecord)
+            file.close()
+file.close()
+
+# 111
 
