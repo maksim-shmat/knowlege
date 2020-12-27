cd3277c2cf8f96dec9628a7b86320ba1424d1a31
maksim-shmat <maksim.shmat@gmail.com>
2020-12-27 17:10:26 +0300

    Create the following menu:
    1) Add to file
    2) View all records
    3) Quit program
    Enter the number of your selection:
    If the user selects 1, allow them to add to a file called Salaries.csv
    which will store their name and salary. If they select 2 it should
    display all records in the Salaries.csv file. If they select 3 it should
    stop the program. If they select an incorrect option they should see
    an error message. They should keep returning to the menu until they
    select opton 3.

--- python/2backend_examples.py
+++ python/2backend_examples.py
@@ -60,7 +60,7 @@ def main():
         print("Incorrect selection")
 
 main()
-"""
+
 # 121
 # Create a program that will allow the user to easily manage a list of names.
 # You should display a menu that will allow them to add a name to the list,
@@ -126,5 +126,49 @@ def main():
 
 names = []
 main()
-
+"""
 # 122
+# Create the following menu:
+# 1) Add to file
+# 2) View all records
+# 3) Quit program
+# Enter the number of your selection:
+# If the user selects 1, allow them to add to a file called Salaries.csv 
+# which will store their name and salary. If they select 2 it should display
+# all records in the Salaries.csv file.  If they select 3 it should stop the
+# program. if they select an incorrect option they should see an error
+# message. They should keep returning to the menu until they select option 3.
+
+import csv
+
+def addtofile():
+    file = open("Salaries.csv", "a")
+    name = input("Enter name: ")
+    salary = int(input("Enter salary: "))
+    newrecord = name + ", " + str(salary) + "\n"
+    file.write(str(newrecord))
+    file.close()
+
+def viewrecords():
+    file = open("Salaries.csv", "r")
+    for row in file:
+        print(row)
+    file.close()
+
+tryagain = True
+while tryagain == True:
+    print("1) Add to file")
+    print("2) View all records")
+    print("3) Quit program")
+    print()
+    selection = input("Enter the number of your selction: ")
+    if selection == "1":
+        addtofile()
+    elif selection == "2":
+        viewrecords()
+    elif selection == "3":
+        tryagain = False
+    else:
+        print("Incorrect option")
+
+# 123
