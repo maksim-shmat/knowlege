e8b9d19c82f7db233f27996f544909ae29cf8442
maksim-shmat <maksim.shmat@gmail.com>
2020-12-27 17:37:17 +0300

    In Python, in is not technically possible to directly delete a
    record from a .csv file. Instead you need to save the file to a
    temporary list in Python, make the changes to the list and then
    overwrite the original file with the temporary list.
    Change the previous program to allow you to do this Your menu
    should now look like this:
    1) Add to file
    2) View all records
    3) Delete a record
    4) Quit program
    Enter the number of your selection:

--- python/2backend_examples.py
+++ python/2backend_examples.py
@@ -126,7 +126,7 @@ def main():
 
 names = []
 main()
-"""
+
 # 122
 # Create the following menu:
 # 1) Add to file
@@ -161,7 +161,7 @@ while tryagain == True:
     print("2) View all records")
     print("3) Quit program")
     print()
-    selection = input("Enter the number of your selction: ")
+    selection = input("Enter the number of your selection: ")
     if selection == "1":
         addtofile()
     elif selection == "2":
@@ -170,5 +170,67 @@ while tryagain == True:
         tryagain = False
     else:
         print("Incorrect option")
-
+"""
 # 123
+# In Python, it is not technically possible to directly delete a record
+# from a .csv file. Instead you need to save the file to a temporary list
+# in Python, make the changes to the list and then overwrite the original
+# file with the temporary list. 
+# Change the previous program to allow you to do this. Your menu should now
+# look like this:
+# 1) Add to file
+# 2) View all records
+# 3) Quit program
+# Enter the number of your selection:
+
+import csv
+
+def addtofile():
+    file = open("Salaries.csv", "a")
+    name = input("Enter name: ")
+    salary = int(input("Enter salary: "))
+    newrecord = name + ", " + str(salary) + "\n"
+    file.write(str(newrecord))
+    file.close()
+
+def viewrecords():
+    file = open("Salaries.csv", "r")
+    for row in file:
+        print(row)
+    file.close()
+
+def deleterecord():
+    file = open("Salaries.csv", "r")
+    x = 0
+    tmplist = []
+    for row in file:
+        tmplist.append(row)
+    file.close()
+    for row in tmplist:
+        print(x, row)
+        x = x + 1
+    rowtodelete = int(input("Enter the row number to delete: "))
+    del tmplist[rowtodelete]
+    file = open("Salaries.csv", "w")
+    for row in tmplist:
+        file.write(row)
+    file.close()
+
+tryagain = True
+while tryagain == True:
+    print("1) Add to file")
+    print("2) View all records")
+    print("3) Delete a record")
+    print("4) Quit program")
+    print()
+    selection = input("Enter the number of your selection: ")
+    if selection == "1":
+        addtofile()
+    elif selection == "2":
+        viewrecords()
+    elif selection == "3":
+        deleterecord()
+    elif selection == "4":
+        tryagain = False
+    else:
+        print("Incorrect option")
