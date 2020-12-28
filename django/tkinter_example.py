40402dde5413e77f381efc5f7ff54e959fe2b092
maksim-shmat <maksim.shmat@gmail.com>
2020-12-28 13:53:01 +0300

    1 km = 0.6214 miles and 1 mile = 1.6093 km. Using these figures, make
    a program that will allow the user to convert between miles and
    kilometres.

--- django/tkinter_example.py
+++ django/tkinter_example.py
@@ -184,7 +184,7 @@ clear_btn = Button(text = "Clear", command = reset)
 clear_btn.place(x = 300, y = 50, width = 50, height = 25)
 
 window.mainloop()
-"""
+
 # 127
 # Create a window that will ask the user to enter a name in a text box.
 # When they click on a button it will add it to the end of the list that
@@ -224,6 +224,52 @@ button2 = Button(text = "Clear list", command = clear_list)
 button2.place(x = 250, y = 50, width = 100, height = 25)
 
 window.mainloop()
-
+"""
 # 128
+# 1 kilometer = 0.6214 miles and 1 mile = 1.6093 kilometers. Using these 
+# figures, make a program that will allow the user to convert between miles
+# and kilometres.
 
+from tkinter import *
+
+def convert1():
+    mile = textbox1.get()
+    mile = int(mile)
+    message = mile * 1.6093
+    textbox2.delete(0, END)
+    textbox2.insert(END, message)
+    textbox2.insert(END, " kilometers")
+
+def convert2():
+    km = textbox1.get()
+    km = int(km)
+    message = km * 0.6214
+    textbox2.delete(0, END)
+    textbox2.insert(END, message)
+    textbox2.insert(END, "miles")
+
+window = Tk()
+window.title("Distance")
+window.geometry("260x200")
+
+label1 = Label(text = "Enter the value you want to convert:")
+label1.place(x = 30, y = 20)
+
+textbox1 = Entry(text = "")
+textbox1.place(x = 30, y = 50, width = 200, height = 25)
+textbox1["justify"] = "center"
+textbox1.focus()
+
+convert1 = Button(text = "Convert miles to km", command = convert1)
+convert1.place(x = 30, y = 80, width = 200, height = 25)
+
+convert2 = Button(text = "Convert km to mile", command = convert2)
+convert2.place(x = 30, y = 110, width = 200, height = 25)
+
+textbox2 = Entry(text = "")
+textbox2.place(x = 30, y = 140, width = 200, height = 25)
+textbox2["justify"] = "center"
+
+window.mainloop()
+
+# 129
