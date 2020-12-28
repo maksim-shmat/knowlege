3ef55876dfd889307cc2396cabfa3d34e28d556a
maksim-shmat <maksim.shmat@gmail.com>
2020-12-28 11:30:29 +0300

    Create a window that will ask the user to enter their name.
    When they click on a button it should display the message "Hello"
    and their name and change the background colour and font colour of
    the message box.

--- django/tkinter_example.py
+++ django/tkinter_example.py
@@ -1,5 +1,6 @@
 """ Tkinter GUI."""
 
+"""
 # A button in right-up corner of screen.
 from tkinter import *
 def Call():
@@ -71,6 +72,42 @@ output_txt["text"] = total
 
 # This must be at the end of the program to make sure it keeps working.
 window.mainloop()
+"""
+# 124
+# Create a window that will ask the user to enter their name. When they click
+# on a button it should display the message "Hello" and their name and change
+# the background colour and font colour of the message box.
 
-###########
+from tkinter import *
+
+def click():
+    name = textbox1.get()
+    message = str("Hello " + name)
+    textbox2["bg"] = "yellow"
+    textbox2["fg"] = "blue"
+    textbox2["text"] = message
+
+window = Tk()
+window.geometry("500x200")
+
+label1 = Label(text = "Enter your name:")
+label1.place(x = 30, y = 20)
+
+textbox1 = Entry(text = "")
+textbox1.place(x = 150, y = 20, width = 200, height = 25)
+textbox1["justify"] = "center"
+textbox1.focus()
+
+button1 = Button(text = "Press me", command = click)
+button1.place(x = 30, y = 50, width = 120, height = 25)
+
+textbox2 = Message(text = "")
+textbox2.place(x = 150, y = 50, width = 200, height = 25)
+textbox2["bg"] = "white"
+textbox2["fg"] = "black"
+
+window.mainloop()
+
+# 125
+
 
