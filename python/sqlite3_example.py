6b49a24d02bcd8605ac45c23e41d6124467c89da
maksim-shmat <maksim.shmat@gmail.com>
2020-12-29 17:10:44 +0300

    Create a table called employees which has four fields(id, name, dept
    and salary). It specifies the data type for each field, defines which
    field is the primary key and which fields cannot be left blank. The
    triple speech marks allow the code to be split over several lines to
    make it easier to read rather than having it all displayed in one line.

--- python/sqlite3_example.py
+++ python/sqlite3_example.py
@@ -7,4 +7,15 @@ import sqlite3
 with sqlite3.connect("company.db") as db:
     cursor=db.cursor()
 
+# Create a table called employees which has four fields(id, name, dept and
+# salary). It specifies the data type for each field, defines which field is
+# the primary key and which fields cannot be left blank. The triple speech
+# marks allow the code to be split over several lines to make it easier to 
+# read rather than having it all displayed in one line.
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS employees(
+        id integer PRIMARY KEY,
+        name text NOT NULL,
+        dept text NOT NULL,
+        salary integer);""")
 
