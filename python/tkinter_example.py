eaa8ee0764e4fb729d5f19e0b8351e86957e9aa7
maksim-shmat <maksim.shmat@gmail.com>
2020-12-29 15:35:46 +0300

    Change program 136 so that when a new name and gender is added to
    the list box it is also written to a text file. Add another button
    that will display the entire text file in the main Python shell window.

--- python/tkinter_example.py
+++ python/tkinter_example.py
@@ -657,7 +657,7 @@ imgbx.place(x = 25, y = 100, width = 200, height = 150)
 nextquestion()
 
 window.mainloop()
-"""
+
 # 135
 # Create a simple program that shows a drop-down list containing several
 # colours and a Click Me button. When the user selects a colour from the
@@ -687,4 +687,97 @@ clickme.place(x = 50, y = 150, width = 60, height = 30)
 mainloop()
 
 # 136
+# Create a program that will ask the user to enter a name and then select the
+# gender for that person from a drop-down list. It should then add the name
+# and the gender (separated by a comma) to a list box when the user clicks
+# on a button.
+
+from tkinter import *
+
+def add_to_list():
+    name = namebox.get()
+    genderselection = gender.get()
+    gender.set("M/F")
+    newdata = name + ", " + genderselection + "\n"
+    name_list.insert(END, newdata)
+    namebox.focus()
+
+window = Tk()
+window.title("People List")
+window.geometry("400x400")
+
+namelbl = Label(text = "Enter their name:")
+namelbl.place(x = 50, y = 50, width = 100, height = 25)
+namebox = Entry(text = "")
+namebox.place(x = 150, y = 50, width = 150, height = 25)
+namebox.focus()
+
+genderlbl = Label(text = "Select Gender")
+genderlbl.place(x = 50, y = 100, width = 100, height = 25)
+gender = StringVar(window)
+gender.set("M/F")
+gendermenu = OptionMenu(window, gender, "M", "F")
+gendermenu.place(x = 150, y = 100)
+
+name_list = Listbox()
+name_list.place(x = 150, y = 150, width = 150, height = 100)
+
+addbtn = Button(text = "Add to List", command = add_to_list)
+addbtn.place(x = 50, y = 300, width = 100, height = 25)
+
+window.mainloop()
+
+"""
+# 137
+# Change program 136 so that when a new name and gender is added to the list
+# box it is also written to a text file. Add another button that will display
+# the entire text file in the main Python shell window.
+
+from tkinter import *
+
+def add_to_list():
+    name = namebox.get()
+    namebox.delete(0, END)
+    genderselection = gender.get()
+    gender.set("M/F")
+    newdata = name + ", " + genderselection + "\n"
+    name_list.insert(END, newdata)
+    namebox.focus()
+    file = open("name.txt", "a")
+    file.write(newdata)
+    file.close()
+
+def print_list():
+    file = open("names.txt", "r")
+    print(file.read())
+
+window = Tk()
+window.title("People List")
+window.geometry("400x400")
+
+namelbl = Label(text = "Enter their name:")
+namelbl.place(x = 50, y = 100, width = 100, height = 25)
+namebox = Entry(text = "")
+namebox.place(x = 150, y = 50, width = 150, height = 25)
+namebox.focus()
+
+genderlbl = Label(text = "Select Gender")
+genderlbl.place(x = 50, y = 100, width = 100, height = 25)
+gender = StringVar(window)
+gender.set("M/F")
+gendermenu = OptionMenu(window, gender, "M", "F")
+gendermenu.place(x = 150, y = 100)
+
+name_list = Listbox()
+name_list.place(x = 150, y = 150, width = 150, height = 100)
+
+addbtn = Button(text = "Add to List", command = add_to_list)
+addbtn.place(x = 50, y = 300, width = 100, height = 25)
+
+printlst = Button(text = "Print List", command = print_list)
+printlst.place(x = 175, y = 300, width = 100, height = 25)
+
+window.mainloop()
+
+# 138
 
