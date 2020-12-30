e3f175cb0a8258928276566465173e602c769728
maksim-shmat <maksim.shmat@gmail.com>
2020-12-30 09:41:09 +0300

    Create an SQL database called PhonBook that contains a table called
    Names with the following data: ID  First Name  Surname Phone Number

--- python/sqlite3_example.py
+++ python/sqlite3_example.py
@@ -1,7 +1,7 @@
 """ sqlite 3 have own site."""
 
 import sqlite3
-
+'''
 # Connects to the company database. If no such database exists, it will
 # create one. The file will be stored in the same folder as teh program.
 with sqlite3.connect("company.db") as db:
@@ -87,4 +87,44 @@ cursor.execute("""SELECT employees.id,employees.name,dept.manager
 # Updates the data in the table(overwriting the original data) to change the
 # name to "Tony" for employee ID1.
 cursor.execute("UPDATE employees SET name = 'Tony' WHERE id=1")
+###
+cursor.execute("DELETE employees WHERE id=1")
+###########
+'''
+# 139
+# Create an SQL database called PhoneBook that contains a table called Names
+# with the following data:
+# ID    First Name    Surname    Phone Number
+
+import sqlite3
+with sqlite3.connect("PhoneBook.db") as db:
+    cursor = db.cursor()
+
+cursor.execute(""" CREATE TABLE IF NOT EXISTS Names(
+        id integer PRIMARY KEY,
+        firstname text,
+        surname text,
+        phonenumber text); """)
+
+cursor.execute(""" INSERT INTO Names(id,firstname,surname,phonenumber)
+        VALUES("1","Simon","Howels","01223 349752")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Names(id,firstname,surname,phonenumber)
+        VALUES("2","Karen","Phillips","01954 295773")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Names(id,firstname,surname,phonenumber)
+        VALUES("3","Darren","Smith","01583 749012")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Names(id,firstname,surname,phonenumber)
+        VALUES("4","Anne","Jones","01323 567322")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Names(id,firstname,surname,phonenumber)
+        VALUES("5","Mark","Smith","01223 855534")""")
+db.commit()
+
+db.close()
 ###
