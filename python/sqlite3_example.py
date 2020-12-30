ce2da992f2b47ff34aa1dd83075eff3c3a08a2d9
maksim-shmat <maksim.shmat@gmail.com>
2020-12-30 11:19:07 +0300

    Sqlite3. Create a new SQL database called BookInfo that will store a
    list of authors and the books they wrote. It will have two tables.
    The first one should be called Authors and contain the following data:
    Name    Place of Birth
    The second should be called Books and contain the following data:
    ID    Title    Author    Date Published

--- python/sqlite3_example.py
+++ python/sqlite3_example.py
@@ -127,7 +127,7 @@ cursor.execute(""" INSERT INTO Names(id,firstname,surname,phonenumber)
 db.commit()
 
 db.close()
-'''
+
 # 140
 # Using the PhoneBook database from program 139, write a program that will
 # display the following menu.  Main Menu
@@ -210,3 +210,90 @@ def main():
 main()
 db.close()
 
+# 141
+# Create a new SQL database called Bookinfo that will store a list of authors
+# and the books they wrote. It will have two tables. The first one should be
+# called Authors and contain the following data:
+# Name                Place of Birth
+# Agatha Christie     Torquay
+# The second should be called Books and contain the following data;
+# ID    Title    Author    Date Published
+'''
+import sqlite3
+
+with sqlite3.connect("BookInfo.db") as db:
+    cursor = db.cursor()
+
+cursor.execute(""" CREATE TABLE IF NOT EXISTS Authors(
+        Name text PRIMARY KEY,
+        PlaceofBirth text); """)
+
+cursor.execute(""" INSERT INTO Authors(Name,PlaceofBirth)
+        VALUES("Agatha Christie","Torquay")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Authors(Name,PlaceofBirth)
+        VALUES("Cecelia Ahern","Dublin")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Authors(Name,PlaceofBirth)
+        VALUES("J.K. Rowling","Bristol")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Authors(Name,PlaceofBirth)
+        VALUES("Oscar Wilde","Dublin")""")
+db.commit()
+
+cursor.execute(""" CREATE TABLE IF NOT EXISTS Books(
+        ID integere PRIMARY KEY,
+        Title text,
+        Author text,
+        DatePublished integer); """)
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("1","De Profundis","Oscar Wilde","1905")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("2","Harry Potter and the chamber of sectets","J.K. Rowling","1998")""")
+db.commit()
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("3","Harry Potter and the prisoner of Azkaban","J.K. Rowling","1999")""")
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("4","Lyrebird","Cecelia Ahern","2017")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("5","Murder on the Orient Express","Agatha Christe","1934")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("6","Perfect","Cecelia Ahern","2017")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("7","The marble collector","Cecelia Ahern","2016")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("8","The murder on the links","Agatha Christie","1923")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("9","The picture of Dorian Gray","Oscar Wilde","1890")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("10","The secret asversary","Agatha Christie","1929")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("11","The seven dials mystery","Agatha Christie","1929")""")
+db.commit()
+
+cursor.execute(""" INSERT INTO Books(ID,Title,Author,DatePublished)
+        VALUES("12","The year I met you","Cecelia Ahern","2014")""")
+db.commit()
+
+db.close()
