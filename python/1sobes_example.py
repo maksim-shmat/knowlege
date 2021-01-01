6e567e5d8fa133d52a2a42994a4ae0b2832d3ade
maksim-shmat <maksim.shmat@gmail.com>
2021-01-01 14:37:25 +0300

    Make 2D list.

---
+++ python/1sobes_example.py
@@ -0,0 +1,12 @@
+""" Simple practice questions for sobes."""
+
+# 2D list
+# Creates a multi-dimensional list. For instance, to create the following
+# table of data:
+#    0    1    2
+#0   23   16   34
+#1   45   29   48
+number_list = [[23, 16, 35], [45, 29, 48]]
+
+# Addition
+# 
