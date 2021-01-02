cb20edac4ce1a1039ad4e70e4097278d0226ad07
maksim-shmat <maksim.shmat@gmail.com>
2021-01-02 18:25:01 +0300

    Overriding the default fields.

--- django/1forms.py
+++ django/1forms.py
@@ -17,4 +17,58 @@ class PartialAuthorForm(ModelForm):
         model = Author
         exlude = ['title']
 
+###########
+# Overriding the default fields
+from django.form import ModelForm, Textarea
+from myapp.models import Author
+
+class AuthorForm(ModelForm):
+    class Meta:
+        model = Author
+        fields  = ('name', 'title', 'birth_date')
+        widgets = {
+            'name': Textarea(attrs={'cols': 80, 'rows': 20}),
+        }
+
+###
+from django.utils.translation import gettext_lazy as _
+
+class AuthorForm(ModelForm):
+    class Meta:
+        model = Author
+        fields = ('name', 'title', 'birth_date')
+        label = {
+            'name': _('Writer'),
+        }
+        help_texts = {
+            'name': _('Some useful help text.'),
+        }
+        error_messages = {
+            'name': {
+                'max_length': _("This writer's name is too long."),
+            },
+        }
+###
+from django.forms import ModelForm
+from myapp.models import Article
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['pub_date', 'headline', 'content', 'reporter', 'slug']
+        field_classes = {
+                'slug': MySlugFormField,
+        }
+###
+from django.forms import CharField, ModelForm
+from myapp.models import Article
+
+class ArticleForm(ModelForm):
+    slug = CharField(validators=[validate_slug])
+
+    class Meta:
+        model = Article
+        fields = ['pub_date', 'headline', 'content', 'reporter', 'slug']
+
+###
 
