a37b28bf21f5290c34f6bf6190fce927da3b42fd
maksim-shmat <maksim.shmat@gmail.com>
2021-01-02 12:44:03 +0300

    Django field types example.

--- django/12models_example.py
+++ django/12models_example.py
@@ -287,4 +287,52 @@ class Waiter(models.Model):
         return "%s the waiter at %s" % (self.name, self.restaurant)
 
 #############
+# Set of models for example.
+from django.db import models
+from django.forms import ModelForm
+
+TITLE_CHOICES = [
+        ('MR', 'Mr.'),
+        ('MRS', 'Mrs.'),
+        ('MS', 'Ms.'),
+]
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=3, choices=TITLE_CHOICES)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+
+class AuthorForm(ModelForm):
+    class Meta:
+        model = Author
+        field = ['name', 'title', 'birth_date']
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ['name', 'authors']
+
+# And this go to form
+from django import forms
+
+class AuthorForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    title = forms.CharField(
+            max_length=3,
+            widget=forms.Select(choices=TITLE_CHOICES),
+    )
+    birth_date = forms.DateField(required=False)
+
+class BookForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    authors = forms.ModelMultipleChoiceField(queryset=Author.objects.all())
+
+##########
 
--- django/12views_example.py
+++ django/12views_example.py
@@ -300,3 +300,22 @@ class SessionStore(DBStore):
         return obj
 
 ############
+# Field data.
+# views.py
+from django.core.mail import send_mail
+
+if form.is_valid():
+    subject = form.cleaned_data['subject']
+    message = form.cleaned_data['message']
+    sender = form.cleaned_data['sender']
+    cc_myself = form.cleaned_data['cc_myself']
+
+    recipients = ['info@example.com']
+    if cc_myself:
+        recipients.append(sender)
+
+    send_mail(subject, message, sender, recipients)
+    return HttpResponseRedirect('/thanks/')
+
+###############
+
