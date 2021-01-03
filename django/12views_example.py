018aed8abb99c172cecc31b5253782b42a80b2bd
maksim-shmat <maksim.shmat@gmail.com>
2021-01-03 17:07:37 +0300

    Using a model formset in a view.

--- django/12views_example.py
+++ django/12views_example.py
@@ -318,4 +318,20 @@ if form.is_valid():
     return HttpResponseRedirect('/thanks/')
 
 ###############
+# Using a model formset in a view.
+from django.forms import modelformset_factory
+from django.shortcuts import render
+from myapp.models import Author
 
+def manage_authors(request):
+    AuthorFormSet = modelformset_factory(Author, fields=('name', 'title'))
+    if request.method == 'POST':
+        formset = AuthorFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            formset.save()
+            # do something.
+    else:
+        formset = AuthorFormSet()
+    return render(request, 'manage_authors.html', {'formset': formset})
+
+#########
