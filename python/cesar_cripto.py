a62b2a8a882a445d80ed463b40b4133a479979b4
maksim-shmat <maksim.shmat@gmail.com>
2021-01-04 16:26:33 +0300

    Make a shiphre of Cesar.

---
+++ python/cesar_cripto.py
@@ -0,0 +1,55 @@
+""" Primitive cripto, shifre of Cesar."""
+
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+MAX_KEY_SIZE = len(SYMBOLS)
+
+def getMode():
+    while True:
+        print('You need to shifre text?')
+        mode = input().lower()
+        if mode in ['shifre', 's', 'unshifre', 'u']:
+            return mode
+        else:
+            print("Enter 'shifre' or 's' for criptup or 'unshifre' or 'u' fro criptclear.")
+
+def getMessage():
+    print('Enter a text:')
+    return input()
+
+def getKey():
+    key = 0
+    while True:
+        print('Enter a key for the shifre(1-%s)' % (MAX_KEY_SIZE))
+        key = int(input())
+
+        if (key >= 1 and key <= MAX_KEY_SIZE):
+            return key
+
+def getTranslatedMessage(mode, message, key):
+    if mode[0] == 'p':
+        key = -key
+    translated = ''
+
+    for symbol in message:
+        symbolIndex = SYMBOLS.find(symbol)
+        if symbolIndex == -1:  # Symbol not found in SYMBOLS.
+            # Just add this simbol without changes
+            translated += symbol
+        else:
+            # Crypt or Uncrypt
+            symbolIndex += key
+
+            if symbolIndex >= len(SYMBOLS):
+                symbolIndex -= len(SYMBOLS)
+            elif symbolIndex < 0:
+                symbolIndex += len(SYMBOLS)
+            
+            translated += SYMBOLS[symbolIndex]
+    return translated
+
+mode = getMode()
+message = getMessage()
+key = getKey()
+print('Changed text: ')
+print(getTranslatedMessage(mode, message, key))
--- python/game8_sonar.py
+++ python/game8_sonar.py
@@ -6,13 +6,16 @@ import math
 
 def getNewBoard():
     """ Make a structure of data in playfield size 60x15."""
-board = []
-for x in range(15):  # Each list in head list have 15 onecharacter string.
-    if random.randint(0, 1) == 0:
-        board[x].append('~')
-    else:
-        board[x].append('`')
-return board
+    board = []
+    for x in range(60):  # Head list with 60 lists
+        board.append([])
+
+    for y in range(15):  # Each list in head list have 15 onecharacter string.
+        if random.randint(0, 1) == 0:
+            board[x].append('~')
+        else:
+            board[x].append('`')
+    return board
 
 def drawBoard(board):
     """ Draw playfield."""
@@ -51,9 +54,9 @@ def getRandomChests(numChests):
     chests = []
     while len(chests) < numChests:
         newChest = [random.randint(0, 59), random.randint(0, 14)]
-if newChest not in chests:  # Make sure that chest is not
-    chests.append(newChest)
-return chests
+    if newChest not in chests:  # Make sure that chest is not
+        chests.append(newChest)
+    return chests
 
 def isOnBoard(x, y):
 # Return Truek, if coord on the field, elif False.
@@ -66,7 +69,7 @@ def makeMove(board, chests, x, y):
     """
     smallesDistance = 100  # All chests less than 100 pts.
     for cx, cy in chests:
-    distance = mathsqrt((cx - x) * (cx - x) + (cy - y) * (cy - y))
+        distance = mathsqrt((cx - x) * (cx - x) + (cy - y) * (cy - y))
 
     if distance < smallestDistance:
         smallestDistance = distance
@@ -75,39 +78,39 @@ def makeMove(board, chests, x, y):
         if smallestDistance == 0:
 # Coord xy on the chest.
             chests.remove([x, y])
-return 'You have a chest of treasures!'
-    else:
-        if smallestDistance < 10:
-            board[x][y] = str(smallestDistance)
-return 'Chest in th %s from hydrolocator.' % (smallestDistance)
-    else:
-        board[x][y] = 'X'
-return 'Hydrolocator nothing.'
+            return 'You have a chest of treasures!'
+        else:
+            if smallestDistance < 10:
+                board[x][y] = str(smallestDistance)
+                return 'Chest in th %s from hydrolocator.' % (smallestDistance)
+            else:
+                board[x][y] = 'X'
+            return 'Hydrolocator nothing.'
 
 def enterPlayerMove(previousMoves):
     # Player make a move. Return coord x,y.
-print("Where is check from hydrolockator? (coord: 0-59 0-14) (or ener "exit")")
-while True:
-    move = input()
-    if move.lower() == "exit":
-        print('Thank you for game!')
-        sys.exit()
+    print('Where is check from hydrolockator? (coord: 0-59 0-14)(or ener "exit")')
+    while True:
+        move = input()
+        if move.lower() == "exit":
+            print('Thank you for game!')
+            sys.exit()
 
-    move = move.split()
-if len(move) == 2 and move[0].isdigit() and move[1].isdigit() and isOnBoard(int(move[0]), int(move[1])):
-    if [int(move[0]), int(move[1])] in previousMoves:
-    print('Thire`s we have already.')
+        move = move.split()
+    if len(move) == 2 and move[0].isdigit() and move[1].isdigit() and isOnBoard(int(move[0]), int(move[1])):
+        if [int(move[0]), int(move[1])] in previousMoves:
+            print('Thire`s we have already.')
     continue
     return [int(move[0]), int(move[1])]
     print('Enter an interger from 9 to 59, after space, ant then integer from 0 to 14.')
 
 def showInstructions():
-    print(''' Instructions:
+    print(""" Instructions:
     You is a captain of ship, which move to the treasures. You target with
     hydrolocators make a fortune. But hyfrolocators show only length not
     curse. Enter coord that move hydrolocator. On the map will be show
     integer that value how far chest. X - it is chest there. C - chest.
-    Enter for play. ''')
+    Enter for play. """)
     input()
 
 print("Go!")
@@ -126,7 +129,7 @@ while True:
 
     while sonarDevices > 0:
 # Show sonars and chests
-print('All sonars is: %s. All chsts is: %s.'%(sonarDevices, len(theChests)))
+        print('All sonars is: %s. All chsts is: %s.'%(sonarDevices, len(theChests)))
     x, y = enterPlayerMove(previousMoves)
 previousMoves.append([x, y]) # checks all moves for visual sonars.
 
@@ -142,16 +145,16 @@ else:
     print(moveResult)
 
 if len(theChests) == 0:
-print('You checked all chests!')
+    print('You checked all chests!')
 break
 sonarDevices -= 1
 if sonarDevices == 0:
-print('All sonars on the deep! That`s all, this is the end')
-print('Go out looser, game over.')
-print('You not checked chsts in these:')
-for x, y in theChests:
-    print(' %s, %s' % (x, y))
+    print('All sonars on the deep! That`s all, this is the end')
+    print('Go out looser, game over.')
+    print('You not checked chsts in these:')
+    for x, y in theChests:
+        print(' %s, %s' % (x, y))
 
-print('Have you more a chance? (yes or no)')
-if not input().lower().startswith('y'):
-    sys.exit()
+    print('Have you more a chance? (yes or no)')
+    if not input().lower().startswith('y'):
+        sys.exit()
