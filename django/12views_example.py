fad6324cf5cc7e1cfe95ffc675dd9010a90291dc
maksim-shmat <maksim.shmat@gmail.com>
2021-01-05 18:37:37 +0300

    feat: adding extra context

--- django/12views_example.py
+++ django/12views_example.py
@@ -508,4 +508,19 @@ class PublisherList(ListView):
     context_object_name = "my_favorite_publishers"
 
 ##########
+# Adding extra context
+from django.views.generic import DetailView
+from books.models import Book, Publisher
+
+class PublisherDetail(DetailView):
+    model = Publisher
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['book_list'] = Book.objects.all()
+        return context
+
+###########
 
