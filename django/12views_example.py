bc8187be063e7516a0be430327103a1552ed3c87
maksim-shmat <maksim.shmat@gmail.com>
2021-01-05 15:26:19 +0300

    feat: handling forms with class-bassed views

--- django/12views_example.py
+++ django/12views_example.py
@@ -418,4 +418,43 @@ urlpatterns = [
         path('about/', GreetingView.as_view(greeting="G'day")),
 ]
 ############
+# Handling forms with class-based views
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import MyForm
+
+def myview(request):
+    if request.method == "POST":
+        form = MyForm(request.POST)
+        if form.is_valid():
+            # <process form cleaned data>
+            return HttpResponseRedirect('/success/')
+        else:
+            form = MyForm(initial={'key': 'value'})
+        return render(request, 'form_template.html', {'form': form})
+###
+# A similar class-based view might look like:
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views import View
+
+from .forms import MyForm
+
+class MyFormView(View):
+    form_class = MyForm
+    initial = {'key': 'value'}
+    template_name = 'form_template.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            # <process form cleaned data>
+            return HttpResponseRedirect('/success/')
+        return render(request, self.template_name, {'form': form})
+############
 
