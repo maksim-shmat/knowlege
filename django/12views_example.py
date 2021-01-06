35b6eb8fa69077e9fb8eed87801bfb3452b57539
maksim-shmat <maksim.shmat@gmail.com>
2021-01-06 16:03:33 +0300

    feat: using FormMixin with DetailView

--- django/12views_example.py
+++ django/12views_example.py
@@ -692,4 +692,42 @@ class PublisherDetail(SingleObjectMixin, ListView):
   </div>
 {% endblock %}
 ########
+# Using FormMixin with DetailView
+# CAUTION: you almost certainly do not want to do this.
+# It is provided as part of a discussion of problems you can
+# run into when combining different generic class-based view
+# functionality that is not designed to be used together.
+
+from django import forms
+from django.http import HttpResponseForbidden
+from django.urls import reverse
+from django.views.generic import DetailView
+from django.views.generic.edit import FormMixin
+from books.models import Author
+
+class AuthorInterestForm(forms.Form):
+    message = forms.CharField()
+
+class AuthorDetail(FormMixin, DetailView):
+    model = Author
+    form_class = AuthorInterestForm
+
+    def get_success_url(self):
+        return reverse('author_detail', kwargs={'pk': self.object.pk})
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        # Here, we would record the user's interest using the message
+        # passed in form.cleaned_data['message']
+        return super().form_valid(form)
+###########
 
