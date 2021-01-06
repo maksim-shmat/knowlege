87646fb47caa9433db393f35d79e0804c9de2b58
maksim-shmat <maksim.shmat@gmail.com>
2021-01-06 14:01:25 +0300

    feat: using SingleObjectMixin with View

--- django/12views_example.py
+++ django/12views_example.py
@@ -611,4 +611,27 @@ class AuthorCreate(JsonableResponseMixin, CreateView):
     model = Author
     fields = ['name']
 ###
+# Using SingleObjectMixin with View
+# views.py
+from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.urls import reverse
+from django.views import View
+from django.views.generic.detail import SingleObjectMixin
+from books.models import Author
+
+class RecordInterest(SingleObjectMixin, View):
+    """Records the current user's interest in an author."""
+    model = Author
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden()
+
+        # Look up the author we're interested in.
+        self.object = self.get_object()
+        # Actually record interest somehow here!
+
+        return HttpResponseRedirect(reverse('author-detail', kwargs={'pk': self.object.pk}))
+
+###
 
