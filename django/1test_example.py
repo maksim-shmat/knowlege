b756dd9356ff00b87e43817b45f585f9991d3b44
maksim-shmat <maksim.shmat@gmail.com>
2021-01-09 17:40:14 +0300

    test: management commands with the call_command() function

--- django/1test_example.py
+++ django/1test_example.py
@@ -385,3 +385,15 @@ class EmailTest(TestCase):
         # Verify that the subject of the first message is correct.
         self.assertEqual(mail.outbox[0].subject, 'Subject here')
 #########
+# management commands can be tested with the call_command() function.
+from io import StringIO
+from django.core.management import call_command
+from django.test import TestCase
+
+class ClosepollTest(TestCase):
+    def test_command_output(self):
+        out = StringIO()
+        call_command('closepoll', stdout=out)
+        self.assertIn('Expected output', out.getvalue())
+
+###########
