78cbff377c71721a2ae10cebb3ad8bd860bdbf6a
maksim-shmat <maksim.shmat@gmail.com>
2021-01-09 09:17:17 +0300

    test: set setting the language for test

--- django/1test_example.py
+++ django/1test_example.py
@@ -6,12 +6,11 @@ from django.utils import timezone
 from .models import Question
 
 class QuestionModelTests(TestCase):
-    """ This test for site of Questions from Django site examples, I don't 
+    """This test for site of Questions from Django site examples, I don't 
     make his that he little primitive and don't actual for prodaction.
     """
     def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
+        """ was_published_recently() returns False for questions whose pub_date
         is in the future.
         """
         time = timezone.now() + datetime.timedelta(days=30)
@@ -23,8 +22,7 @@ class QuestionModelTests(TestCase):
 ########
 # more tests for tose example
 def test_was_published_recently_with_old_question(self):
-    """
-    was_published_recently() returns False for questions whose pub_date
+    """was_published_recently() returns False for questions whose pub_date
     is older than 1 day.
     """
     time = timezone.now() - datetime.timedelta(days=1, seconds=1)
@@ -32,21 +30,19 @@ def test_was_published_recently_with_old_question(self):
     self.assertIs(old_question.was_published_recently(), False)
 
 def test_was_published_recently_with_recent_question(self):
-    """
-    was_published_recently() returns True for questions whose pub_date
+    """was_published_recently() returns True for questions whose pub_date
     is within the last day.
     """
     time = timezone.now() - datetime.timedelta(hours=23, minutes=59, second=59)
     recent_question = Question(pub_date=time)
     self.assertIs(recent_question.was_published_recently(), True)
-"""
+
 ###########
 # polls/tests.py
 from django.urls import reverse
 
 def create_question(qusetion_text, days):
-    """
-    Create a question with the given 'qustion_text' and published the
+    """Create a question with the given 'qustion_text' and published the
     given number of 'days' offset to now (negative for questions published
     in the past, polsitive for questions that have yet to be published).
     """
@@ -55,8 +51,7 @@ def create_question(qusetion_text, days):
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
-        """
-        If no questions exist, an appropriate message is displayed.
+        """If no questions exist, an appropriate message is displayed.
         """
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
@@ -64,8 +59,7 @@ class QuestionIndexViewTests(TestCase):
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_question(self):
-        """
-        Questions with a pub_date in the past are displayed on the
+        """Questions with a pub_date in the past are displayed on the
         index page.
         """
         create_question(question_text="Past question.", days=-30)
@@ -76,8 +70,7 @@ class QuestionIndexViewTests(TestCase):
         )
 
     def test_future_question(self):
-        """
-        Questions with a pub_date in the future aren't displayed on
+        """Questions with a pub_date in the future aren't displayed on
         the index page.
         """
         create_question(question_text="Future question.", days=30)
@@ -86,8 +79,7 @@ class QuestionIndexViewTests(TestCase):
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_future_question_and_past_question(self):
-        """
-        Even if both past and future question exist, only past questions
+        """Even if both past and future question exist, only past questions
         are displayed.
         """
         create_question(question_text="Past question.", days=-30)
@@ -99,16 +91,15 @@ class QuestionIndexViewTests(TestCase):
         )
 
     def test_two_past_questions(self):
-        """
-        The questions index page may display multiple questions.
+        """The questions index page may display multiple questions.
         """
         create_question(question_text="Past question 1.", days=-30)
         create_question(qusstion_text="Past question 2.", days=-5)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
                 response.context['latest_question_list'],
-                ['<Question: Past question 2.>', '<Question: Past question 1.>']
-"""
+                ['<Question: Past question 2.>', '<Question: Past question 1.>'])
+
 ###########
 # polls/tests.py testing class DetailView in views.py
 
@@ -138,3 +129,26 @@ class AnimalTestCase(TestCase):
         self.assertEqual(lion.speak(), 'The lion says "roar"')
         self.assertEqual(cat.speak(), 'The cat says "meow"')
 #########
+# Set setting the language for test
+from django.conf import settings
+
+def test_language_using_cookie(self):
+    self.client.cookies.load({settings.LANGUAGE_COOKIE_NAME: 'fr'})
+    response = self.client.get('/')
+    self.assertEqual(response.content, b"Bienvenue sur mon site.")
+
+###
+
+def test_language_using_header(self):
+    response = self.client.get('/', HTTP_ACCEPT_LANGUAGE='fr')
+    self.assertEqual(response.content, b"Bienvenue sur mon site.")
+
+###
+from django.utils import translation
+
+def test_language_using_override(self):
+    with translation.override('fr'):
+        response = self.client.get('/')
+    self.assertEqual(response.content, b"Bienvenue sur mon site.")
+
+###
