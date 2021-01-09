9944539b35551ce29ee06f48f81fd10b062f19ff
maksim-shmat <maksim.shmat@gmail.com>
2021-01-09 13:02:25 +0300

    test: use SimpleTestCase.settings()

--- django/1test_example.py
+++ django/1test_example.py
@@ -260,4 +260,18 @@ class AnimalTestCase(TestCase):
         call_some_test_code()
 
 ###########
+# use SimpleTestCase.settings()
+from django.test import TestCase
+
+class LoginTestCase(TestCase):
+    def test_login(self):
+        # First check for the default behavior
+        response = self.client.get('/sekrit/')
+        self.assertRedirect(response, '/accounts/login/?next=/sekrit/')
+
+        # Then override the LOGIN_URL setting
+        with self.settings(LOGIN_URL='/other/login/'):
+            response = self.client.get('/sekrit/')
+            self.assertRedirects(response, '/other/login/?next=/sekrit/')
+###########
 
