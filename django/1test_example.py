ac20f348ec3a3474afdf5137c470eaa2f96afb4c
maksim-shmat <maksim.shmat@gmail.com>
2021-01-09 12:30:37 +0300

    test: use dumbdata with fixtures for test

--- django/1test_example.py
+++ django/1test_example.py
@@ -244,4 +244,20 @@ class MyTest(TestCase):
         # Here self.client is an instance of MyTestClient...
         call_some_test_code()
 ##########
+# add dumbdata fixtures for the test
+from django.test import TestCase
+from myapp.models import Animal
+
+class AnimalTestCase(TestCase):
+    fixtures = ['mammals.json', 'birds']
+
+    def setUp(self):
+        # Test definitions as before.
+        call_setup_methods()
+
+    def test_fluffy_animals(self):
+        # A test that uses the fixtures.
+        call_some_test_code()
+
+###########
 
