7ab74829a296460b761d91b4ba4e56e8eb869043
maksim-shmat <maksim.shmat@gmail.com>
2021-01-09 16:23:17 +0300

    test: test email services with dummy outbox

--- django/1test_example.py
+++ django/1test_example.py
@@ -362,4 +362,26 @@ class SampleTestCase(TestCase):
         @tag('bar')
         def test(self):
             ...
-###########
+### start tests
+# $./manage.py test --tag=fast
+# $./manage.py test --tag=fast --tag=core
+# $./manage.py test --tag=core --exclude-tag=slow
+###############
+# test email services with dummy outbox
+from django.core import mail
+from django.test import TestCase
+
+class EmailTest(TestCase):
+    def test_send_email(self):
+        # Send message.
+        mail.send_mail(
+                'Subject here', 'Here is the message.',
+                'from@example.com', ['to@example.com'],
+                fail_silently=False,
+        )
+        # Test that one message has been sent.
+        self.assertEqual(len(mail.outbox), 1)
+
+        # Verify that the subject of the first message is correct.
+        self.assertEqual(mail.outbox[0].subject, 'Subject here')
+#########
