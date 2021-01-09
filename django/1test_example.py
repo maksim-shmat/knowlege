93e6fb35359f09aa25301a6a0d262bf9a7d6839f
maksim-shmat <maksim.shmat@gmail.com>
2021-01-09 10:59:53 +0300

    test: use LiveServerTestCase with selenium

--- django/1test_example.py
+++ django/1test_example.py
@@ -172,4 +172,29 @@ class SimpleTest(unittest.TestCase):
         self.assertEqual(len(response.context['customers']), 5)
 
 ############
+# liveServerTestCase with selenium
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 
+class MySeleniumTests(StaticLiveServerTestCase):
+    fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.slenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('myuser')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('secret')
+        self.selenium.find_element_by_wpath('//input[@value="Log in"]').click()
+##########
