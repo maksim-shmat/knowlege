34f1ed0765b01ec94f83375e14c9e0866a117ec5
maksim-shmat <maksim.shmat@gmail.com>
2021-01-09 09:37:00 +0300

    test: unit test using the test client

--- django/1test_example.py
+++ django/1test_example.py
@@ -151,4 +151,25 @@ def test_language_using_override(self):
         response = self.client.get('/')
     self.assertEqual(response.content, b"Bienvenue sur mon site.")
 
-###
+############
+# Unit test using the test client
+import unittest
+from django.test import Client
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/customer/details/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the rendered context contains 5 customers.
+        self.assertEqual(len(response.context['customers']), 5)
+
+############
+
