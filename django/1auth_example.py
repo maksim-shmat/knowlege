67a5e28c9c6f987adb079a700a0f5bd94abcadf9
maksim-shmat <maksim.shmat@gmail.com>
2021-01-12 16:06:55 +0300

    feat: extending the existing User model

--- django/1auth_example.py
+++ django/1auth_example.py
@@ -344,4 +344,34 @@ class SettingsBackend(BaseBackend):
         except User.DoesNotExist:
             return None
 ##########
+# extending the existing User model
+from django.contrib.auth.models import User
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = model.CharField(max_length=100)
+
+###
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from my_user_profile_app.models import Employee
+
+# Define an inline admin descriptor for Employee model
+# which acts a bit like a singleton
+class EmployeeInline(admin.StackedInline):
+    model = Employee
+    can_delete = False
+    verbose_name_plural = 'employee'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (EmployeeInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+#############
 
