f7a07450452d17803974e05a7f8c8df5fd023587
maksim-shmat <maksim.shmat@gmail.com>
2021-01-16 16:48:55 +0300

    feat: sending alternative content types

--- django/12views_example.py
+++ django/12views_example.py
@@ -904,4 +904,20 @@ def send_email(request):
         return HttpResponse('Make sure all fields are entered and valid.')
 
 ###########
+# Send a text and HTML combination
+from django.core.mail import EmailMultiAlternatives
+
+subject, from_email, to = 'hello', 'from@example.com', 'to@example.com'
+text_content = 'This is an important message.'
+html_content = '<p>This is an <strong>important</strong> message.</p>'
+msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+msg.attach_alternative(html_content, "text/html")
+msg.send()
+
+###
+msg = EmailMessage(subject, html_content, from_email, [to])
+msg.content_subtype = "html" # Main content is now text/html
+msg.send()
+
+#############
 
