6b883b18d886b590ea251ad194ed8a7d0cf9e9c3
maksim-shmat <maksim.shmat@gmail.com>
2021-01-17 19:38:35 +0300

    feat: customizing the mekemessages command

--- django/1views_example.py
+++ django/1views_example.py
@@ -421,4 +421,34 @@ urlpatterns += i18n_patterns(
         path(_('news/'), include(news_patterns, namespace='news')),
 )
 ##############
+# customazing the makemessages command
+from django.core.management.commands import makemessages
+
+class Command(makemessages.Command):
+    xgettext_options = makemessages.Command.xgettext_options + ['--
+    keyword=mytrans']
+
+###
+from django.core.management.commands import makemessages
+
+class Command(makemessages.Command):
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+                '--extra-keyword',
+                dest='xgettext_keywords',
+                action='append',
+        )
+
+    def handle(self, *args, **options):
+        xgettext_keywords = options.pop('xgettext_keywords')
+        if xgettext_keywords:
+            self.xgettext_options = (
+                    makemessages.Command.xgettext_options[:] +
+                    ['--keyword=%s' % kwd for kwd in xgettext_keywords]
+            )
+        super().handle(*args, **options)
+
+###########
 
