aa5eba254185613bd6882bee0da9298d4d2e24fc
maksim-shmat <maksim.shmat@gmail.com>
2021-01-23 06:51:02 +0300

    feat: using paginator in a view function

--- django/1views_example.py
+++ django/1views_example.py
@@ -572,4 +572,19 @@ class ContactList(ListView):
 </div>
 
 ###########
+# using paginator in a view function
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
+from myapp.models import Contact
+
+def listing(request):
+    contact_list = Contact.objects.all()
+    paginator = Paginator(contact_list, 25) # Show 25 contacts per page.
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'list.html', {'page_obj': page_obj})
+
+###########
 
