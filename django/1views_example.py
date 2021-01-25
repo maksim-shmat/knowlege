c84c1518f69cbf78c31dea95f98701ae17669b7e
maksim-shmat <maksim.shmat@gmail.com>
2021-01-25 14:54:33 +0300

    feat: connecting to singals sent by specific senders

--- django/1views_example.py
+++ django/1views_example.py
@@ -701,4 +701,13 @@ def my_callback(sender, **kwargs):
     print("Request finished!")
 
 ############
+# connecting to signals sent by specific senders
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from myapp.models import MyModel
 
+@receiver(pre_save, sender=MyModel)
+def my_handler(sender, **kwargs):
+    ...
+
+############
