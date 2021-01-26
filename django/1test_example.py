6aac22c718bdbc64bcfe31911f30e49645507df9
maksim-shmat <maksim.shmat@gmail.com>
2021-01-26 13:45:21 +0300

    feat: writing custom django-admin commands

--- django/1test_example.py
+++ django/1test_example.py
@@ -650,4 +650,37 @@ def sync_function(...):
     ...
 
 #############
+# writing custom django-admin command
+polls/
+    __init__.py
+    models.py
+    management/
+        commands/
+            _private.py
+            closepoll.py
+    tests.py
+    views.py
 
+### polls/management/commands/closepoll.py
+from django.core.management.base import BaseCommand, CommandError
+from polls.models import Question as Poll
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('poll_ids', nargs='+', type=int)
+
+    def handle(self, *args, **options):
+        for poll_id in options['poll_ids']:
+            try:
+                poll = Poll.objects.get(pk=poll_id)
+            except Poll.DoesNotExist:
+                raise CommandError('Poll "%s" does not exist' % poll_id)
+
+            poll.opened = False
+            poll.save()
+
+            self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+
+###########
