78115416f24ddb135afce8a2482f1afa4c4bc989
maksim-shmat <maksim.shmat@gmail.com>
2021-01-27 15:27:50 +0300

    feat: writing the compilation function

--- django/1views_example.py
+++ django/1views_example.py
@@ -848,4 +848,26 @@ def urlize_and_linebreaks(text, autoescape=True):
             autoescape=autoescape
     )
 ############
+# writing the compilation function
+<p>The time is {% current_time "%Y-%m-%d %I:%M %p" %}.</p>
+
+###
+from django import template
+
+def do_current_time(parser, token):
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, format_string = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError(
+                "%r tag requires a single argument" % token.contents.split()[0]
+        )
+    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+        raise template.TemplateSyntaxError(
+                "%r tag's argument should be in quotes" % tag_name
+        )
+    return CurrentTimeNode(format_string[1:-1])
+
+##########
+
 
