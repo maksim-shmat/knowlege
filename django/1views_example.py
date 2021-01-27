5f1fd3c20cdab0824aa02b15c4f187a9ed0cde32
maksim-shmat <maksim.shmat@gmail.com>
2021-01-27 16:46:12 +0300

    feat: passing template variables to the tag

--- django/1views_example.py
+++ django/1views_example.py
@@ -922,5 +922,40 @@ def shout(parser, token):
     ...
 
 ##########
+# passing template variables to the tag
+<p>This post was last updated at {% format_time blog_entry.date_updated "%Y-%m-%d %I:%M %p" %}.</p>
+
+###
+from django import template
+
+def do_format_time(parser, token):
+    try:
+        # split_contents() knows not to split quoted strings.
+        tag_name, date_to_be_formatted, format_string = token.split_contents()
+
+    except ValueError:
+        raise template.TemplateSyntaxError(
+                "%r tag requires exactly two arguments" % token.contents.split()[0]
+        )
+    if not (format_string[0] == format_string[-1] and format_string[0] in ('"', "'")):
+        raise template.TemplateSyntaxError(
+                "%r tag's argument should be in quotes" % tag_name
+        )
+    return FormatTimeNode(date_to_be_formatted, format_string[1:-1])
+
+###
+class FormatTimeNode(template.Node):
+    def __init__(self, date_to_be_formatted, format_string):
+        self.date_to_be_formatted = template.Variable(date_to_be_formatted)
+        self.format_string = format_string
+
+    def render(self, context):
+        try:
+            actual_date = self.date_to_be_formatted.resolve(context)
+            return actual_date.strftime(self.format_string)
+        except template.VariableDoesNotExist:
+            return ''
+
+#############
 
 
