60bb23513fe98b15db9ab1fb9fdf1eea35e4cc0b
maksim-shmat <maksim.shmat@gmail.com>
2021-01-28 12:28:10 +0300

    feat: streaming large csv files

--- django/13views_example.py
+++ django/13views_example.py
@@ -31,4 +31,31 @@ def some_view(request):
     return response
 
 ############
+# streaming large csv file
+import csv
+
+from django.http import StreamingHttpResponse
+
+class Echo:
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+def some_streaming_csv_view(request):
+    """A view that streams a large csv file."""
+    # Generate a sequence of rows. The range is based on the maximum number of
+    # rows that can be handled by a single sheet in most spreadsheet
+    # application.
+    rows = (["Row {}".format(idx), str(idx)] for idx in range(65536))
+    pseude_buffer = Echo()
+    writer = csv.writer(pseudo_buffer)
+    response = StreamingHttpResponse((writer.writerow(row) for row in rows),
+                                     content_type="text/csv")
+    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+    return response
+
+############
 
