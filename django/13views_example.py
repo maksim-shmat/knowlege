e320b4b4a8850681fdd3b8611d8d43ff742703b6
maksim-shmat <maksim.shmat@gmail.com>
2021-01-28 11:48:15 +0300

    feat: outputting csv

--- django/13views_example.py
+++ django/13views_example.py
@@ -17,4 +17,18 @@ class MyStorage(Storage):
         ...
 
 ###########
+# outputting csv
+import csv
+from django.http import HttpResponse
+
+def some_view(request):
+    # Create the HttpResponse object with the appropriate csv header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
+    writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+    return response
+
+############
 
