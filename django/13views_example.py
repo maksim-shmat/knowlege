570ea6f4d4db3229f8293aa5c20b68a2f6e8152e
maksim-shmat <maksim.shmat@gmail.com>
2021-01-30 13:05:28 +0300

    feat: setting x-frame-options per view

--- django/13views_example.py
+++ django/13views_example.py
@@ -800,4 +800,18 @@ from django.views.decorators.clickjacking import xframe_options_exempt
 def ok_to_load_in_a_frame(request):
     return HttpResponse("This page is safe to load in a frame on any site.")
 
-###
+### setting X-Frame-Options per view
+
+from django.http import HttpResponse
+from django.views.decorators.clickjacking import xframe_options_deny
+from django.views.decorators.clickjacking import xframe_options_sameorigin
+
+@xframe_options_deny
+def view_one(request):
+    return HttpResponse("I won't display in any frame!")
+
+@xframe_options_sameorigin
+def view_two(request):
+    return HttpResponse("Display in a frame if it's from the same origin as me.")
+
+############
