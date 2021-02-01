aaac5b36fde9defb62c952c64f49ae72000574fc
maksim-shmat <maksim.shmat@gmail.com>
2021-02-01 10:37:48 +0300

    feat: adding a password reset feature

--- django/1admin_examples.py
+++ django/1admin_examples.py
@@ -775,6 +775,30 @@ urlpatterns = [
         path('advanced-admin/', advanced_site.urls),
 ]
 
-###########
+########### adding a password reset feature
+from django.contrib.auth import views as auth_views
+
+path(
+        'admin/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='admin_password_reset',
+),
+path(
+        'admin/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+),
+path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+),
+path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+),
+
+##############
 
 
