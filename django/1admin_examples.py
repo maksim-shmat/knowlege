8af438c62ad5c8cf5cae83c9faf351f4a7e1e148
maksim-shmat <maksim.shmat@gmail.com>
2021-02-02 06:42:01 +0300

    feat: change flatpages via the admin interface

--- django/1admin_examples.py
+++ django/1admin_examples.py
@@ -1027,6 +1027,30 @@ django.jQuery(document).on('formset:removed', function(event, $row, formsetName)
     // Row removed
 });
 
-###############
+############### via the admin interface
+from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.utils.translation import gettext_lazy as _
+
+# Define a new FlatPageAdmin
+class FlatPageAdmin(FlatPageAdmin):
+    fieldsets = (
+            (None, {'fields': ('url', 'title', 'content', 'sites')}),
+            (_('Advanced options'), {
+                'classes': ('collapse',),
+                'fields': (
+                    'enable_comments',
+                    'registration_required',
+                    'template_name',
+                ),
+            }),
+    )
+
+    # Re-register FlatPageAdmin
+    admin.site.unregister(FlatPage)
+    admin.site.register(FlatPage, FlatPageAdmin)
+
+#############
 
 
