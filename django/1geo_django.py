311c1c5b392e09c1b2aa4b164ecebe8613576c8e
maksim-shmat <maksim.shmat@gmail.com>
2021-02-03 06:55:33 +0300

    feat: layer mapping

--- django/1geo_django.py
+++ django/1geo_django.py
@@ -25,4 +25,30 @@ class WorldBorder(models.Model):
     def __str__(self):
         return self.name
 
-###########
+########### LayerMapping
+# create a file called load.py inside the world application
+from pathlib import Path
+from django.contrib.gis.utils import LayerMapping
+
+world_mapping = {
+        'fips': 'FIPS',
+        'iso2': 'ISO2',
+        'iso3': 'ISO3',
+        'un': 'UN',
+        'name': 'NAME',
+        'area': 'AREA',
+        'pop2005': 'POP2005',
+        'region': 'REGION',
+        'subregion': 'SUBREGION',
+        'lon': 'LON',
+        'lat': 'LAT',
+        'mpoly': 'MULTIPOLYGON',
+}
+
+world_shp = Path(__file__).resolve().parent / 'data' / 'TM_WORLD_BORDERS-0.3.shp'
+def run(verbose=True):
+    lm = LayerMapping(WorldBorder, str(world_shp), world_mapping,
+            transform=False)
+    lm.save(strict=True, verbose=verbose)
+
+#############
