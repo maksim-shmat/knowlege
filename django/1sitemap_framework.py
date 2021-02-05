2e94cccaa361362a825c84c8a4aa4ab8a6ba67ac
maksim-shmat <maksim.shmat@gmail.com>
2021-02-05 07:38:15 +0300

    feat: using GenericSitemap

--- django/1sitemap_framework.py
+++ django/1sitemap_framework.py
@@ -22,4 +22,24 @@ class BlogSitemap(Sitemap):
     def lastmod(self, obj):
         return obj.pub_date
 
-###
+############# using GenericSitemap
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps.views import sitemap
+from django.urls import path
+from blog.models import Entry
+
+info_dict = {
+        'queryset': Entry.objects.all(),
+        'date_field': 'pub_date',
+}
+
+urlpatterns = [
+        # some generic view using info_dict
+        # ...
+        # the sitemap
+        path('sitemap.xml', sitemap,
+            {'sitemaps': {'blog': GenericSitemap(info_dict, priority=0.6)}},
+            name='django.contrib.sitemaps.views.sitemap'),
+]
+
+############# 
