b2701369e3defa158b7a508e0681e8295e38a241
maksim-shmat <maksim.shmat@gmail.com>
2021-02-06 18:25:47 +0300

    feat: multi value field

--- django/1forms.py
+++ django/1forms.py
@@ -118,5 +118,63 @@ class ContactView(FormView):
         # It should return an HttpResponse.
         form.send_email()
         return super().form_valid(form)
-##########
+
+########## configuring the rendering of a form's widgets
+from django import forms
+
+class MyForm(forms.Form):
+    default_renderer = MyRenderer()
+
+### customizing BoundField
+class GPSCoordinatesBoundField(BoundField):
+    @property
+    def country(self):
+        """
+        Return the country the coordinates lie in or None if it can't be
+        determined.
+        """
+        value = self.value()
+        if value:
+            return get_country_from_coordinates(value)
+        else:
+            return None
+
+
+class GPSCoordinatesField(Field):
+    def get_bound_field(self, form, field_name):
+        return GPSCoordinatesBoundField(form, self, field_name)
+
+### MultiValueField
+from django.core.validators import RegexValidator
+
+class PhoneField(MultiValueField):
+    def __init__(self, **kwargs):
+        # Define one message for all fields.
+        error_messages = {
+                'incomplete': 'Enter a country calling code and a phone number.',}
+        # Or define a different message for each field.
+        fields = (
+                CharField(
+                    error_messages={'incomplete': 'Enter a country calling
+                    code.'},
+                    validators=[
+                        RegexValidator(r'^[0-9]+$', 'Enter a valid country
+                        calling code.'),
+                        ],
+                    ),
+                CharField(
+                    error_messages={'incomplete': 'Enter a phone number.'},
+                    validators=[RegexValidator(r'^[0-9]+$', 'Enter a valid phone number.')],
+                    ),
+                CharField(
+                    validators=[RegexValidator(r'^[0-9]+$', 'Enter a valid extension.')],
+                    required=False,
+                ),
+            )
+        super().__init__(
+                error_messages=error_messages, fields=fields,
+                require_all_fields=False, **kwargs
+        )
+
+##############
 
