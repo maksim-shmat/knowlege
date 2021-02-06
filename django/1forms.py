288e51273d068520f2936265c4b13eef0edbc17e
maksim-shmat <maksim.shmat@gmail.com>
2021-02-06 19:21:29 +0300

    feat: setting arguments for widgets

--- django/1forms.py
+++ django/1forms.py
@@ -228,5 +228,25 @@ class CommentForm(forms.Form):
     url = forms.URLField()
     comment = forms.CharField(widget=forms.Textarea)
 
-###########
+########### setting arguments for widgets
+
+from django import forms
+
+BIRTH_YEAR_CHOICES = ['1980', '1981', '1982']
+FAVORITE_COLOR_CHOICES = [
+        ('blue', 'Blue'),
+        ('green', 'Green'),
+        ('black', 'Black'),
+]
+
+class SimpleForm(forms.Form):
+    birth_year = 
+    forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+    favorite_colors = forms.MultipleChoiceField(
+            required=False,
+            widget=forms.CheckboxSelectMultiple,
+            choices=FAVORITE_COLORS_CHOICES,
+    )
+
+############
 
