437a18e008e2406b392cb2edbcbdd332f9dfd99c
maksim-shmat <maksim.shmat@gmail.com>
2021-02-07 18:06:39 +0300

    feat: model field reference, choices

--- django/12models_example.py
+++ django/12models_example.py
@@ -459,5 +459,30 @@ class CommaSepField(models.Field):
             kwargs['separator'] = self.separator
         return name, path, args, kwargs
 
-############
+############ model field reference
+# choices
+from django.db import models
 
+class Student(models.Model):
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    GRADUATE = 'GR'
+    YEAR_IN_SCHOOL_CHOICES = [
+            (FRESHMAN, 'Freshman'),
+            (SOPHOMORE, 'Sophomore'),
+            (JUNIOR, 'Junior'),
+            (SENIOR, 'Senior'),
+            (GRADUATE, 'Graduate'),
+    ]
+    year_in_school = models.CharField(
+            max_length=2,
+            choices=YEAR_IN_SCHOOL_CHOICES,
+            default=FRESHMAN,
+    )
+
+    def is_upperclass(self):
+        return self.year_in_school in {self.JUNIOR, self.SENIOR}
+
+#########
