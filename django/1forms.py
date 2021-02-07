1a0db81d9eb142c8a569e2a69ccb36ebf972adac
maksim-shmat <maksim.shmat@gmail.com>
2021-02-07 11:53:48 +0300

    feat: multi widget

--- django/1forms.py
+++ django/1forms.py
@@ -278,5 +278,50 @@ class CommentForm(forms.ModelForm):
         self.fields['name'].widget.attrs.update({'class': 'special'})
         self.fields['comment'].widget.attrs.update(size='40')
 
+########### MultiWidget
+# decompress(value)
+from django.forms import MultiWidget
+
+class SplitDateTimeWidget(MultiWidget):
+    # ...
+    def decompress(self, value):
+        if value:
+            return [value.date(), value.time()]
+        return [None, None]
+
+### the widget template
+{% for subwidget in widget.subwidgets %}
+  {% include subwidget.template_name with widget=subwidget %}
+{% endfor %}
+
+###
+from datetime import date
+from django import forms
+
+class DateSelectorWidget(forms.MultiWidget):
+    def __init__(self, attrs=None):
+        days = [(day, day) for day in range(1, 32)]
+        months = [(month, month) for month in range(1, 13)]
+        years = [(year, year) for year in [2018, 2019, 2020]]
+        widgets = [
+                forms.Select(attrs=attrs, choices=days),
+                forms.Select(attrs=attrs, choices=months),
+                forms.Select(attrs=attrs, choices=years),
+        ]
+        super().__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if isinstance(value, date):
+            return [value.day, value.month, value.year]
+        elif isinstance(value, str):
+            year, month, day = value.split('-')
+            return [day, month, year]
+        return [None, None, None]
+
+    def value_from_datadict(self, data, files, name):
+        day, month, year = super().value_from_datadict(data, files, name)
+        # DateField expects a single string that it can parse into a date.
+        return '{}-{}-{}'.format(year, month, day)
+
 ###########
 
