7299e45e4dfcdae1793be51ecfe72991fa0743a2
maksim-shmat <maksim.shmat@gmail.com>
2021-02-07 15:45:42 +0300

    feat: make an operation that loads PostgreSQL

--- django/1postgres.py
+++ django/1postgres.py
@@ -148,4 +148,25 @@ class MyCustomOperation(Operation):
         # This is used to describe what the operation does in console output.
         return "Custom Operation"
 
-#############
+############# load in postgres 
+from django.db.migrations.operations.base import Operation
+
+class LoadEctension(Operation):
+    reversible = True
+
+    def __init__(self, name):
+        self.name = name
+
+    def state_forwards(self, app_label, state):
+        pass
+
+    def database_forwards(self, app_label, schema_editor, from_state, to_state):
+        shema_editor.execute("CREATE EXTENSION IF NOT EXISTS %s" % self.name)
+
+    def database_backwards(self, app_label, schema_editor, from_state, to_state):
+        schema_editor.execute("DROP EXTENSION %s" % self.name)
+
+    def describe(self):
+        return "Creates extension %s" % self.name
+
+###############
