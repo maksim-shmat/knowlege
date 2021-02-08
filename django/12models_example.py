49384f0042c719f46efd4abacc8315369fdce6f3
maksim-shmat <maksim.shmat@gmail.com>
2021-02-08 17:11:26 +0300

    feat: __str__()

--- django/12models_example.py
+++ django/12models_example.py
@@ -742,4 +742,14 @@ class Article(models.Model):
                     ),
                 })
 
-############
+############ __str__()
+from django.db import models
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+###
