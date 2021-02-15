26c30bc49455a1de282167f8cb69c6936d388a30
maksim-shmat <maksim.shmat@gmail.com>
2021-02-15 14:55:19 +0300

    test: testing Views

--- django/1test_example.py
+++ django/1test_example.py
@@ -806,5 +806,46 @@ class WhateverTest(TestCase):
         self.assertTrue(isinstance(w, Whatever))
         self.assertEqual(w.__unicode__(), w.title)
 
-#########
+#shell
+# coverage run manage.py test whatever -v 2
+#############
+# testing Views
+def test_whatever_list_view(self):
+    w = self.create_whatever()
+    url = reverse("whatever.views.whatever")
+    resp = self.client.get(url)
+
+    self.assertEqual(resp.status_code, 200)
+    self.assertIn(w.title, resp.content)
+
+########## coverage for whatever/views: 55%
+from django.shortcuts import render_to_response
+from whatever.models import Whatever
+from django.core.context_processors import csrf
+from django.utils import timezone
+from whatever.forms import WhateverForm
+from django.http import HttpResponse, HttpResponseRedirect
+
+def whatever(request):
+    args = {}
+    args.update(csrf(request))
+    args['whatever'] = Whatever.objects.all()
+    return render_to_response('index.html', args)
+
+def add(request):
+    if request.POST:
+        form = WhateverForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            form = WhateverForm()
+
+        args = {}
+        args.update(csrf(request))
+        args['form'] = form
+        return render_to_response('add.html', args)
+
+### selenium quick test
+import unittest
 
