f0d38004c8cac042694f610a4199fda49522a846
maksim-shmat <maksim.shmat@gmail.com>
2021-02-16 17:49:40 +0300

    feat: bootstrapping Django Outside of a Project

--- django/rezero_django.py
+++ django/rezero_django.py
@@ -99,4 +99,35 @@ class ReceiptTest(TestCase):
         expected = Decimal("37.55")
         self.assertEqual(expected, total)
 
+# $ ./manage.py test
+### boot_django.py
+# This file sets up and configures Django. It's used by scripts that
+# need to execute as if running in a Django server.
+
+import os
+import django
+from django.cont import settings
+
+BASE_DIR =
+os.path.abspath(os.path.join(os.path.dirname(__file__), "receipts"))
+
+def boot_django():
+    settings.configure(
+            BASE_DIR=BASE_DIR,
+            DEBUG=True,
+            DATABASES={
+                "default": {
+
+"ENGINE": "django.db.backends.sqlite3",
+"NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+}
+                },
+            INSTALLED_APPS=(
+                "receipts",
+            ),
+            TIME_ZONE="UTC",
+            USE_TZ=True,
+        )
+    django.setup()
+
 ###
