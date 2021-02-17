880f94a295e4cde857b40fa8dc20a6c04305027e
maksim-shmat <maksim.shmat@gmail.com>
2021-02-17 13:58:15 +0300

    feat: add admin site docs app

---
+++ django/2django_book.py
@@ -0,0 +1,10 @@
+"""Beginning django web app development."""
+
+# configure and install the Django admin site docs App
+pip install docutils
+
+url(r'^admin/doc/', include('django.contrib.admindocs.urls'))
+# add before the url(r'^admin/'...)
+http://127.0.0.1.8000/admin/doc/
+
+#########
