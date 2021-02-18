8dd6a8a7d7a669221f5ee9deec1fe91ac8655077
maksim-shmat <maksim.shmat@gmail.com>
2021-02-18 13:35:28 +0300

    feat: urls.py with include to consolidate urls

--- django/regularki4django.py
+++ django/regularki4django.py
@@ -89,4 +89,28 @@ def detail(request,store_id='1',location=None):
     # 'map' has value 'flash' or '' if map not in url
     return render(request, 'stores/detail.html')
 
+####### django urls.py with no url consolidation
+from django.conf.urls import url
+from django.views.generic import TemplateView
+from coffeehouse.about import views as about_views
+from coffeehouse.stores import views as stores_views
+
+urlpatterns = [
+        url(r'^$',TemplateView.as_view(template_name='homepage.html')),
+        url(r'^about/',about_views.index),
+        url(r'^about/contact/',about_views.contact),
+        url(r'^stores/',stores_views.index),
+        url(r'^stores/(?P<store_id>\d+)/',stores_views.detail,{'location':'headquarters'}),
+]
+
+### django urls.py with include to consolidate urls
+from django.conf.urls import include, url
+from django.views.generic import TemplateView
+
+urlpatterns = [
+        url(r'^$',TemplateView.as_view(template_name='homepage.html')),
+        url(r'^about/',include('coffeehouse.about.urls')),
+        url(r'^stores/',include('coffeehouse.stores.urls'),{'location':'headquarters'}),
+]
+
 #######
