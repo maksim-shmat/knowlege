11c31c9c978c572b93c205585235492cb0742c7e
maksim-shmat <maksim.shmat@gmail.com>
2021-02-18 08:19:22 +0300

    feat: django view method in views.py with default value

--- django/regularki4django.py
+++ django/regularki4django.py
@@ -63,4 +63,11 @@ urlpatterns = patterns[
         url(r'^stores/(?P<store_id>\d+)/',stores_views.detail),
 ]
 
-###
+####### django view method in views.py with default value
+from django.shortcuts import render
+
+def detail(request,store_id='1'):
+    # Access store_id with 'store_id' variable
+    return render(request,'stores/detail.html')
+
+######
