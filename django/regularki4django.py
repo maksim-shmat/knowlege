97cce70e53ad09aa98ed42f461f215a132ec348c
maksim-shmat <maksim.shmat@gmail.com>
2021-02-19 12:30:05 +0300

    feat: set up dict in view method for access in template

--- django/regularki4django.py
+++ django/regularki4django.py
@@ -312,4 +312,18 @@ def method(request):
 # template banners/index.html
 <a href="{% url 'banners_adverts:index' %}">{^ url'banners_adverts:index' %}</a>
 
-##########
+########## set up dict in view method for access in template
+from django.shortcuts import render
+
+def detail(request,store_id='1',location=None):
+    # Create fixed data structures to pass to template
+    # data could equally come from database queries
+    # web services or social APIs
+    STORE_NAME = 'Downtown'
+    store_address = {'street':'Main #385','city':'San Diego','state':'CA'}
+    store_amenities = ['WiFi','A/C']
+    store_menu = ((0,''),(1,'Drinks'),(2,'Food'))
+    value_for_template = {'store_name':STORE_NAME, 'store_address':store_address, 'store_amenities':store_amenities, 'store_menu':store_menu}
+    return render(request,'stores/detail.html', values_for_template)
+
+###########
