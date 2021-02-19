6af9c15f1216c9b054dafd65a8f128612cb33fa0
maksim-shmat <maksim.shmat@gmail.com>
2021-02-19 17:14:22 +0300

    feat: middleware class structure

--- django/2views_example.py
+++ django/2views_example.py
@@ -79,4 +79,31 @@ c = Context({'users': sorted_user,})
 response.write(t.render(c))
 return response
 
-######
+###### django middleware class structure
+class CoffeehouseMiddleware(object):
+    
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One_time configuration and initializtion on start_up
+
+    def __call__(self, request):
+        # Logic executed on a request before the view (and other middleware) is called.
+        # get_response call triggers next phase
+        response = self.get_response(request)
+
+        # Logic executed on response after the view is called.
+        # Return response to finish middleware sequence
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        # Logic executed before a call to view
+        # Gives access to the view itself & arguments
+
+    def process_exception(self, request, exception):
+        # Logic executed if an exception/error occurs in the view
+
+    def process_template_response(self, request, response):
+        # Logic executed after the view is called,
+        # ONLY IF vew response is TemplateResponse
+
+#######
