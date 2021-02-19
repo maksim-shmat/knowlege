cf89ae28e7299285c99a2805943884cac4c5412d
maksim-shmat <maksim.shmat@gmail.com>
2021-02-19 12:51:34 +0300

    feat: django view method response alternatives

---
+++ django/2views_example.py
@@ -0,0 +1,29 @@
+""" From beginning django web app development."""
+
+# django view method response alternatives
+# Option 1)
+from django.shortcuts import render
+
+def detail(request,store_id='1',location=None):
+    ...
+    return render(request, 'stores/detail.html', values_for_template)
+
+# Option 2)
+from django.template.response import TemplateResponse
+
+def detail(request, store_id='1', location=None):
+    ...
+    return TemplateResponse(request, 'stores/detail.html', values_for_template)
+
+# Option 3)
+from django.http import HttpResponse
+from django.template import loader, Context
+
+def detail(request, store_id='1', location=None):
+    ...
+    response = HttpResponse()
+    t = loader.get_template('stores/detail.html')
+    c = Context(values_for_template)
+    return response.write(t.render(c))
+
+######
