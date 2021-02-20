e3820ba7db0c384f68256684f3757a40609db3b4
maksim-shmat <maksim.shmat@gmail.com>
2021-02-20 12:31:25 +0300

    feat: class-based view inherited from View with multiple HTTP handling

--- django/2views_example.py
+++ django/2views_example.py
@@ -177,19 +177,42 @@ from django.views.generic import TemplateView
 class AboutIndex(TemplateView):
     template_name = 'index.html'
 
-def get_context_data(self, **kwargs):
-    # **kwargs contains keyword context initialization value (if any)
-    # Call base implementation to get a context
-    context = super(AboutIndex, self).get_context_data(**kwargs)
-    # Add context data to pass to template
-    context['aboutdata'] = 'Custom data'
-    return context
+    def get_context_data(self, **kwargs):
+        # **kwargs contains keyword context initialization value (if any)
+        # Call base implementation to get a context
+        context = super(AboutIndex, self).get_context_data(**kwargs)
+        # Add context data to pass to template
+        context['aboutdata'] = 'Custom data'
+        return context
 
 ### urls.py
 from coffeehouse.about.views import AboutIndex
 
 urlpatterns = [
         url(r'^about/index/',AboutIndex.as_view(),{'onsale':True}),
+]
+
+######### class-based view inherited from View with multiple HTTP handling
+# view.py
+from django.views.generic import View
+from django.http import HttpResponse
+from django.shortcuts import render
+
+class ContactPage(View):
+    mytemplate = 'contact.html'
+    unsupported = 'Unsupported opration'
+
+    def get(self, request):
+        return render(request, self.mytemplate)
+
+    def post(self, request):
+        return HttpResponse(self.unsupported)
+
+# urls.py
+from coffeehouse.contact.views import ContactPage
+
+urlpatterns = [
+        url(r'^contact/$', ContactPage.as_view()),
 ]
 
 #########
