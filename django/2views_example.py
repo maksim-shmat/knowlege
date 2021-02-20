2474014c6ecbb2207d839b032d284c2d46056a24
maksim-shmat <maksim.shmat@gmail.com>
2021-02-20 12:11:43 +0300

    feat: clas-based view inherited from TemplateView with url definition

--- django/2views_example.py
+++ django/2views_example.py
@@ -170,4 +170,26 @@ the_req_messages = messages.get_messages(request)
 for msg in the_req_messages:
     do_something_with_the_flash_message(msg)
 
+######### class-based view inherited from TemplateView with url definition
+# views.py
+from django.views.generic import TemplateView
+
+class AboutIndex(TemplateView):
+    template_name = 'index.html'
+
+def get_context_data(self, **kwargs):
+    # **kwargs contains keyword context initialization value (if any)
+    # Call base implementation to get a context
+    context = super(AboutIndex, self).get_context_data(**kwargs)
+    # Add context data to pass to template
+    context['aboutdata'] = 'Custom data'
+    return context
+
+### urls.py
+from coffeehouse.about.views import AboutIndex
+
+urlpatterns = [
+        url(r'^about/index/',AboutIndex.as_view(),{'onsale':True}),
+]
+
 #########
