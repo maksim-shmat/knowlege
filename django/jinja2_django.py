19ab68aece5f190bf79760a5640726413bae5d65
maksim-shmat <maksim.shmat@gmail.com>
2021-02-22 08:42:16 +0300

    feat: custom jinja environment with global variable

--- django/jinja2_django.py
+++ django/jinja2_django.py
@@ -190,4 +190,15 @@ TEMPLATES = [
   Sidebar ads
 </div>
 
-############
+############ custom jinja environment with global variable
+from jinja2.environment import Environment
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.core.urlresolvers import reverse
+
+class JinjaEnvironment(Environment):
+    def __init__(self,**kwargs):
+        super(JinjaEnvironment, self).__init__(**kwargs)
+        self.globals['static'] = staticfiles_storage.url
+        self.globals['reverse'] = reverse
+
+##########
