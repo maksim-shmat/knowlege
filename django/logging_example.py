abe0eab390b352794ea0eb7b2e259a298d3f775c
maksim-shmat <maksim.shmat@gmail.com>
2021-02-24 12:37:33 +0300

    feat: define log messages in a Python module

--- django/logging_example.py
+++ django/logging_example.py
@@ -17,4 +17,34 @@ stdlogger = logging.getLogger(__name__)
 # Custom instance logging with explicit name
 dbalogger = logging.getLogger('dba')
 
-#########
+######### define log messages in a Python module
+# Python logging package
+import logging
+
+# Standard instance of a logger with __name__
+stdlogger = loggig.getLogger(__name__)
+
+# Custom instance logging with explicit name
+dbalogger = logging.getLogger('dba')
+
+def index(request):
+    stdlogger.debug("Entering index method")
+
+def contactform(request):
+    stdlogger.info("Call to contactform method")
+
+    try:
+        stdlogger.debug("Entering store_id conditional block")
+        # Logic to handle store_id
+    except Exception, e:
+        stdlogger.exception(e)
+
+    stdlogger.info("Starting search on DB")
+    try:
+        stdlogger.info("About to search db")
+        # Loging to search db
+    except Exception, e:
+        stdlogger.error("Error in searchdb method")
+        dbalogger.error("Error in searchdb method, stack %s" % (e))
+
+##########
