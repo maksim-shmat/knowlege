837bd480718c8c9867ac5398ec74279f96d11327
maksim-shmat <maksim.shmat@gmail.com>
2021-02-24 16:14:59 +0300

    feat: send multiple emails in a single connection

--- django/2django_book.py
+++ django/2django_book.py
@@ -18,4 +18,29 @@ email = EmailMessage(subject='Coffeehouse specials', body='We would like to let 
 # Send message with built-in send() method
 email.send()
 
-########
+######## send multiple emails in a single connection with EmailMessage class
+from django.core import mail
+connection = mail.get_connection()
+
+# Manually open the connection
+connection.open()
+
+# Build message
+email = EmailMessage(subject='Coffeehouse specials', body='We would like to let you know about this week/`s specials...', from_email='stores@coffeehouse.com',
+        to=['ilovecoffee@hotmail.com', 'offecemgr@startups.com'],
+        bcc=['marketing@coffeehouse.com'], cc=['ceo@coffeehouse.com']
+        headers = {'Reply-To': 'support@coffeehouse.com'})
+
+# Build message
+email2 = EmailMessage(subject='Coffeehouse coupons', body='New coupons for our best customers...', from_email='stores@coffeehouse.com',
+        to=['officemgr@startups.com', 'food@momandpopshop.com'],
+        bcc=['marketing@coffeehouse.com'], cc=['ceo@coffeehouse.com']
+        headers = {'Reply-To': 'support@coffeehouse.com'})
+
+# Send the two emails in a single call
+connection.send_messages([email, email2])
+# The connection was already open so send_messages() doesn't close it.
+# We need to manually close the connection.
+connection.close()
+
+############
