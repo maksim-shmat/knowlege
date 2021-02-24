e7b76c568ce5e002920cd1ebb4e6e21c047cd448
maksim-shmat <maksim.shmat@gmail.com>
2021-02-24 12:21:24 +0300

    feat: define loggers in a Python module

--- django/logging_example.py
+++ django/logging_example.py
@@ -6,3 +6,15 @@ logger = logging.get Logger(__name__)
 
 def complicated_view():
     logger.debug("Entered the complicated_view()!")
+
+########## define loggers in a Python module
+# Python logging package
+import logging
+
+# Standard instance of a logger with __name__
+stdlogger = logging.getLogger(__name__)
+
+# Custom instance logging with explicit name
+dbalogger = logging.getLogger('dba')
+
+#########
