590633902e2dad6144bc8cf942ae67fbc2271ec8
maksim-shmat <maksim.shmat@gmail.com>
2021-02-24 16:02:25 +0300

    feat: send basic email with EmailMessage class

--- django/2django_book.py
+++ django/2django_book.py
@@ -7,4 +7,15 @@ url(r'^admin/doc/', include('django.contrib.admindocs.urls'))
 # add before the url(r'^admin/'...)
 http://127.0.0.1.8000/admin/doc/
 
-#########
+######### send basic email with EmailMessage class
+from django.core.mail.message import EmailMessage
+
+# Build message
+email = EmailMessage(subject='Coffeehouse specials', body='We would like to let you know about this week/`s specials...', from _email='stores@coffeehouse.com',
+        to=['ilovecoffee@hotmail.com', 'officemgr@startups.com'], bcc=['marketing@coffeehouse.com'], cc=['ceo@coffeehouse.com']
+        headers = {'Reply-To': 'support@coffeehouse.com'})
+
+# Send message with built-in send() method
+email.send()
+
+########
