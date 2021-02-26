a16d856a0cf550f7505e3145e531a1a1f3fe8f80
maksim-shmat <maksim.shmat@gmail.com>
2021-02-26 08:00:15 +0300

    feat: django form is_valid() method for form processing

--- django/1forms.py
+++ django/1forms.py
@@ -476,3 +476,37 @@ def contact(request):
 </td></tr>
 
 ###########
+class ContactForm(forms.Form):
+    name = forms.CharField(required=False)
+    email = forms.EmailField(label='Your email')
+    comment = forms.CharField(widget=forms.Textarea)
+    field_order = ['email','comment','name'] # Set order email,comment,name
+
+########### django form is_valid() method for form processing
+from django.http import HttpResponseRedirect
+
+def contact(request):
+    if request.method == 'POST':
+        # POST, generate form with data from the request
+        form = ContactForm(request.POST)
+        # Reference is now a bound instance with user data sent in POST
+        # Call is_valid() to validate data and create cleaned_data and errors dict
+        if form.is_valid():
+            # Form data is valid, you can now access validated values in the cleaned_data dict
+            # e.g form.cleaned_data['email']
+            # process data, insert into DB, generate email
+            # Redirect to a new URL
+            return HttpResponseRedirect('/about/contact/thankyou')
+        else:
+            pass # Not needed
+            # is_valid() method created errors dict, so form reference now contains errors
+            # this form reference drops to the last return statement where errors
+            # can then be presented accessing form.errors in a template
+    else:
+        # GET, generate blank form
+        form = ContactForm()
+        # Reference is now an unbound (empty) form
+    # Reference form instance (bound/unboun) is sent to template for rendering
+    return render(request,'about/contact.html',{'form':form})
+
+##########
