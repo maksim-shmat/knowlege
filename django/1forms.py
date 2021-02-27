917c48b1193fa7455f1a206e3e2251928c6bff13
maksim-shmat <maksim.shmat@gmail.com>
2021-02-27 13:55:08 +0300

    feat: djano custom form widget inherits behavior from
    forms.widgets.Input

--- django/1forms.py
+++ django/1forms.py
@@ -770,4 +770,13 @@ class PlaceholderInput(forms.widgets.Input):
         context['widget']['attrs']['placeholder'] = name.title()
         return context
 
+###### django custom form widget inherits behavior from forms.widgets.Input
+
+# about/placeholder.html
+<input type="{{ widget.type }}" name="{{ widget.name }}"{% if widget.value != None %}
+value="{{ widget.value }}"{% endif %}{% include "django/forms/widgets/attrs.html" %} ?>
+
+# django/forms/widgets/attrs.html
+{% for name, value in widget.attrs.items %}{% if value is not False %} {{ name }}{% if value is not True %}="{{ value }}"{% endif %}{% endif %}{% endfor %}
+
 ######
