67b47a5079dd7993035b5c9c46cc727888db401a
maksim-shmat <maksim.shmat@gmail.com>
2021-03-10 19:58:19 +0300

    chore: permission check in view methods with @user_passes_test

---
+++ django/15models_example.py
@@ -0,0 +1,35 @@
+""" Fifth chapter about django models."""
+
+###### Permission check in view method with @user_passes_test and @permission_required
+# Method check to see if User belongs to group called 'Barista'
+
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import Group
+
+@user_passes_test(lambda u: Group.objects.get(name='Baristas') in u.groups.all())
+def dashboard(request):
+    # Logic for dashboard
+
+# Explicit method check, if User is authenticated and has prmissions to change Store model
+
+# Explicit method with test
+def user_of_stores(user):
+    if user.is_authenticated() and user.has_perm("stores.change_store"):
+        return True
+    else:
+        return False
+
+    # Method check using method
+    @user_passes_test(user_of_stores)
+    def store_manager(request):
+        # Logic for store_manager
+
+    # Method check to see if User has permissions to add Store model
+    
+    from django.contrib.auth.decorators import permission_required
+
+    @permission_required('stores.add_store')
+    def store_creator(request):
+        # Logic for store_creator
+
+######
