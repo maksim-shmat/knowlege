fed626989627b8140e090de79b0ad5bb4a9e9f44
maksim-shmat <maksim.shmat@gmail.com>
2021-03-12 07:38:08 +0300

    chore: base Django allauth settings.py configuration

--- django/1settings_example.py
+++ django/1settings_example.py
@@ -378,4 +378,43 @@ EMAIL_HOST_USER = 'SMTP_Injection'
 EMAIL_HOST_PASSWORD = '<sparkpost_api_key>'
 EMAIL_USE_TLS = True
 
-#########
+######### Base Django allauth settings.py configuration
+
+#Ensure th 'django.contrib.sites' is declared in INSTALLED_APPS
+# And also add the allauth, allauth.account and allauth.socialaccount to INSTALLED_APPS
+
+INSTALLED_APPS = [
+        # Django sites app required
+        'django.contrib.sites',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+]
+
+# Ensure SITE_ID is set sites app
+SITE_ID = 1
+
+# Add the 'allauth' backend to AUTHENTICATION_BACKEND and keep default ModelBackend AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend', allauth.account.auth_backends.AuthenticationBackend']
+
+# EMAIL_BACKEND so allauth can proceed to send confirmation emails
+# ONLY for development/testing use console
+EMAIL_BACKEND='django.cor.mail.backends.console.EmailBackend'
+
+# Custom allauth settings
+# Use email as the primary identifier
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+# Make email verification mandatory to avoid junk email accounts
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+# Eliminate need to provide username, as it's a very old practice
+ACCOUNT_USERNAME_REQUIRED = False
+
+### Django allauth url configuration urls.py
+
+urlpatterns = [
+        ...
+        url(r'^accounnts/', include('allauth.urls')),
+        ...
+]
+
+######
