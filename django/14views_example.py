8eadae49d090bdb8cf17cf9590faf1bbd389b152
maksim-shmat <maksim.shmat@gmail.com>
2021-03-14 14:45:27 +0300

    chore: standard view method designed as REST service

--- django/14views_example.py
+++ django/14views_example.py
@@ -88,4 +88,26 @@ class UserSignUp(SuccessMessageMixin,CrateView):
             # Redirect to success url
             return HttpResponseRedirect(self.get_success_url())
 
+###### Standard view method designed as REST service
+
+# urls.py (In stores app)
+from coffeehouse.stores import views as stores_views
+
+urlpatterns = [
+        url(r'^rest/$',stores_views.rest_store,name="rest_index"),
+]
+
+# views.py (In stores app)
+from django.http import HttpResponse
+from coffeehouse.stores.models import Store
+import json
+
+def rest_store(request):
+    store_list = Store.objects.all()
+    store_names = [{"name":sotre.name} for store in store_list]
+    return HttpResponse(json.dumps(store_names), content_type='application/json')
+
+# Sample output
+# [{"name": "Corporate"}, {"name": "Downtown"}, {"name": "Uptown"}, {"name": "Midtown"}]
+
 ######
