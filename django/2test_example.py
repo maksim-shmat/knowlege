28ff9e2aad24cdd4be3071f8557d7e079753837a
maksim-shmat <maksim.shmat@gmail.com>
2021-03-17 08:55:55 +0300

    test: for homepage throws an HTTP 200 response

--- django/2test_example.py
+++ django/2test_example.py
@@ -32,4 +32,39 @@ class PostModelTest(TestCase):
         expected_object_name = f'{post.text}'
         self.assertEqual(expected_object_name, 'just a test')
 
+###### Test homepage for throws an HTTP 200 response
+
+from django.test import TestCase
+from django.urls import reverse
+from .model import Post
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post objects.create(text='just a test')
+
+    def test_text_content(self):
+        post=Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEqual(expected_object_nam, 'just a test')
+
+
+class HomePageViewTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='this is another test')
+
+    def test_view_url_exists_at_proper_location(self):
+        rest = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
+
 ######
