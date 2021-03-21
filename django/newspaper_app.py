5a8bd731bbcc6bd9ea8a57fa8dbb67cce8196ffc
maksim-shmat <maksim.shmat@gmail.com>
2021-03-21 14:55:42 +0300

    chore: add LoginRequired Mixin for auth

--- django/newspaper_app.py
+++ django/newspaper_app.py
@@ -286,4 +286,103 @@ urlpatterns = [
 </div>
 {% endblock content %}
 
-###
+###### Permissions and Authorization
+# articles/views.py
+...
+classs ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body') # new
+
+    def form_valid(self, form): # new
+        form.istance.author = self.request.user
+        return super().form_valid(form)
+...
+###### Mixins
+# articles/views.py
+from django.contrib.auth.mixins import LoginRequiredMixin # new
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+
+fom .models import Article
+
+...
+class ArticleCreateView(LoginRequiredMixin, CreateView): # new
+    ...
+
+### articles/views.py
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Article
+
+class ArticleListView(LoginRequiredMixin, ListView): # new
+    model = Article
+    template_name = 'article_list.html'
+    login_url = 'login' #new
+
+class ArticleDetailView(LoginRequiredMixin, DetailView): # new
+    model = Article
+    template_name = 'article_delete.html'
+    login_url = 'login' # new
+
+class ArticleUpdateView(LoginRequiredMixin, UpdateView): # new
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+    login_url = 'login' # new
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView): # new
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+    login_url = 'login' # new
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = models.Article
+    template_name = 'article_new.html'
+    fields = ['title', 'body',]
+    login_url = 'login' # new
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+###### UpdateView and DeleteView
+# articles/views.py
+from django.contrib.auth.mixins import (
+        LoginRequiredMixin,
+        UserPassesTestMixin # new
+)
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
+
+from .models import Article
+...
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): # new
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+    login_url = 'login'
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.use
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): # new
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+    login_url = 'login'
+
+    def test_func(self): # new
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+######
