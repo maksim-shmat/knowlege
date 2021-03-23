86c54e433424795b0e647e3d9ae73c0137c5c9a9
maksim-shmat <maksim.shmat@gmail.com>
2021-03-23 08:20:33 +0300

    chore: simple plugin architecture

--- python/py_learning/examples_from_django.py
+++ python/py_learning/examples_from_django.py
@@ -286,4 +286,20 @@ class ClassTwo(TrackedClass):
 TrackedClass._registry
 # [<class '__main__.ClassOne'>, <class '__main__.ClassTwo'>]
 
+###### A Simple Plugin Architecture
+
+class PluginMount(type):
+    def __init__(cls, name, bases, attrs):
+        if not hasattr(cls, 'plugins'):
+            # The branch only executes when processing the mount point itself.
+            # So, since this is a new plugin type, not an implementation, this
+            # class shouldn't be registered as a plugin. Instead, it sets up a
+            # list where plugins can be registered later.
+            cls.plugins = []
+        else:
+            # This must be a plugin implementation, which should be registered
+            # Simply appending it to the list is all that's needed to keep
+            # track of it later.
+            cls.plugins.append(cls)
+
 ######
