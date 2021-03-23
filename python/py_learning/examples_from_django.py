14304a23b0ec4808d319a9d91f4e9ed70cbdb1a9
maksim-shmat <maksim.shmat@gmail.com>
2021-03-23 08:50:35 +0300

    chore: make a cheker passwords with plugin

--- python/py_learning/examples_from_django.py
+++ python/py_learning/examples_from_django.py
@@ -302,4 +302,71 @@ class PluginMount(type):
             # track of it later.
             cls.plugins.append(cls)
 
+###
+class PasswordValidator(metaclass=PluginMount):
+    """
+    Plugins extending this class will be used to validate passwords.
+    Valid plugins must provide the following method.
+
+    validate(self, password)
+        Receives a password to test, and either finishes silently or raises a
+        ValueError if the password was invalid. The exception may be displayed
+        to the user, so make sure it adequately describes what's wrong.
+    """
+
+###
+def is_valid(password):
+    """
+    Return True if the password was fine, False if there was a problem.
+    """
+    for plugin in PasswordValidator.plugins:
+        try:
+            plugin().validate(password)
+        except ValueError:
+            return False
+    return True
+
+def get_password_errors(password):
+    """
+    Returns a list of messages indicating any problems that were found
+    with the password. If it was fine, this returns an empty list.
+    """
+    errors = []
+    for plugin in PasswordValidator.plugins:
+        try:
+            plugin().validate(password)
+        except ValueError as e:
+            errors.append(str(e))
+    return errors
+
+###
+class MinimumLength(PasswordValidator):
+    def validate(self, password):
+        "Raises ValueError if the password is too short."
+        if len(password) < 6:
+            raise ValueError('Passwords must be at least 6 characters.')
+
+class SpecialCharacters(PasswordValidator):
+    def validate(self, password):
+        "Raises ValueError if the password doesn't contain any special characters."
+        if password.isalnum():
+            raise ValueError('Passwords must contain at least one special character.')
+
+###
+for password in ('pass', 'password', 'p@ssword!'):
+    print(('Checking %r...' % password), end=' ')
+    if is_valid_password(password):
+        print('valid!')
+    else:
+        print() # Force a new line
+        for error in get_password_errors(password):
+            print('  %s' % error)
+
+# Checking 'pass'...
+# Password must be at least 6 chatacters.
+# Password must contanin at least one special chatacter.
+# Checking 'password'...
+# Passwords must contain at least one special character.
+# Checking 'p@ssword!'... valid!
+
 ######
