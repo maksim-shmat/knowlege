8bf1ca9c8ac097196390d51f0099796057d459f1
maksim-shmat <maksim.shmat@gmail.com>
2021-03-25 18:52:56 +0300

    chore: create a Django models (old way)

--- django/signals_django.py
+++ django/signals_django.py
@@ -179,4 +179,15 @@ class PickleField(models.TextField):
         super(PickleField, self).contibute_to_class(cls, name)
         setattr(cls, name, PickleDescriptor(self))
 
+###### Create Django models (old style)
+
+from django.db import models
+
+def create_model(name, attrs={}, meta_attrs={}, module_path='django.db.models'):
+    attrs['__module__'] = module_path
+    class Meta: pass
+    Meta.__dict__.update(meta_attrs, __module__=module_path)
+    attrs['Meta'] = Meta
+    return type(name, (models.Model,), attrs)
+
 ######
