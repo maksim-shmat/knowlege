4c5220ea13c8871ed8acbd9ae75630bf27fa1e09
maksim-shmat <maksim.shmat@gmail.com>
2021-03-25 18:27:10 +0300

    chore: unpickling with descriptor

--- django/signals_django.py
+++ django/signals_django.py
@@ -110,4 +110,73 @@ class PickleField(models.TextField):
     def get_db_prep_lookup(self, lookup_type, value):
         raise ValueError("Can't make comparisons against pickled data.")
 
+###### Unpickling on Demand 
+class PickleDescriptor(property):
+    def __init__(self, field):
+        self.field = field
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.field.name] = value
+        setattr(instance, self.field.attname, self.field.pickle(value))
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+
+        if self.field.name not in instance.__dict__:
+            # The object hasn't been created yet, so unpickle the data
+            raw_data = getattr(instance, self.field.attname)
+            instance.__dict__[self.field.name] = self.field.unpickle(raw_data)
+
+        return instance.__dict__[self.field.name]
+
+###### putting it all together, how use descriptor in this case
+
+def contribute_to_class(self, cls, name):
+    super(PickleField, self).contribute_to_class(cls, name)
+    setattr(cls, name, PickleDescriptor(self))
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+from django.db import models
+
+class PickleDescriptor(property):
+    def __init__(self, field):
+        self.field = field
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+
+        if self.field.name not in instance.__dict__:
+            # The object hasn't been created yet, so unpickle the data
+            raw_data = getattr(instance, self.field.attname)
+            instance.__dict__[self.field.name] = self.field.unpickle(raw_data)
+
+        return instance.__dict__[self.field.name]
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.field.name] = value
+        setattr(instance, self.field.attname, self.field.pickle(value))
+
+class PickleField(models.TextField):
+    def pickle(self, obj):
+        return pickle.dumps(obj)
+
+    def unpickle(self, data):
+        return pickle.loads(str(data))
+
+    def get_attname(self):
+        return '%s_pickled' % self.name
+
+    def get_db_prep_lookup(self, lookup_type, value):
+        raise ValueError("Can't make comparisons against pickled data.")
+
+    def contribute_to_class(self, cls, name):
+        super(PickleField, self).contibute_to_class(cls, name)
+        setattr(cls, name, PickleDescriptor(self))
+
 ######
