d38ab5a09bf41f556886700360c7249ed45f5ee5
maksim-shmat <maksim.shmat@gmail.com>
2021-03-27 08:14:55 +0300

    chore: form validation

--- django/12forms.py
+++ django/12forms.py
@@ -352,4 +352,31 @@ class CreateObject(edit.CreateView):
         model = MyModel
         success_url = '/'
 
+###### forms validation
+
+from django.forms import fields, uril
+
+class LatitudeField(fields.DecimalField):
+    default_error_messages = {
+            'out_of_range': u'Value must be within -90 and 90.',
+    }
+
+    def clean(self, value):
+        value = super(LatitudeField, self).clean(value)
+        if not -90 <= value <= 90:
+            raise util.ValidationError(self.error_messages['out_of_range'])
+        return value
+
+
+class LongitudeField(fields.DecimalField):
+    default_error_messages = {
+            'out_of_range': u'Value must be within -180 and 180.',
+    }
+
+    def clean(self, value):
+        value = super(LatitudeField, self).clean(value)
+        if not -180 <= value <= 180:
+            raise util.ValidationError(self.error_messages['out_of_range'])
+        return value
+
 ######
