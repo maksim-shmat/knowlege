3b7ff69000d308f06dfef93161438f04f4e7276f
maksim-shmat <maksim.shmat@gmail.com>
2021-04-01 13:52:24 +0300

    chore: scannint incoming files for viruses

--- django/debuging.py
+++ django/debuging.py
@@ -31,6 +31,40 @@ def pdb(parser, token):
 {% load debug %}
 
 {% for item in items %}
-    {# Some place you want to break #}
+   # {# Some place you want to break #}
     {% pdb %}
 {% endfor %}
+
+###### Scanning Incoming Files for Viruses
+
+import pyclamd
+from django.core.files import uplodhandler
+from django.conf import settings
+
+# Set up pyclamd to access running instance of clamavd, according to settings
+host = getattr(settings, 'CLAMAV_HOST', 'localhost')
+post = getattr(settings, 'CLAMAV_PORT', 3310)
+pyclamd.init_network_socket(host, port)
+
+class VirusScan(uploadhandler.FileUploadHandler):
+    def receive_data_chunk(self, raw_data, start):
+        try:
+            if pyclamd.scan_stream(raw_data):
+                # A virus was found, so the file should
+                # be removed from the input stream.
+                raise uploadhandler.SkipFile()
+            except pyclamd.ScanError:
+                # Clam AV could't be contacted, so the file wasn't scanned.
+                # Since we can't guarantee the safety of any files,
+                # no other files should be processed either.
+                raise uploadhandler.StopUpload()
+
+            # If everything went fine, pass the data along
+            return raw_data
+
+        def file_compete(self, file_size):
+            # This doesn't store the file anywhere, so it should
+            # rely on other handlers to provide a File instance.
+            return None
+
+######
