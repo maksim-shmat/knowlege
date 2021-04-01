c6c30457df71b1278b73158461e6823651640b64
maksim-shmat <maksim.shmat@gmail.com>
2021-04-01 18:34:17 +0300

    chore: django.utils curry(func) example

--- django/utils_django.py
+++ django/utils_django.py
@@ -33,3 +33,26 @@ f.bar
 'baz'
 
 ###### curry(func)
+
+from django.utils.functional import curry
+
+def normalize_value(value, max_value, factor=1, comment='Original'):
+    """
+    Normalizes the given value according to the provided maximum,
+    scaling it according to factor.
+    """
+    return '%s (%s)' % (float(value) / max_value * factor, comment)
+noralize_value(3, 4)
+# '0.75 (Original)'
+normalize_value(3, 4, factor=2, comment='Double')
+# '1.5 (Double)'
+percent = curry(normalize_value, max_value=100, comment='Percent')
+percent(50)
+# '0.5 (Percent)'
+percent(50, factor=2, comment='Double')
+# '1.0 (Double)'
+tripled = curry(normalize_value, factor=3, comment='Triple')
+tripled(3, 4)
+# '2.25 (Triple)'
+
+######
