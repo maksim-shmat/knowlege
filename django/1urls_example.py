dbf15947c5bf62e27fb08ee1646b584aab759e0f
maksim-shmat <maksim.shmat@gmail.com>
2021-04-02 18:59:44 +0300

    chore: url configuration for Contacts

--- django/1urls_example.py
+++ django/1urls_example.py
@@ -523,4 +523,30 @@ class FormView(View):
         else:
             return render(request, self.template_name, {'form': form})
 
+###### url configuration for Contacts
+
+from django.cont.urls.defaults import *
+from django.views.generic import ListView, DetailView
+
+from contacts import models, views
+
+urlpatterns = patterns('',
+        url(r'^$',
+            ListView.as_view(queryset=models.Contact.objects.all(),
+                             template_name='contacts/list.html',
+                             paginate_by=25),
+            name='contact_list'),
+        url(r'^add/$', views.EditContact.as_view(
+            template_name='contacts/editor_form.html',
+            ), name='contact_add_form'),
+        url(r'^(?P<slug>[\w-]+)/$',
+            DetailView.as_view(queryset=models.Contact.objects.all(),
+                               slug_field='user__username',
+                               template_name='contacts/list.html'),
+            name='contact_detail'),
+        url(r'^(?P<username>[\w-]+)/edit/$', views.EditContact.as_view(
+            template_name='contacts/editor_form.html',
+            ), name='contact_edit_form'),
+)
+
 ######
