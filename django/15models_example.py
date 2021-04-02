d1367972a5f8e5768a4fe597ef153538df3c10fa
maksim-shmat <maksim.shmat@gmail.com>
2021-04-02 15:26:44 +0300

    chore: contacts.models.contact

--- django/15models_example.py
+++ django/15models_example.py
@@ -93,4 +93,24 @@ class EmailBackend(object):
 # setting.py
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend', 'coffeehouse.registration.models.EmailBackend']
 
-######
+###### contacts.models.contact
+
+from django.db import models
+from django.contrib.auth.models import User
+from django_localflavor_us import models as us_models
+
+class Contact(models.Model):
+    user = models.OneToOneField(User)
+    phone_number = us_models.PhoneNumberField()
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = us_models.USStateField()
+    zip_code = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('user__last_name', 'user__first_name')
+
+    def __unicode__(self):
+        return self.user.get_full_name()
+
+#####
