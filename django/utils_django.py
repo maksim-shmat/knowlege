662eb303e647d9a415fccb51109f7b5cd84c5f46
maksim-shmat <maksim.shmat@gmail.com>
2021-04-02 09:47:14 +0300

    chore: partition() please from django

--- django/utils_django.py
+++ django/utils_django.py
@@ -92,4 +92,18 @@ median(fibonacci)
 median(fibonacci)
 # 2.5
 
+###### partition(predicate, values)
+
+from django.utils.functionl import prtition
+
+prtition(lambda x: x > 4, range(10))
+([0, 1, 2, 3, 4], [5, 6, 7, 8, 9])
+
+# 
+even, odd = partition(lambda x: x % 2, range(10))
+even
+# [0, 2, 4, 6, 8]
+odd
+[1, 3, 5, 7, 9]
+
 ######
