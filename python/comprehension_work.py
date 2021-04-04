ca71516d8ab82f6da123625ff4456d0a68705262
maksim-shmat <maksim.shmat@gmail.com>
2021-04-04 18:15:40 +0300

    chore: little bit work wit scraper

--- python/comprehension_work.py
+++ python/comprehension_work.py
@@ -16,3 +16,43 @@ def addOneAndTwo(nums, n):
     return [nums[i] + 1 if i % 2 == 1 else nums[i] + 2 for i in range(n)]
 
 ######
+Input: nums = [2,5,1,3,4,7],n = 3
+Output: [2,3,5,4,1,7]
+
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is
+[2,3,5,4,1,7].
+
+def shuffle(self, nums, n):
+    return reduce(lambda a, b: a + b, [[nums[i], nums[j]] for i, j in zip(range(0, n), range(n, 2 * n))])
+
+###### Number of Good Pairs
+Input nums=[1,2,3,1,1,3]
+Output: 4
+
+Explanation: There are 4 good pairs(0,3),(0,4),(3,4),(2,5) O-indexed.
+
+def numIdenticalPairs(self, nums):
+    return sum([int(i != j and nums[i] == nums[j]) for i in range(0, len(nums)) for j in range(i + 1, len(nums))])
+
+###### Kids with the Geratest Number of Candies
+Input: candies = [2,3,5,1,3] extraCandies = 3
+Output: [true,true,true,false,true]
+
+def kidsWithCandies(sel, candies, extraCandies):
+    return [candy + extraCandies >= max(candies) for candy in candies]
+
+###### Decompress Run-Length Encoded List
+Input: nums = [1,2,3,4]
+Output: [2,4,4,4]
+
+def decompressRLElist(self, nums):
+    return reduse(lambda a, b: a + b, [[nums[i + 1]] * nums[i] for i in range(0, len(nums), 2)])
+
+###### Reachest Customer's Wealth
+Input: accounts = [[1,2,3],[3,2,1]]
+Output: 6
+
+def maximumWealth(self, accounts):
+    return max([sum(row) for row in accounts])
+
+######
---
+++ python/scraper_work.py
@@ -0,0 +1,57 @@
+""" Little bit about web scraping."""
+
+import request
+from bs4 import BeautifulSoup
+import pandas as pd
+
+baseurl = "http://www.thewhiskeychange.com"
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0: Wind64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+productlinks = []
+t = {}
+data = []
+c = 0
+for x in range(1,6):
+    k = requests.get('https://www.thewhiskeyexchandge.com/c/36/japanese-whiskey?pg={}&psize=24&sort=pasc'.format(x)).text
+    soup=BeautifulSoup(k,'html.parser')
+    productlist = soup.find_all("li",{"class":"prduct_grid__item"})
+
+    for product in prductlist:
+        link = product.find("a",{"class":"prduct_card"}).get('href')
+        prductlinks.append(baseurl + link)
+
+for link in productlinks:
+    f = request.get(link,headers=headers).text
+    hub=BeautifulSoup(f,'html.parser')
+
+    try:
+        price=hun.find("p",{"class":"product-action__price"}).text.replace('\n',"")
+    except:
+        price = None
+        
+    try:
+        about=hun.find("div",{"class":"prduct-main__description"}).text.replace('\n',"")
+    except:
+        about = None
+
+    try:
+        rating = hun.find("div",{"class":"review-overview"}).text.replace('\n',"")
+    except:
+        rating = None
+
+    try:
+        name=hun.find("h1",{"class":"product-main__name"}).text.replace('\n',"")
+    except:
+        name = None
+
+    whiskey = {"name":name,"price":price,"rating":rating,"about":about}
+
+    data.append(whisky)
+    c=c+1
+    print("completed",c)
+
+df = pd.DataFrame(data)
+
+print(df)
+
+
