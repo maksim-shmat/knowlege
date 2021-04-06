59768220301c3cf49ce70b1ef959ad09d70a9010
maksim-shmat <maksim.shmat@gmail.com>
2021-04-06 15:04:31 +0300

    chore: update instruction about start shop app

--- django/shop/magAzin.py
+++ django/shop/magAzin.py
@@ -1,21 +1,28 @@
 """ magAzin.py """
 
-mkdir django2
-python3.8 -m venv virt1
-source virt1/bin/activate
-mkdir django_cms
-cd django_cms
+mkdir django
+python3.8 -m venv virt3    # make a virtual environment
+source virt3/bin/activate
+
+mkdir maksim.shmat
 python3.8 -m pip install --upgrade pip
 
 python3.8 -m pip install django
 >>> import django
 >>> print(django.get_version())
+"""
 python3.8 -m pip install django-cms
 python3.8 -m pip install djangocms-admin-style
+У них свой магаз приложений и по ходу свой хостинг, так что задеплоить на heroku наверное будет trubles. Мб потом. 
+"""
 
-
-django-admin.py startproject myproject
+django-admin.py startproject maksimshmat
 settings.py
+INSTALLED_APPS =[
+        ...
+        'maksimshmat.apps.ShopConfig',
+
+
 add 'sekizai' and more, gide: http://docs.django-cms.org/en/latest/how_to/install.html
 urls.py
 
