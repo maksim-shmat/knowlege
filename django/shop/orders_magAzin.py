59834e27d1ba9c5c2c50a0d3ed4b48a28f2f5d32
maksim-shmat <maksim.shmat@gmail.com>
2021-04-11 18:34:27 +0300

    refactor: I'm stuble cart about, Decimal not serialized JSON

--- django/shop/orders_magAzin.py
+++ django/shop/orders_magAzin.py
@@ -15,3 +15,35 @@ python manage.py makemigrations
 python manage.py migrate
 
 # next
+add models into admin site
+orders/admin.py
+
+# next
+add orders/forms.py
+class OrderCreateForm
+    class Meta
+    model = Order
+    fields =
+# next
+add handler for forms into orders/views.py
+
+# next
+make file orders/urls.py
+urlpatterns = [
+        path('create/'...)
+
+# next
+add path from orders/urls.py into maksimshmat/urls.py
+
+# next
+refactor cart/detail.html
+<a href ="#"..>
+add to "#" <a href="{% urk "orders:order_create" %}" class="button">
+  Checkout
+</a>
+
+# next
+make a structure: templates/orders/order/create.html created.html
+
+# next
+
