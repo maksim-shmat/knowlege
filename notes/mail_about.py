06fdf14523aef5a85f2ec9d8ed1c2f0bc4f02766
maksim-shmat <maksim.shmat@gmail.com>
2021-04-14 18:29:38 +0300

    chore: little bit fail with mailsend, write tutorial.

---
+++ notes/mail_about.py
@@ -0,0 +1,25 @@
+"""How send withot problems, notes."""
+
+# add me as recipient
+From reply all add my email address to folder "To", and it be in "From" and "To".
+And send it all and me myself.
+
+# Delete content
+If you push down checkbox "Include previous messages" that
+your resume not be included.
+
+# Delete default message.
+In the hands method delete "Sent from my Galaxy" it be dumb.
+
+# Cc 
+Correspondent copy, sent it my accountant for agree, for example.
+But that it address is view Recipient, keep it eye.
+
+# Bcc
+Blind correspondent copy, that address is hide, in archive in another place, for example.
+
+# Forward
+Sent new message without conversation. It is whole new message without tails.
+
+# Reply
+And reply is a chat
