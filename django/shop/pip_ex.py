c4d88137d9cfddfc405305923f155124b251dbdb
maksim-shmat <maksim.shmat@gmail.com>
2021-05-03 15:32:55 +0300

    chore: work with python and self temperature

--- django/shop/pip_ex.py
+++ django/shop/pip_ex.py
@@ -17,5 +17,37 @@ I think install all programs handle from venv3, but it be with open versions,
 not fixed. Memento!
 
 # next 
-and requirements.txt need it?
+and requirements.txt need it? Not need man.
 there are pipenv install -r requirements.txt yeah?
+
+# next
+Download from github example (requests lib)
+$ pipenv install -e git+https://github.com/requests/requests.git#egg=requests
+
+# next
+install in setarate pack --dev, not into the dev pack programs
+$ pipenv install pytest --dev 
+
+# next
+pipenv lock ???
+
+# next
+pipenv graph
+pipenv praph --reverse # all dependancies
+
+# next
+pipenv open psycopg2 (for example, open programm into editor)
+pipenv run <insert command here> ( for example python manage.py runserver)
+
+# next
+check version of programm
+pipenv check
+
+# next
+pipenv uninstall --all
+pipenv uninstall --all-dev
+
+# next
+generate requirements.txt from Pipenv
+pipenv lock -r > requirements.txt
+pipenv lock -r -d > dev-requirements.txt
---
+++ python/list_work.py
@@ -0,0 +1,13 @@
+"""Program to check if the Given is in Ascenging Order or Not."""
+
+list1 = [1, 2, 3, 5, 4, 8, 7, 9]
+temp_list = list1[:]
+list1.sort()
+if temp_list == list1:
+    print("Given List is in Ascending Order")
+else:
+    print("Given List is not in Ascending Order")
+"""
+Expected Output:
+Given List is not in Ascending Order
+"""
