29e0577ef07961416a5cc66a6dbeded60bd57d8b
maksim-shmat <maksim.shmat@gmail.com>
2021-05-10 16:06:16 +0300

    chore: little bit instructions for adding a custom model about

---
+++ django/shop/instructions.py
@@ -0,0 +1,9 @@
+"""Instructions."""
+
+# Adding  a custom user model
+1. Create a CustomUser model
+2. Update settings.py
+3. Customize UserCreationForm and UserChangeForm
+4. Add the custom user model to admin.py
+
+######
