f2d0c4cae19bc4450526bcfc94d012e5ff9a4dcb
maksim-shmat <maksim.shmat@gmail.com>
2021-05-15 18:46:38 +0300

    chore: acursed environ, or environs, or django-environ ?

--- django/shop/import_os.py
+++ django/shop/import_os.py
@@ -1,9 +1,13 @@
 """Mistical difference import os and without import os."""
 
-1. Add imporrt os
+1. Add import os
 
 2. BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 2a. BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 3. STATICFILES_DIRS = [BASE_DIR/'staticfiles',]
 3a. STATICFILE_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+
+pathlib = import os
+
+from pathlib import Path
