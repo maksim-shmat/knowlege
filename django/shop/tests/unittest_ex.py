edc3eab49659e99734aff5f63a9ec647ec846ae2
maksim-shmat <maksim.shmat@gmail.com>
2021-05-19 18:43:52 +0300

    test: work with tests

--- django/shop/tests/unittest_ex.py
+++ django/shop/tests/unittest_ex.py
@@ -21,3 +21,50 @@ class TestStringMethods(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+######
+import unittest
+
+class DefaultWidgetSizeTestCase(unittest.TestCase):
+    def test_default_widget_size(self):
+        widget = Widget('The widget')
+        self.assertEqual(widget.size(), (50, 50))
+
+###
+import unittest
+
+class WidgetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.widget = Widget('The widget')
+
+    def test_default_widget_size(self):
+        self.assertEqual(self.widget.size(), (50, 50),
+               'incorrect default size')
+
+        def test_widget_resize(self):
+            self.widget.resize(100, 150)
+            self.assertEqual(self.widget.size(), (100, 150),
+                    'wrong size after resize')
+
+######
+import unittest
+
+class WidgetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.widget = Widget('The widget')
+
+    def tearDown(self):
+        self.widget.dispose()
+
+######
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(WidgetTestCase('test_default_widget_size'))
+    suite.addTest(WidgetTestCase('test_widget_resize'))
+    return suite
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
+######
