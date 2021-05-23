7501be3e41ad2291fa482f5923fd4a1eac47941c
maksim-shmat <maksim.shmat@gmail.com>
2021-05-23 09:03:19 +0300

    test: test auth and email auth

--- django/shop/tests/test_django02.py
+++ django/shop/tests/test_django02.py
@@ -84,3 +84,70 @@ class HomepageTests(SimpleTestCase):
         )
 
 ######
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+class CustomUserTests(TestCase):
+    ...
+
+class SignupPageTests(TestCase):
+
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(
+                self.response, 'Hi there! I should not be on the page.')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+        
+    def test_signup_view(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(
+                view.func.__name__,
+                SignupPageView.as_view().__name__
+        )
+######
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from .views import HomePageView, AboutPageView
+
+class HomepageTests(SimpleTestCase):
+    ...
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About Page')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+                self.response, 'Hi there! I should not be on the page.')
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+                view.func.__name__,
+                AboutPageView.as_view().__name__
+        )
+
+######
