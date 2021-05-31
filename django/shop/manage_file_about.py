29e0bbedc5625ab246244570c8c30ac504370f6f
maksim-shmat <maksim.shmat@gmail.com>
2021-05-31 11:20:25 +0300

    chore: work with migrate.py

---
+++ django/shop/manage_file_about.py
@@ -0,0 +1,40 @@
+"""Available subcommands."""
+
+# auth
+changepassword
+createsuperuser
+
+# contenttypes
+remove_stale_contettype
+
+# django
+check
+compilemessages
+createcachetable
+dbshell
+diffsettings
+dumpdata
+flush
+inspectdb
+loaddata
+makemigrations
+migrate
+sendtestemail
+shell
+showmigrations
+sqlflush
+sqlmigrate
+sqlsequencereset
+squashmigrations
+startapp
+startproject
+test
+testserver
+
+# sessions
+clearsessions
+
+# staticfiles
+collectstatic
+findstatic
+runserver
