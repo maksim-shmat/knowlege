4be43f8350d02fd607717e6ffcd31b6f82c24dd4
maksim-shmat <maksim.shmat@gmail.com>
2021-06-08 19:29:07 +0300

    chore: work with django flash messages

--- django/flash_django_msg.py
+++ django/flash_django_msg.py
@@ -26,4 +26,36 @@ MESSAGE_LEVEL = message_constants.DEBUG
 from django.contrib.messages import constants as message_constants
 MESSAGE_LEVEL = message_constants.WARNING
 
+### Set default Django message level on a per request basis
+# Reduce threshold to DEBUG level rer request
+from django.contrib import messages
+messages.set_level(request, messages.DEBUG)
+
+# Increase threshold to WARNING level per request
+from djano.contrib import messages
+messages.set_level(request, messages.WARNING)
+
+###### Use of the fail_silently=True attribute to ignore errors in case Djangomessags framework not installed.
+from django.contrib import messages
+
+# Generic add_message method, with fail_silently=True
+messages.add_message(request, messages.INFO, 'All items on this page have freeshipping.', fail_lilently=True)
+
+# Shortcut level method, with fail_silently=True
+messages.info(request, 'All items on this page have free shipping.', fail_silently=True)
+
+###### Boilerplate code to use in Django template to display Django flash messages
+{% if messages %}
+<ul class="messages">
+  {% for msg in messages %}
+  <li>
+    <div class="alert alert-{{msg.level_tag}}" role="alert">
+    {{msg.message}}
+    </div>
+  </li>
+  {% endfor %}
+</ul>
+{% endif %}
+
 ######
+
