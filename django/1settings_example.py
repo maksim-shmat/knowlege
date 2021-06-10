e1db513b990a28c440cb747a1e1996610500da6e
maksim-shmat <maksim.shmat@gmail.com>
2021-06-10 18:56:11 +0300

    chore: work with DIRS definition with relative path in settings.py

--- django/1settings_example.py
+++ django/1settings_example.py
@@ -417,4 +417,27 @@ urlpatterns = [
         ...
 ]
 
-######
+###### DIRS definition with relative path in settings.py
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': ['%s/templates/' % (PROJECT_DIR),
+                     '%S/dev_templates/' %(PROJECT_DIR),],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+]
+
+#####
+
+
