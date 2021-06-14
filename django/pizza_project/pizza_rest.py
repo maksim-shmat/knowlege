b52bd20061bf29347ba15266db575c72197f26f8
maksim-shmat <maksim.shmat@gmail.com>
2021-06-14 08:38:15 +0300

    chore: work with Django REST serializer

--- django/pizza_project/pizza_rest.py
+++ django/pizza_project/pizza_rest.py
@@ -154,4 +154,110 @@ class PizzeriaListSerializer(serializers.ModelSerializer):
     def get_absolute_url(self, obj):
         return reverse('pizzeria_detail', args=(obj.pk,))
 
+###### update method
+class PizzeriaDetailSerializer(serializers.ModelSerializer):
+    update = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Pizzeria
+        fields = [
+                'id',
+                'pizzeria_name',
+                'street',
+                'city',
+                'state',
+                'website',
+                'phone_number',
+                'description',
+                'logo_image',
+                'email',
+                'active',
+                'update',
+        ]
+
+    def get_update(self, obj):
+        return reverse('pizzeria_update', args=(obj.pk,))
+
+###### delete method
+class PizzeriaDetailSerializer(serializers.ModelSerializer):
+    update = serializers.SerializerMethodField()
+    delete = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Pizzeria
+        fields = [
+                'id',
+                'pizzeria_name',
+                'street',
+                'city',
+                'state',
+                'zip_code',
+                'website',
+                'phone_number',
+                'description',
+                'logo_image',
+                'email',
+                'ative',
+                'update',
+                'delete',
+        ]
+
+    def get_update(self, obj):
+        return reverse('pizzeria_update', args=(obj.pk,))
+
+    def get_delete(self, obj):
+        return reverse('pizzeria_delete', args=(obj.pk,))
+
+###### add ImageSerializer to serializers.py
+from rest_framework import serializers
+from .models import Pizzeria
+from .models import Image
+from rest_framework.reverse import reverse
+
+class PizzeriaListSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Pizzeria
+        fields = [
+                'id',
+                'pizzeria_name',
+                'city',
+                'zip_code',
+                'absolute_url',
+                'logo_image',
+        ]
+
+    def get_absolute_url(self, obj):
+        return reverse('pozzeria_detail', args=(obj.pi,))
+
+
+class PizzeriaDetailSerializer(serializers.ModelSerializer):
+    update = serializers.SerializerMethodField()
+    delete = serilaizers.SerializerMethodField()
+    pizzeria_images = ImageSerializer(many=True)
+
+    class Meta:
+        model = Pizzeria
+        fields = [
+                'id',
+                'pizzeria_name',
+                'street',
+                'city',
+                'state',
+                'zip_code',
+                'website',
+                'phone_number',
+                'description',
+                'email',
+                'active',
+                'update',
+                'delete',
+                'pizzeria_images',
+        ]
+
+    def get_update(self, obj):
+
 ######
+
+
