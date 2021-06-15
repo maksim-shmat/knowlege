df4bba5130576f63f80fbd44bfa7f89af5091cd8
maksim-shmat <maksim.shmat@gmail.com>
2021-06-15 09:17:55 +0300

    chore: work with deploy

--- django/shop/deploy.py
+++ django/shop/deploy.py
@@ -16,4 +16,17 @@ else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = Path(BASE_DIR, '/var/www/ /media/')
 
+###### wirk with git
+pip freeze > requirements.txt
+make .gitignore
+
+git init
+git add .gitignore
+git commit -m "gitignore"
+git add *
+git commit -m "initial commit"
+git remote add origin your_remote_repo_url
+git remote -v
+git push origin master
+
 ######
