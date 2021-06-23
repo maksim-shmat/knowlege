c8e1f0c3804e9e2e4136a8683633b2fe70059c84
maksim-shmat <maksim.shmat@gmail.com>
2021-06-23 19:13:17 +0300

    chore: work with python print() examples

--- python/py_learning/print_n_ones.py
+++ python/py_learning/print_n_ones.py
@@ -3,3 +3,17 @@
 n=5
 string="Hello World "
 print(string * n) 
+
+### print both variables
+x = 'pi is'
+y = 3.14
+print(x,y)
+
+### print() with a specific serparator
+x = 'pi is'
+y = 3.14
+print(x, y, sep=' : ')
+
+### print() with a specific end
+print('Hello', end='-')
+print('World', end='.\n')
