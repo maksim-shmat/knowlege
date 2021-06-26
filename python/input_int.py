796fadce4c1a7d8a4a74be3174c3e701efcf7d35
maksim-shmat <maksim.shmat@gmail.com>
2021-06-26 18:03:53 +0300

    chore: work with input for integer

---
+++ python/input_int.py
@@ -0,0 +1,13 @@
+"""Input integer."""
+
+# read integer from user
+n1 = int(input('Enter a number: '))
+n2 = int(input('Ener another number: '))
+
+print('The sum of two numbers is:', n1+n2)
+
+#read integer from user
+n1 = int(input('Enter a number: '))
+print(type(n1))
+
+
