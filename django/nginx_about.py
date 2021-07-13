71f1c0cffdbcc0954e9d977e487d12e9d6b8c940
maksim-shmat <maksim.shmat@gmail.com>
2021-07-13 14:02:34 +0300

    chore: work with nginx server

---
+++ django/nginx_about.py
@@ -0,0 +1,13 @@
+"""NGINX abot."""
+
+curl localhost # nginx werlcome default HTML site
+
+nginx -v    # version
+
+ps -ef | grep nginx   # running processes
+
+nginx -h    # help
+
+nginx -s reload    # reload without stopping the server
+
+
