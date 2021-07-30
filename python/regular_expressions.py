24350e586a5861964f2c469b00a54034748c96bc
maksim-shmat <maksim.shmat@gmail.com>
2021-07-30 08:17:03 +0300

    chore: work with regular expression

--- python/regular_expressions.py
+++ python/regular_expressions.py
@@ -41,4 +41,67 @@ result = re.findall(pattern, string, flags)
 print(result)
 print()
 
+# ======================
+# re.search() return the first match for a pattern in a string
+
+import re
+
+pattern = '[a-z]+'
+string = '------2344-Hello--World!'
+result = re.search(pattern, string)
+print(result)
+print(result.group())
+print()
+
+###### re.search() pattern not in string
+
+import re
+
+pattern = '[a-z]+'
+string = '-----2344-HELLO--WORLD!'
+result = re.search(pattern, string)
+print(result)
+print()
+
+# ===================
+# re.split()
+
+import re
+
+pattern = '-+'
+string = '2344------------HELLO--WORLD!'
+result = re.split(pattern, string)
+print(result)
+print()
+
+###### re.split() split string by space
+
+import re
+
+pattern = '\s+'
+string = 'Today   is a    present'
+result = re.split(pattern, string)
+print(result)
+print()
+
+###### re.split() no matches
+
+import re
+
+pattern = '\s+'
+string = 'HelloWorld'
+result = re.split(pattern, string)
+print(result)
+print()
+
+###### re.split() maximum numbers of split
+
+import re
+
+pattern = '\s+'
+string = 'Today is a present.'
+result = re.split(pattern, string, maxsplit=2)
+print(result)
+print()
+
 ######
