ca3c54274e7826d68945d99e1e0d8bc4d3d12620
maksim-shmat <maksim.shmat@gmail.com>
2021-08-04 05:14:40 +0300

    chore: add abs() example

---
+++ python/abs_ex.py
@@ -0,0 +1,16 @@
+"""abs() function about."""
+
+# abs() with number
+
+x = -523
+absVal = abs(x)
+print(f'Absolute value of {x} is {absVal}.')
+
+###### abas() with numeric expression
+
+a = -523
+b = 451
+absVal = abs(a+b)
+print(f'Absolute value of the expression (a+b) is {absVal}.')
+
+###### string as argument to abs() - TypeError
