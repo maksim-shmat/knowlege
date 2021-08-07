2421ac50e54b0c2fd9a2fc274e5c0c48ec68d7d4
maksim-shmat <maksim.shmat@gmail.com>
2021-08-07 06:35:59 +0300

    chore: work with len()

---
+++ python/len_ex.py
@@ -0,0 +1,52 @@
+"""len() about."""
+
+# len(frozenset)
+
+vowels = frozenset(('a', 'e', 'i', 'o', 'u'))
+n = len(vowels)
+print(n)
+print()
+
+###### len(set)
+
+mySet = {24, 85, 73, 66}
+n = len(mySet)
+print(n)
+print()
+
+###### len(dict)
+
+myDict = {'a': 12, 'b':45, 'c':98}
+n = len(myDict)
+print(n)
+print()
+
+###### len(range)
+
+myRange = range(0, 8)
+n = len(myRange)
+print(n)
+print()
+
+###### len(list)
+
+myList = [25, 74, 'hello']
+n = len(myList)
+print(n)
+print()
+
+###### len(bytes)
+
+bytes1 = b'\x00\x10'
+n = len(bytes1)
+print(n)
+print()
+
+###### len(string)
+
+string = 'hello world!'
+n = len(string)
+print(n)
+print()
+
+######
