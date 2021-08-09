322fab0b2c2cfa3ad1fd176bafe71c80f1c6ed0c
maksim-shmat <maksim.shmat@gmail.com>
2021-08-09 06:39:59 +0300

    chore: work with max()

---
+++ python/max_ex.py
@@ -0,0 +1,31 @@
+"""max() about."""
+
+# find maximum of iterable
+
+a = [18, 52, 23, 41, 32]
+largest = max(a)
+print(f'Largest number in the list is : {largest}.')
+print()
+
+###### find maximum of two or more items
+
+largest = max(18, 52, 23, 41, 32)
+print(f'Largest number in the list is : {largest}.')
+print()
+
+###### max() with key function
+
+a = [18, 52, 23, 41, 32]
+keyfunc = lambda x: x % 10
+largest = max(a, key=keyfunc)
+print(f'Number that leaves largest reminder is : {largest}.')
+print()
+
+###### max() with default value
+
+a = []
+largest = max(a, default = 99)
+print(f'Largest number in the list is : {largest}.')
+print()
+
+######
