9860b3edb956a5a025180c45401b2d014408b2bf
maksim-shmat <maksim.shmat@gmail.com>
2021-08-29 16:37:44 +0300

    chore: work with itertools

---
+++ python/itertools_ex.py
@@ -0,0 +1,22 @@
+"""itertools about."""
+
+import itertools as it
+
+# accumulate() - min(11), min(11,3), min(11, 3, 9)...
+zacum = list(it.accumulate([11, 3, 9, 7, 5], func=min))
+print(zacum)
+print()
+
+###### chain() glued several elements to one
+
+fchain = list(it.chain([1, 2, 3], [4, 5], [6]))
+print(fchain)
+print()
+
+###### combinations() - combinations and length of combinations
+
+acombo = list(it.combinations([1, 2, 3], 2))
+print(acombo)
+print()
+
+######
