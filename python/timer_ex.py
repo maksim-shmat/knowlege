c9f6ca82c73f4eeee5375aefbf7facb3eabc2508
maksim-shmat <maksim.shmat@gmail.com>
2021-08-31 13:18:53 +0300

    chore: which script faster?

--- python/timer_ex.py
+++ python/timer_ex.py
@@ -5,3 +5,17 @@ def test(reps, func, *args):
     for i in range(reps):
         func(*args)
     return time.clock() - start
+
+###### which script faster?
+import time
+
+time_start = time.perf_counter()
+
+# YOUR SCRIPT HERE
+
+time_end = time.perf_counter()
+total_time = time_end - time_start
+
+print(f'{total_time:0.2f} second have passed')
+
+######
