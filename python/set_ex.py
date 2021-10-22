447e76d95b77aeed393aab2df76029f03ae6cd6a
maksim-shmat <maksim.shmat@gmail.com>
2021-10-22 10:04:48 +0300

    chore: work with set difference

--- python/set_ex.py
+++ python/set_ex.py
@@ -16,5 +16,22 @@ set_3 = {'d', 'p', 's', 'v'}
 
 set_output = set_1.intersection(set_2).intersection(set_3)
 print(set_output)
+print()
 
-######2
+######2 Set difference
+
+set_1 = {'a', 'b', 'c', 'd'}
+set_2 = {'c', 'd', 'e', 'f'}
+
+set_3 = set_1.difference(set_2)
+print(set_3)
+
+### set difference() method chaining
+
+set_1 = {'a', 'b', 'c', 'd'}
+set_2 = {'c', 'd', 'e', 'f'}
+set_3 = {'a'}
+set_4 = set_1.difference(set_2).difference(set_3)
+print(set_4)
+
+######3
