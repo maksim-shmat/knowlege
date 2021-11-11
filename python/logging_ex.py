f86305de20b1c2d7147316c630013056c599c4b8
maksim-shmat <maksim.shmat@gmail.com>
2021-11-11 06:01:06 +0300

    chore: work with logger

--- python/logging_ex.py
+++ python/logging_ex.py
@@ -59,8 +59,10 @@ class MyFilter(object):
 
 # create a logger
 logger = logging.getLogger('mylogger')
+# set logger level
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler('jill.log')
+# create a logging format
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 # set filter to log only DEBUG lines
@@ -74,4 +76,51 @@ logger.warning('This is a WARNING message')
 logger.error('This is an ERROR message')
 logger.critical('This is a CRITICAL message')
 
-######6
+######6 Log INGO lines
+
+import logging
+
+# create a logger
+logger = logging.getLogger('mylogger')
+# set logger level
+logger.setLevel(logging.INFO)
+# or you can set the following level
+# logger.setLevel(logging.DEBUG)
+
+handler = logging.FileHandler('jill.log')
+# create a logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# write a info message
+logger.info('This is an INFO message')
+
+######7 Log only INFO lines using Python logger
+
+import logging
+
+class MyFilter(object):
+    def __init__(self, level):
+        self.__level = level
+
+    def filter(self, logRecord):
+        return logRecord.levelno <= self.__level
+
+# create a logger
+logger = logging.getLogger('mylogger')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler('jill.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+# set fileter to log only INFO lines
+handler.addFilter(MyFilter(logging.INFO))
+logger.addHandler(handler)
+
+# write an INFO line to log file
+logger.info('This is a INFO message')
+logger.warning('This is a WARNING message')
+logger.error('This is an ERROR message')
+logger.critical('This is a CREITICAL message')
+
+######8 
