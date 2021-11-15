ae2a52d6b6f618e82bfe828e0b6caefeb68106a9
maksim-shmat <maksim.shmat@gmail.com>
2021-11-15 06:01:43 +0300

    chore: work with date formatting

--- python/datetime_ex.py
+++ python/datetime_ex.py
@@ -16,4 +16,35 @@ tz = pytz.timezone('US/Pacific')
 datetime_1 = datetime.now(tz)
 print(str(datetime_1))
 
-######3
+######3 Format Datetime in python
+
+from datetime import datetime
+
+dt = datetime.now()
+
+print(dt)
+print('\nDirectives\n----------------')
+print(dt.strftime('Weekday short version : %a'))
+print(dt.strftime('Weekday full version  : %A'))
+print(dt.strftime('Weekday as a number   : %w'))
+print(dt.strftime('Day of month          : %d'))
+print(dt.strftime('Month Name short ver  : %b'))
+print(dt.strftime('Month Name full ver   : %B'))
+print(dt.strftime('Month as a number     : %m'))
+print(dt.strftime('Year short version    : %y'))
+print(dt.strftime('Year full version     : %Y'))
+print(dt.strftime('Hour (00-23)          : %H'))
+print(dt.strftime('Hour (00-11)          : %I'))
+print(dt.strftime('AM/PM                 : %p'))
+print(dt.strftime('Minute                : %M'))
+print(dt.strftime('Second                : %S'))
+
+print('\nFormatted Date String\n-----------------')
+print(dt.strftime('%a %d-%m-%Y'))
+print(dt.strftime('%a %d/%m/%Y'))
+print(dt.strftime('%a %d/%m/%y'))
+print(dt.strftime('%A %d-%m-%y'))
+print(dt.strftime('%A %d-%m-%Y, %H:%M:%S'))
+print(dt.strftime('%X %x'))
+
+######4
