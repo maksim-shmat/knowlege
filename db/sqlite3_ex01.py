2087d65fa47af06e4b81d30be3cd9832588f6bd4
maksim-shmat <maksim.shmat@gmail.com>
2021-11-23 10:14:47 +0300

    chore: check if table exist in sqlite3 db

--- db/sqlite3_ex01.py
+++ db/sqlite3_ex01.py
@@ -19,4 +19,25 @@ conn.commit()
 # close the connection
 conn.close()
 
-######2
+######2 Check if table exists in sqlite3 database
+
+import sqlite3
+
+conn = sqlite3.connect('mysqlite.db')
+c = conn.cursor()
+
+# get the count of tables with the name
+c.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='students1' ''')  # for check in memory(RAM) change for sqlite_temp_master
+
+# if the count is 1, then table exists
+if c.fetchone()[0]==1 :
+    print('Table exists.')
+else:
+    print('Table does not exist.')
+
+# commit the changes to dg
+conn.commit()
+# close the connection
+conn.close()
+
+######3
