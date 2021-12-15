e2af790a4b03748193c04a69eca06e13a187d4e7
maksim-shmat <maksim.shmat@gmail.com>
2021-12-15 18:42:36 +0300

    chore: work with buffer from xsel

--- unix/xsel.py
+++ unix/xsel.py
@@ -6,6 +6,10 @@ xsel -b
 #2 Clear buffer
 xsel -bc
 
-#3 Write primary buffer into file
+#3 Write the primary buffer into file
 
 xsel -p > message.py.gpg
+
+#4 Write into the primary buffer from file
+
+xsel -b -i < message.asc
