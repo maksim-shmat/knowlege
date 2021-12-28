184a8163901a95073f5d17cc268310663fc0615e
maksim-shmat <maksim.shmat@gmail.com>
2021-12-28 17:04:56 +0300

    chore: make a currnecy converter into Exel/Calc

--- libre/calc_exel.py
+++ libre/calc_exel.py
@@ -3,6 +3,9 @@
 #1 Sum
 =SUM(A1:A4)  # apply from one cell result to another, A1+A2+A3+A4
 
-#2
+#2 Count currency
 
+=A4*VLOOKUP(B4,F4:G11,2,FALSE)
+# A4 = 100, B4 = EUR, D4 = answer, F4-11 = name of money, G4-11 = currency
 
+#3 
