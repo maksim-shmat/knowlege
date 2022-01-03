9280471fc676d428321b14b5d61079f435242be3
maksim-shmat <maksim.shmat@gmail.com>
2022-01-03 11:38:34 +0300

    chore: work with libreoffice macro

---
+++ libre/libre_python_macro.py
@@ -0,0 +1,18 @@
+"""LibreOffice macro from Python about."""
+
+# First, LibreOffice use self Python 3.5.4 (2019 info)
+
+#2 Need Libre Extension APSO (Alternative Script Organizer for Pyton)
+
+- I`m install APSO but it not run. May be need make dir /scripts/ ?
+
+- Then, need:
+sudo apt install libreoffice-script-provider-python
+
+- Profile USER folder, macros available only for USER 
+GNU/Linux
+/home/USER/.config/libreoffice/4/user/Scripts/python
+
+- Directory of LibreOffice, macros available for all users
+GNU/Linux
+/usr/lib/libreoffice/share/Scripts/python/
