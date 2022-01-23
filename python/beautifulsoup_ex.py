c3a54000d8c50d854fb7613899c7e46bd6fcff66
maksim-shmat <maksim.shmat@gmail.com>
2022-01-23 11:01:39 +0300

    chore: work with webscrapping

--- python/beautifulsoup_ex.py
+++ python/beautifulsoup_ex.py
@@ -1,5 +1,7 @@
 """Beautifulsoup4 webscrapping examples."""
 
+# ON the Internet, man!
+
 import bs4 as bs
 import requests
 
@@ -10,7 +12,6 @@ def load_sp500_tickers():
     soup = bs.BeautifulSoup(response.text, 'lxml')
     table = soup.find('table', {'class': 'wikitable sortable'})
     tickers = []
-
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text[:-1]
         tickers.append(ticker)
@@ -18,3 +19,39 @@ def load_sp500_tickers():
     return tickers
 
 # Serializing Tickers
+# IT IS IN A NEW FILE MAN!
+import pickle
+
+with open("sp500tickers.pickle", 'wb') as f:
+    pickle.dump(tickers, f)
+
+# Loading share prices
+
+import os
+import datetime as dt
+import pandas_datareader as web
+
+def load_prices(reload_tickers=False):
+    if reload_tickers:
+        tickers=load_sp500_tickers()
+    else:
+        if os.path.exists('sp500tickers.pickle'):
+            with open('sp500tickers.pickle', 'rb') as f:
+                tickers = pickle.load(f)
+
+        if not os.path.exists('companies'):
+            os.makedirs('companies')
+
+start = dt.datetime(2016, 1, 1)
+end = dt.datetime(2019, 1, 1)
+
+for ticker in tickers:
+    if not os.path.exists('companies/{}.csv'.format(ticker)):
+        print("{} is loading...".format(ticker))
+        df = web.DataReader(ticker, 'yahoo', start, end)
+        df.to_csv('companies/{}.csv'.format(ticker))
+    else:
+        print("{} already downloaded!".format(ticker))
+
+# Compiling data
+
--- python/jill.py
+++
@@ -1,8 +0,0 @@
-yebat cool story bro!
-wow wow
-big sousage
-in your mouth
-sister
-Apple is a very bih company. An apple a day keeps doctor away. A big fat cat
-come across the raad beside doctor`s office.
-The doctor owns apple device.
