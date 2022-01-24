013ad1ea9e8f9a87c81328c796eed78eb1a85358
maksim-shmat <maksim.shmat@gmail.com>
2022-01-24 07:48:20 +0300

    chore: analizes the prices with NumPy

--- python/beautifulsoup_ex.py
+++ python/beautifulsoup_ex.py
@@ -54,4 +54,89 @@ for ticker in tickers:
         print("{} already downloaded!".format(ticker))
 
 # Compiling data
+# take the data out in csv files and combine it to one data frame
 
+with open('sp500tickers.pickle', 'rb') as f:
+    tickers = pickle.load(f)
+
+main_df = pd.DataFrame()
+print("Compiling data...")
+
+for ticker in tickers:
+    df = pd.read_csv('companies/{}.csv'.format(ticker))
+    df.set_index('Date', inplace=True)
+
+df.rename(columns={'Adj Close': ticker}, inplace=True)
+df.drop(['Open', 'High', 'Low', 'Close'], 1, inplace=True)
+
+if main_df.empty:
+    main_df = df
+else:
+    main_df = main_df.join(df, how='outer')
+
+main_df.to_csv('sp500_data.csv')
+print("Data compiled!")
+
+# save our main data frame into a new csv file
+
+load_prices(reload_tickers=True)
+compile_data()
+
+# Visualizing data
+
+sp500 pd.read_csv('sp500_data.csv')
+sp500['MSFT'].plot()
+plt.show()
+
+# Correlations with Pandas corr()
+
+correlation = sp500.corr()
+print(correlation)
+
+# Visualizing correlations with matplotlib
+
+plt.matshow(correlation)
+plt.show()
+
+# Next. Regression Lines
+
+import numpy as np
+
+start = dt.datetime(2016, 1, 1)
+end = dt.datetime(2019, 1, 1)
+
+apple = web.DataReader('AAPL', 'yahoo', start, end)
+data = apple['Adj Close']
+
+x = data.index.map(mdates.date2num)
+
+fit = np.polyfit(x, data.values, 1)
+fit1d = np.poly1w(fit)
+
+plt.grid()
+plt.plot(data.index, data.values, 'b')
+plt.plot(data.index.fit1d(x), 'r')
+plt.show()
+
+# Setting the time frame
+
+rstart = dt.datetime(2018, 7, 1)
+rend = dt.datetime(2019, 1, 1)
+
+fit_data = data.reset_index()
+post1 = fit_data[fit_data.Date >= rstart].index[0]
+post2 = fit_data[fit_data.Date <= rend].index[-1]
+
+fit_data = fit_data.iloc[pos1:pos2]
+
+dates = fit_data.Date.map(mdates.date2num)
+
+fit = np.polyfit(dates, fit_data['Adj Close'], 1)
+fit1d = np.poly1d(fit)
+
+plt.gtid()
+plt.plot(data.index, data.values, 'b')
+plt.plot(fit_data.Date, fit1d(dates), 'r')
+plt.show()
+
+# Next. Predicting Share Prices
