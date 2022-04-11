f5c02f1ca2cb16551b0b21189f394c05aada616e
maksim-shmat <maksim.shmat@gmail.com>
2022-04-11 05:19:21 +0300

    chore: work with kwargs

--- python/func_examples.py
+++ python/func_examples.py
@@ -1,31 +1,50 @@
 """A Functions about."""
 
-# Defining the function
+#1 Defining the function
 
-def func(name):
+def func(name):  # 'name' is parameter
     print("Hi",name)
 
 # Calling the function
+func("Chirag")  # 'Chirag' is argument
 
-func("Chirag")
+#2 Parameters and args
 
-######
+def printMax(a, b):  # a, b is parameters
+    if a > b:
+        print(a, 'max')
+    elif a == b:
+        print(a, 'equals', b)
+    else:
+        print(b, 'max')
 
-# Defining the function for sum of two variables
+printMax(3, 4)  # bring values
+
+x = 5
+y = 7
+
+printMax(x, y)  # variables as arguments
+
+results:
+    4 max
+    7 max
+
+
+#3 Defining the function for sum of two variables
 
 def sum(num1,num2):
     return num1 + num2
 
-# Taking values from the user as an input
+#4 Taking values from the user as an input
 
 num1 = int(input("Enter the value of num1: "))
 num2 = int(input("Enter the value of num2: "))
 
-# Calculating and Printing the sum of num1 and num2
+#5 Calculating and Printing the sum of num1 and num2
 
 print("Sum = ",sum(num1,num2))
 
-###### maximum from the given three numbers
+#6 maximum from the given three numbers
 
 def max_of_two(x, y):
     if x > y:
@@ -37,7 +56,7 @@ def max_of_three(x, y, z):
 
 print(max_of_three(3, 6, -5))
 
-###### calculate the sum of all the numbers present in a list
+#7 calculate the sum of all the numbers present in a list
 
 def sum(numbers):
     total = 0
@@ -46,7 +65,7 @@ def sum(numbers):
     return total
 print(sum((8, 2, 8, 0, 7)))
 
-###### calculate the multiplication of all the numbers present in a list
+#8 calculate the multiplication of all the numbers present in a list
 
 def multiply(numbers):
     total = 1
@@ -56,7 +75,7 @@ def multiply(numbers):
 
 print(multiply((3, 8, 2, -1, 7)))
 
-###### takes a string as an input and calculates the number of upper case
+#9 takes a string as an input and calculates the number of upper case
 #      and lower case letters present in the string
 
 def string_test(string):
@@ -74,7 +93,7 @@ def string_test(string):
 
 string_test('The quick Brown Fox')
 
-###### program that takes a number(non-negative integer) as an argument
+#10 program that takes a number(non-negative integer) as an argument
 #      and calculates its factorial.
 
 def factorial(number):
@@ -86,7 +105,7 @@ def factorial(number):
 number = int(input("Input a number to compute the factorial : "))
 print(factorial(number))
 
-###### 
+#11 
 """Athributes of function in python."""
 
 def func(x):
@@ -110,42 +129,18 @@ print('Final answer is', y)
 print('Show calculations -->', func.optional_return)
 print('Is my function awesome? -->', func.is_awesome)
 
-######
-"""Athributes of function in python."""
+#12 func with default values
 
-def func(x):
-    intermediate_var = x**2 + x + 1
+def say(message, times = 1):  # default may be only end of list of default parameters def func(a, b=6), but not def func(a=5, b)
 
-    if intermediate_var % 2:
-        y = intermediate_var ** 3
-    else:
-        y = intermediate_var ** 3 + 1
+    print(message * times)
+    say('Hello')
+    say('World', 5)
+result:
+    Hello
+    WorldWorldWorldWorldWorld
 
-    # settings attributes here
-    func.optonal_return = intermediate_var
-    func.is_awesome = 'Yes, my function is awesome.'
-
-    return y
-
-y = func(3)
-print('Final answer is', y)
-
-# Accessing function attributes
-print('Show calculations -->', func.optional_return)
-print('Is my function awesome? -->', func.is_awesome)
-
-###### simple function example
-
-def allowed_to_drive(age):
-    if age >= 21:
-        return True
-    else:
-        return False
-
-print(allowed_to_drive(42))
-print(allowed_to_drive(12))
-
-###### 
+# 
 
 def is_laundry_day(today, laundry_day = "Monday", on_vacation = False):
     if today is laundry_day and today != "Sunday" and on_vacation is False:
@@ -157,4 +152,17 @@ print(is_laundry_day("Tuesday"))
 print(is_laundry_day("Tuesday", "Tuesday"))
 print(is_laundry_day("Friday", "Friday", True))
 
-######
+#13 kwargs
+
+def func(a, b=5, c=10):
+    print('a eqwl', a, ', b eqwl', b, ', and c eqwl', c)
+
+func(3,7)
+func(25, c=24)
+func(c=50, a=100)
+results:
+    a eqwl 3, b eqwl 7, and c eqwl 10
+    a eqwl 25, b eqwl 5, and c eqwl 24
+    a eqwl 100, b eqwl 5, and c eqwl 50
+
+
