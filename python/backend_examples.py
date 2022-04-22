eff25cbd198bb623918aedb97e149535496d0fcc
maksim-shmat <maksim.shmat@gmail.com>
2022-04-22 04:44:44 +0300

    chore: work with lists into dict

--- python/backend_examples.py
+++ python/backend_examples.py
@@ -486,7 +486,8 @@ print(list[row])
 sales = {"John": {"N": 3056, "S": 8463, "E": 8441, "W": 2694},
         "Tom": {"N": 4832, "S": 6786, "E": 4737, "W":3612},
         "Anne": {"N": 5239, "S": 4802, "E": 5820, "W": 1859},
-        "Fiona": {"N": 3904, "S": 3645, "E": 8821, "W": 2451}}
+        "Fiona": {"N": 3904, "S": 3645, "E": 8821, "W": 2451},
+        }
 
 # 101
 # Using programm 100, ask the user for a name and a region. Display the 
@@ -497,7 +498,8 @@ sales = {"John": {"N": 3056, "S": 8463, "E": 8441, "W": 2694},
 sales = {"John": {"N": 3056, "S": 8463, "E": 8441, "W": 2694},
         "Tom": {"N": 4832, "S": 6786, "E": 4737, "W": 3612},
         "Anne": {"N": 5239, "S": 4802, "E": 5820, "W": 1859},
-        "Fiona": {"N": 3904, "S": 3645, "E": 8821, "W": 2451}}
+        "Fiona": {"N": 3904, "S": 3645, "E": 8821, "W": 2451},
+        }
 person = input("Enter sales person's name: ")
 region = input("Select region: ")
 print(sales[person][region])
--- python/dict_work.py
+++ python/dict_work.py
@@ -5,8 +5,8 @@
 myDictionary = {
         'a': '65',
         'b': '66',
-        'c': '67'
-}
+        'c': '67',
+        }
 
 myDictionary['d'] = '68'
 myDictionary['e'] = '69'
@@ -21,12 +21,14 @@ dict_salesid = {'SID1': "Fiat",
                 'SID2': "Mersedes",
                 'SID3': "Maruti",
                 'SID4': "Volkswagen",
-                'SID5': "Kia"}
+                'SID5': "Kia",
+                }
 
 dict_salesinfo = {'SID': "Fiat",
                   'Sales': 20000,
                   'LaunchDay': 'Wed',
-                  'Cost': 500000}
+                  'Cost': 500000,
+                  }
 
 print(len(dict_salesid))
 print()
@@ -57,8 +59,8 @@ print()
 myDictionary = {
         'a': '65',
         'b': '66',
-        'c': '67'
-}
+        'c': '67',
+        }
 del myDictionary['c']
 print(myDictionary)
 print('above - delete with del')
@@ -69,21 +71,21 @@ print()
 myDictionary = {
         'a': '65',
         'b': '66',
-        'c': '67'
-}
+        'c': '67',
+        }
 poppedItem = myDictionary.pop('c')
 print(poppedItem)
 print(myDictionary)
 print('above - delete with pop() example')
 print()
 
-### Clear all items of a dictionary
+# Clear all items of a dictionary
 
 myDictionary = {
         "a": 65,
         "b": 66,
-        "c": 67
-}
+        "c": 67,
+        }
 print('Dictionary items:\n', myDictionary)
 myDictionary.clear()
 print('Dictionary items after clear():\n', myDictionary)
@@ -96,16 +98,40 @@ print(dict_keys)
 print(type(dict_keys))
 print()
 
-### and more
+# and more
 for var in dict_keys:
     print(var + ": " + str(dict_salesinfo[var]))
     print()
 
-#4 printing dictionary object values in key, value pair
+#4 print dictionary object values in key, value pair
 
 dict_values = dict_salesinfo.values()
 print(dict_values)
 print()
+
+or  # with dublicates
+
+favorite_languages = {
+        'jen': 'python',
+        'sarah': 'c',
+        }
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+results:
+    The following languages have been mentioned:
+        Python
+        C
+
+or  # without dublicates in dict, use set
+
+favorite_languages = {
+        'jen': 'python',
+        'jen': 'python',
+        }
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
 
 #5 the item function converts a dictionary item into a tuple
 
@@ -140,8 +166,13 @@ print()
 
 d = {'apples':40, 'oranges':80, 'bananas':70}
 print(sorted(d, key=d.get))
-
-print()
+or
+for fruits in sorted(d.keys()):
+    print(f"{fruits.title()}, thank you for having me!")
+results:
+    Apples, thank you for having me!
+    Oranges, thank you for having me!
+    Bananas, thank you for having me!
 
 #10 generator dict and set
 
@@ -152,13 +183,13 @@ print(D)
 print()
 
 #11 Python dictionary methods
-### clear()
+# clear()
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 dictionary.clear()
 print(dictionary)
 
-### copy()
+# copy()
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 dictionary_1 = dictionary.copy()
@@ -176,7 +207,7 @@ d['names'].append('Clive')
 print(c)  # {'names': ['Alfred', 'Bertrand', 'Clive']}
 print(dc)  # {'names': ['Alfred', 'Bertrand']}
 
-### fromkeys()
+# fromkeys()
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 dictionary_1 = dict.fromkeys(dictionary, 1)
@@ -185,7 +216,7 @@ print(dictionary_1)  # default value None
 # can use your own default value
 # dict.fromkeys(['name', 'age'], '(unknown_artist)')
 
-### get()
+# get()
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 x = dictionary.get("b")
@@ -195,19 +226,19 @@ print(x)
 # if need your own defaul
 # d.get('name', 'N/A')  # key 'name' - value 'N/A'
 
-### items()
+# items()
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 for key, value in dictionary.items():
     print(key, '-', value)  # or print(key)
 
-### keys()
+# keys()
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 for key in dictionary.keys():
     print(key)
 
-### values()
+# values()
 
 dictonary = {"a": 4, "b": 5, "c": 6}
 for value in dictionary.values():
@@ -220,21 +251,21 @@ d[3] = 3
 d[4] = 1
 d.values()  # dict_values([1,2,3,2])
 
-### pop() removes the key-value pair
+# pop() removes the key-value pair
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 x = dictionary.pop("b")
 print(x)
 print(dictionary)
 
-### popitem() removes the last inserted key-value pair
+# popitem() removes the last inserted key-value pair
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 x = dictionary.popitem()
 print(x)
 print(dictionary)
 
-### setdefault()
+# setdefault()
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 x = dictionary.setdefault("b")
@@ -251,7 +282,7 @@ d['name'] = 'Bumby'
 d.stdefault('name', 'N/A')  # 'Gumby'
 print(d)  # {'name': 'Gumby'}
 
-### update(), added to old one
+# update(), added to old one
 
 dictionary = {"a": 4, "b": 5, "c": 6}
 dictionary_1 = {"a": 8, "m": 2, "v": 7}
@@ -269,13 +300,13 @@ myDict = {x: someThing(x) for x in (5, 8, 9, 12)}
 print(type(myDict))
 print(myDict)
 
-### create dictionary with keyword argument to dict()
+# create dictionary with keyword argument to dict()
 
 myDict = dict(a='foo', b='bar', c='moo')
 print(type(myDict))
 print(myDict)
 
-### create dictionary using dict() constructor
+# create dictionary using dict() constructor
 
 myDict = dict([(1, 'foo'), (2, 'bar'), (3, 'moo')])
 print(type(myDict))
@@ -290,7 +321,7 @@ if not myDict:
 else:
     print('The dictionary is not empty.')
 
-### check if dictionary is empty using len()
+# check if dictionary is empty using len()
 
 myDict = {}
 if (len(myDict)) == 0:
@@ -304,7 +335,8 @@ print('    next')
 myDictionary = {
         "name": "Luna",
         "year": 1989,
-        "expertise": "data expeliarmos"}
+        "expertise": "data expeliarmos",
+        }
 isPresent = 'expertise' in myDictionary
 print(isPresent)
 print('    next')
@@ -314,33 +346,14 @@ print('    next')
 myDictionary = {
         "name": "Lini",
         "year": 1989,
-        "expertise": "data analytics"}
+        "expertise": "data analytics",
+        }
 length = len(myDictionary)
 print('Length of dictionary is: ', length)
 print('    next')
 
 #16 List of dictionaries
-### Create
-
-myList = [
-        {
-            'foo': 12,
-            'bar': 14
-        },
-        {
-            'moo': 52,
-            'car': 642
-        },
-        {
-            'doo': 6,
-            'tar': 84
-        }
-]
-print(myList)
-print(myList[0])
-print(myList[0]['bar'])
-
-### Update
+# Create
 
 myList = [
         {
@@ -349,11 +362,31 @@ myList = [
         },
         {
             'moo': 52,
-            'car': 642
+            'car': 642,
         },
         {
             'doo': 6,
-            'tar': 84
+            'tar': 84,
+        }
+]
+print(myList)
+print(myList[0])
+print(myList[0]['bar'])
+
+# Update
+
+myList = [
+        {
+            'foo': 12,
+            'bar': 14,
+        },
+        {
+            'moo': 52,
+            'car': 642,
+        },
+        {
+            'doo': 6,
+            'tar': 84,
         }
 ]
 # update value for 'bar in first dict
@@ -366,25 +399,25 @@ myList[1]['gar'] = 38
 del myList[2]['doo']
 print(myList)
 
-### Append a dict to list of dict
+# Append a dict to list of dict
 myList.append({'joo': 48, 'par': 28})
 print(myList)
 print('next')
 
 #17 Dictionary keys to list
-### using dict.keys()
+# using dict.keys()
 
 myDict = {'a': 'apple', 'b': 'banana', 'c': 'cherry'}
 keysList = list(myDict.keys())
 print(keysList)
 
-### using list comprehension
+# using list comprehension
 
 myDict1 = {'a': 'apple', 'b': 'banana', 'c': 'cherry'}
 keysList1 = [key for key in myDict1]
 print(keysList)
 
-### using for loop
+# using for loop
 
 myDict2 = {'a': 'apple', 'b': 'banana', 'c': 'cherry'}
 keysList2 = []
@@ -394,19 +427,19 @@ print(keysList2)
 print('   ^ above dict keys to list')
 
 #18 Dictionary values to list
-### Using dict.values()
+# Using dict.values()
 
 myDict3 = {'a': 'apple', 'b': 'banana', 'c': 'cherry'}
 valuesList = list(myDict3.values())
 print(valuesList)
 
-### using list comprehension
+# using list comprehension
 
 myDict4 = {'a': 'apple', 'b': 'banana', 'c': 'cherry'}
 valuesList1 = [myDict4[key] for key in myDict4]
 print(valuesList1)
 
-### using for loop
+# using for loop
 
 myDict5 = {'a': 'apple', 'b': 'banana', 'c': 'cherry'}
 valuesList2 = []
@@ -415,4 +448,71 @@ for key in myDict5:
 print(valuesList2)
 print('   ^ above dict values to list')
 
-#19 
+#19 print all key-value
+
+user_0 = {
+        'username': 'efic',
+        'first': 'epusa',
+        'last': 'rufl',
+        }
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
+results:
+    Key:last
+    Value: rufl
+
+    Key: first
+    Value: epusa
+
+    Key: username
+    Value: efic
+
+#20 Search in dict target keys and print it with value
+
+favorite_languages = {
+        'jen': 'puppsi',
+        'zarah': 'sibr',
+        'endbart': 'bubi',
+        'pkealis': 'phearphear',
+        }
+friends = ['jen', 'zarah']
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+results:
+    Hi Jen
+        Jen, I see you love Puppsi!
+    Hi Zarah
+        Zarah, I see you love Sibr!
+    Hi Endbard
+    Hi Pkealis
+
+#21 Check if man not include in dict
+
+favorite_languages = {
+        'jen': 'puppsi',
+        'zarah': 'sibr',
+        'endbart': 'bubi',
+        'pkealis': 'phearphear',
+        }
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+#22 How write long line - with quotes!
+# And work with list into dict
+
+pizza = {
+        'crust': 'thick',
+        'toppings': ['mushrooms', 'extra cheese'],
+        }
+print(f"You ordered a {pizza['crust']}-crust pizza "  # quote one
+        "with the following toppings:")  # quote two for sum line
+for topping in pizza['toppings']:
+    print("\t" + topping)
+
+#23
