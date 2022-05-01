407cef04afd4c4da3de3ab2db85c78adaa1b6508
maksim-shmat <maksim.shmat@gmail.com>
2022-05-01 15:12:55 +0300

    feat: add moving pic object to left/right in a pygame field

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -17,7 +17,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
                 (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.ship = Ship(screen)  # ! Not attribute screen. And where it is?
+        self.ship = Ship(self.screen)
 
         # Set background color. Default is black.
        # self.bg_color = (230, 230, 230)  # light gray color
@@ -29,6 +29,7 @@ class AlienInvasion:
         """Start game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -36,6 +37,19 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                   #self.ship.rect.x += 1  # for one pixel to right
+                   self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                #   self.ship.rect.x -= 1  # for one pixel to left
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update rectangle and show new screen."""
--- python/alien_invasion/ship.py
+++ python/alien_invasion/ship.py
@@ -7,8 +7,10 @@ class Ship():
 
     def __init__(self, ai_game):
         """Init ship and make a start position."""
-        self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+        self.screen = ai_game
+        self.screen_rect = ai_game.get_rect()
+        self.moving_right = False
+        self.moving_left = False
 
         # Load image of ship and get the rectangle.
         self.image = pygame.image.load('/home/jack/django2/knowlege/python/alien_invasion/images/ship.png')
@@ -16,6 +18,13 @@ class Ship():
 
         # Every new ship will be appeared in a bottom line of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+    def update(self):
+        """Update the ship position with True/False triger."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Design the ship in a current position."""
