5892fde57ef66c32d305245a60b28448e33b9351
maksim-shmat <maksim.shmat@gmail.com>
2022-05-29 06:36:23 +0300

    chore: make a restriction views of themes for not owners.

--- django/learning_log/learning_logs/models.py
+++ django/learning_log/learning_logs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class Topic(models.Model):
     """Theme which user is study e.g."""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return string value of model."""
