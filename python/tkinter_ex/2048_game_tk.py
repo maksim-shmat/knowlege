b34b0d3f0e8a7db17531d184c8c518a43bf3df96
maksim-shmat <maksim.shmat@gmail.com>
2022-06-07 05:13:23 +0300

    refactor: 2048_game_tk

--- python/tkinter_ex/2048_game_tk.py
+++ python/tkinter_ex/2048_game_tk.py
@@ -37,7 +37,7 @@ class Board:
                     [self.bd[12], self.bd[13], self.bd[14], self.bd[15]]]
         else:
             # generate transposed 4x4 2d array instead:
-            rows = [[self.db[0], self.bd[4], self.bd[8], self.bd[12]],
+            rows = [[self.bd[0], self.bd[4], self.bd[8], self.bd[12]],
                     [self.bd[1], self.bd[5], self.bd[9], self.bd[13]],
                     [self.bd[2], self.bd[6], self.bd[10], self.bd[14]],
                     [self.bd[3], self.bd[7], self.bd[11], self.bd[15]]]
@@ -48,7 +48,7 @@ class Board:
             if key in 'RD':
                 # reverse these rows and slide to left:
                 row = row[::-1]
-            nrow = self.slide_square(row)
+            nrow = self.slide_squares(row)
             if key in 'RD':
                 # restore reversed rows:
                 nrow = nrow[::-1]
@@ -82,9 +82,7 @@ class Board:
         inew = 0
         for x in row:
             if x:
-                if (inew > 0 and
-                    x == new[inew-1] and
-                    icmb != inew-1):
+                if (inew > 0 and x == new[inew-1] and icmb != inew-1):
                     new[inew-1] = str(2*int(x))
                     icmb = inew-1
                 else:
@@ -112,7 +110,7 @@ class Game:
         self.window = gw
 
         self.rosybrown1 = '#ffc1c1'
-        self.lenonchiffon = '#fffacd'
+        self.lemonchiffon = '#fffacd'
         self.skyblue1 = '#87ceff'
         self.springgreen = '#00ff7f'
         self.tomato1 = '#ff6347'
@@ -144,7 +142,143 @@ class Game:
 
         # game data:
         self.bd = None
+        self.playable = False
 
+        # top frame:
+        self.top_fr = Frame(gw,
+                            width=600,
+                            height=100,
+                            bg=self.springgreen)
+        self.top_fr.pack(fill=X)
 
-        
+        self.hdg = Label(self.top_fr,
+                         text='  2048  ',
+                         font='arial 22 bold',
+                         fg=self.navyblue,
+                         bg=self.white)
+        self.hdg.place(relx=0.5, rely=0.4,
+                anchor=CENTER)
 
+        self.dir = Label(self.top_fr,
+                text="(Select a 'Goal' & Click 'New Game')",
+                font='arial 12',
+                fg=self.navyblue,
+                bg=self.springgreen)
+        self.dir.place(relx=0.5, rely=0.8,
+                anchor=CENTER)
+
+        self.play_btn = Button(self.top_fr,
+                               text='New \nGame',
+                               bd=5,
+                               bg=self.cobaltgreen,
+                               fg=self.white,
+                               font='times 12 bold',
+                               command=self.new_game)
+        self.play_btn.place(relx=0.92, rely=0.5,
+                anchor=E)
+
+        self.lbl_cb = Label(self.top_fr,
+                            text='    Goal',
+                            font='arial 12 bold',
+                            fg=self.darkgreen,
+                            bg=self.springgreen)
+        self.lbl_cb.place(relx=0.08, rely=0.35,
+                anchor=W)
+
+        goals = ['2048', '1024', '512', '256', '128']
+        self.cur_goal = StringVar()
+        self.goal_cb = ttk.Combobox(self.top_fr,
+                                    foreground=self.darkgreen,
+                                    values=goals,
+                                    font='times 12 bold',
+                                    justify='left',
+                                    state='readonly',
+                                    textvariable=self.cur_goal,
+                                    width=7,
+                                    height=30)
+        self.goal_cb.place(relx=0.08, rely=0.6,
+                anchor=W)
+        self.goal_cb.current(0)
+
+        # bottom frame:
+        self.btm_fr = Frame(gw,
+                            width=600,
+                            height=500,
+                            bg=self.lightsteelblue)
+        self.btm_fr.pack(fill=X)
+
+        # board frame:
+        self.bd_fr = Frame(self.btm_fr,
+                           width=400+2,
+                           height=400+2,
+                           relief='solid',
+                           bd=1,
+                           bg=self.lemonchiffon)
+        self.bd_fr.place(relx=0.5, rely=0.5,
+                anchor=CENTER)
+        self.bd = Board()
+        self.play_game()
+
+    # action to take if 'new game' button is clicked
+    # or if 'play again' is chosen after win or loss:
+    def new_game(self):
+        self.playable = True
+        self.bd = Board()
+        self.bd.place_two()
+        self.bd.goal = self.goal_cb.get()
+        self.goal_cb.config(state='disabled')
+        self.dir.config(text='(Use arrow keys to play game)')
+        self.play_game()
+
+    # show current contentes of board:
+    def play_game(self):
+        objh = 100  # widget height
+        objw = 100  # widget width
+        objx = 0  # x-position of widget in frame
+        objy = 0  # y-position of widget in frame
+
+        i = 0
+        for r in range(4):
+            for c in range(4):
+                txt = self.bd.get_text(i)
+                bg_color = self.doc[txt]
+                game_sq = Label(self.bd_fr,
+                                text=txt,
+                                relief='solid',
+                                bd=1,
+                                fg=self.black,
+                                bg=bg_color,
+                                font='times 16 bold')
+                game_sq.place(x=objx, y=objy,
+                        height=objh, width=objw)
+                i += 1
+                objx = objx = objw
+            objx = 0
+            objy = objy + objh
+
+    # control play when an arrow key is pressed:
+    def key(self, event):
+        if event.keysym in ('Left', 'Right', 'Up', 'Down'):
+            if self.playable:
+                self.bd.move_squares(event.keysym[0])
+                self.play_game()
+                x = self.bd.is_end()
+                if x == 'C':
+                    return
+                elif x == 'W':
+                    msg = 'You won!!!    Play again?'
+                elif x == 'L':
+                    msg = 'You lost!!!    Play again?'
+                ans = messagebox.askquestion(msg)
+                if ans == 'no':
+                    self.window.destroy()
+                else:
+                    self.new_game()
+
+root = Tk()
+root.title('2048')
+root.geometry('600x600+100+50')
+root.resizable(False, False)
+g = Game(root)
+root.bind_all('<Key>', g.key)
+root.mainloop()
