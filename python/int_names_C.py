85be814d9b6923d98c1cf21391928f9e786178df
maksim-shmat <maksim.shmat@gmail.com>
2022-06-22 04:28:20 +0300

    feat: make pyramid of integers for names

---
+++ python/int_names_C.py
@@ -0,0 +1,38 @@
+"""Pyramid integers for names with C style."""
+
+def partitions(n):
+    partitions.p.append(0)
+
+    for k in range(1, n + 1):
+        d = n - k * (3 * k - 1) // 2
+        if d < 0:
+            break
+
+        if k & 1:
+            partitions.p[n] += partitions.p[d]
+        else:
+            partitions.p[n] -= partitions.p[d]
+
+        d -= k
+        if d < 0:
+            break
+
+        if k & 1:
+            partitions.p[n] += partitions.p[d]
+        else:
+            partitions.p[n] -= partitions.p[d]
+    return partitions.p[-1]
+
+partitions.p = [1]
+
+def main():
+    ns = set([23, 123, 1234, 12345])
+    max_ns = max(ns)
+
+    for i in range(1, max_ns + 1):
+        if i > max_ns:
+            break
+        p = partitions(i)
+        if i in ns:
+            print("%6d: %s" % (i, p))
+main()
