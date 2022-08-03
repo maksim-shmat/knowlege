e0febb6e0fae33d214ddeaaa76ae8820488c3895
maksim-shmat <maksim.shmat@gmail.com>
2022-08-03 05:03:27 +0300

    chore: work with anagrams

--- python/anagramm_check.py
+++ python/anagramm_check.py
@@ -1,8 +1,38 @@
-"""Check anagramms."""
+"""Check anagrams."""
 
+"""
 from collections import Counter
 
 str1 = 'proglib'
 str2 = 'proglib'
 
 print(Counter(str1) == Counter(str2))
+
+#2 Using default dict
+
+import urllib.request
+from collections import defaultdict
+
+#words = urllib.request.urlopen('http://wiki.puzzlers.org/pub/wordlists/unixdict.txt').read().split()
+words_file = open('unixdict.txt', 'r')
+text = words_file.read()
+words_file.close()
+
+text = text.lower()
+words = text.split()
+print(len(words))
+
+anagram = defaultdict(list)  # map sorted chars to anagrams
+for word in words:
+    anagram[tuple(sorted(word))].append(word)
+
+count = max(len(ana) for ana in anagram.values())
+for ana in anagram.values():
+    if len(ana) >= count:
+        print([x.decode() for x in ana])
+
+#error: decode() not str method
+"""
+
+# Using groupby
+
