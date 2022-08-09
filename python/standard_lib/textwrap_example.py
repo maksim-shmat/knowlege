8442fa833344d64ef6054f8d7514ac461617d76c
maksim-shmat <maksim.shmat@gmail.com>
2022-08-09 05:15:32 +0300

    chore: work with text for indent(), dedent() functions

---
+++ python/standard_lib/textwrap_example.py
@@ -0,0 +1,8 @@
+"""This file contain text for examples of work other functions."""
+
+sample_text = """
+    The textwrap module can be used to format text for output in
+    situations where pretty-printing is desired. It offers
+    programmatic functionality similar to the paragraph wrapping
+    or filling features found in many text editors.
+    """
---
+++ python/standard_lib/textwrap_indent_predicate.py
@@ -0,0 +1,7 @@
+"""File for work with indent()."""
+
+Indent 'The textwrap module can be used to format text\n'?
+Indent 'for output in situations where pretty-printing is\n'?
+Indent 'desired. It offers programmatic functionally\n'?
+Indent 'similar to the paragraph wrapping or filling\n'?
+Indent 'features found in many text editors.'?
