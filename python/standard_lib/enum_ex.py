58aa4f893fd6e02faea7645f9744cbefd9a23dd7
maksim-shmat <maksim.shmat@gmail.com>
2022-08-27 19:34:08 +0300

    chore: work with enum.IntEnum

--- python/standard_lib/enum_ex.py
+++ python/standard_lib/enum_ex.py
@@ -1,7 +1,6 @@
 """enum() about."""
 
 #1 enum create
-
 import enum
 
 class BugStatus(enum.Enum):
@@ -16,12 +15,10 @@ class BugStatus(enum.Enum):
 
 print('\nMember name: {}'.format(BugStatus.wont_fix.name))
 print('Member value: {}'.format(BugStatus.wont_fix.value))
-
 '''RESULTS:
 Member name: wont_fix
 Member value: 4
 '''
-
 #2 enum iterate
 
 incomplete = 6
@@ -45,8 +42,7 @@ fix_released    = 1
 '''
 
 #3 enum comparison
-
-import enum
+#import enum
 
 class BugStatus(enum.Enum):
 
@@ -79,10 +75,61 @@ except TypeError as err:
 Identity: False True
 Ordered by value:
   Cannot sort: '<' not supported between instances of 'BugStatus' and 'BugStatus'
-'''
 
 Equality: False True
 Identity: False True
 Ordered by value:
   Cannot sort: '<' not supported between instances of 'BugStatus' and 'BugStatus'
-#4
+'''
+#4 IntEnum for integer elements
+import enum
+
+class BugStatus(enum.IntEnum):
+    new = 7
+    incomplete = 6
+    invalid = 5
+    wont_fix = 4
+    in_progress = 3
+    fix_committed = 2
+    fix_released = 1
+
+print('Ordered by value:')
+print('\n'.join('  ' + s.name for s in sorted(BugStatus)))
+
+'''RESULTS:
+    Ordered by value:
+  fix_released
+  fix_committed
+  in_progress
+  wont_fix
+  invalid
+  incomplete
+  new
+'''
+
+#5 enum pragrammatic create
+
+import enum
+
+BugStatus = enum.Enum(
+        value = 'BugStatus',
+        names = ('fix_released fix_committed in_progress '
+            'wont_fix invalid incomplete new'),
+)
+
+print('Member: {}'.format(BugStatus.new))
+for status in BugStatus:
+    print('{:15} = {}'.format(status.name, status.value))
+
+'''RESULTS:
+    Member: BugStatus.new
+fix_released    = 1
+fix_committed   = 2
+in_progress     = 3
+wont_fix        = 4
+invalid         = 5
+incomplete      = 6
+new             = 7
+'''
+
+#6 
