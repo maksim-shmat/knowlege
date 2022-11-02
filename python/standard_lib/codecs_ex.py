fee91e139a6b7c247e9ca3099f6cef755d03bbcc
maksim-shmat <maksim.shmat@gmail.com>
2022-11-02 04:52:25 +0300

    chore: work with change caps for codecs

--- python/standard_lib/codecs_ex.py
+++ python/standard_lib/codecs_ex.py
@@ -236,4 +236,65 @@ Decoding: ..........
 Total uncompressed length: 1350
 '''
 
-#10 
+#10  invertcaps (don't effective example)
+
+import string
+
+def invertcaps(text):
+    """Return new string with new form of caps."""
+    return ''.join(
+            c.upper() if c in string.ascii_lowercase
+            else c.lower() if c in string.ascii_uppercase
+            else c
+            for c in text
+    )
+
+if __name__ == '__main__':
+    print(invertcaps('ABCdef'))
+    print(invertcaps('abcDEF'))
+
+'''RESULTS:
+abcDEF
+ABCdef
+'''
+
+#11 invertcaps charmap
+
+import codecs
+import string
+
+decoding_map = codecs.make_identity_dict(range(256))
+
+pairs = list(zip(
+    [ord(c) for c in string.ascii_lowercase],
+    [ord(c) for c in string.ascii_uppercase],
+))
+
+decoding_map.update({
+    upper: lower
+    for (lower, upper)
+    in pairs
+})
+
+decoding_map.update({
+    lower: upper
+    for (lower, upper)
+    in pairs
+})
+
+encoding_map = codecs.make_encoding_map(decoding_map)
+
+if __name__ == '__main__':
+    print(codecs.charmap_encode('abcDEF', 'strict',
+                                encoding_map))
+    print(codecs.charmap_decode(b'abcDEF', 'strict',
+                                decoding_map))
+    print(encoding_map == decoding_map)
+
+'''RESULTS:
+(b'ABCdef', 6)
+('ABCdef', 6)
+True
+'''
+
+#12
