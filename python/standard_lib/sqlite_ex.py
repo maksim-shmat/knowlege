0d9983a1cae7c282a568946d6e159e83a879292b
maksim-shmat <maksim.shmat@gmail.com>
2022-11-09 05:00:29 +0300

    chore: work with methods against sql_injections

--- python/standard_lib/sqlite_ex.py
+++ python/standard_lib/sqlite_ex.py
@@ -133,4 +133,115 @@ Next 5 tasks:
  3 [1] write about sqlite3       [active  ] (2017-07-31)
 '''
 
-#4 
+#4 cursor description
+
+import sqlite3
+
+db_filename = 'todo.db'
+
+with sqlite3.connect(db_filename) as conn:
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    select * from task where project = 'pymotw'
+    """)
+
+    print('Task table has these columns:')
+    for colinfo in cursor.description:
+        print(colinfo)
+
+'''RESULTS:
+Task table has these columns:
+('id', None, None, None, None, None, None)
+('priority', None, None, None, None, None, None)
+('details', None, None, None, None, None, None)
+('status', None, None, None, None, None, None)
+('deadline', None, None, None, None, None, None)
+('completed_on', None, None, None, None, None, None)
+('project', None, None, None, None, None, None)
+'''
+
+#5 positional arguments - <?>, for safe
+
+'''
+import sqlite3
+import sys
+
+db_filename = 'todo.db'
+project_name = sys.argv[1]
+
+with sqlite3.connect(db_filename) as conn:
+    cursor = conn.cursor()
+
+    query = """
+    select id, priority, details, status, deadline from task
+    where project = ?
+    """
+    cursor.execute(query, (project_name,))
+
+    for row in cursor.fetchall():
+        task_id, priority, details, status, deadline = row
+        print('{:2d} [{:d}] {:<25} [{:<8}] ({})'.format(
+            task_id, priority, details, status, deadline))
+
+EXPECTED RESULTS: After write in CLI: python3 sqlite_ex.py pymotw
+1 [1] write about select    [done    ] (2011-04-27)
+2 [1] write about random    [waiting ] (2016-08-22)
+3 [1] write about sqlite3
+'''
+
+#6 named arguments - <:>, for safe
+
+'''
+import sqlite3
+import sys
+
+db_filename = 'todo.db'
+project_name = sys.argv[1]
+
+with sqlite3.connect(db_filename) as conn:
+    cursor = conn.cursor()
+
+    query = """
+    selct id, priority, details, status, deadline from task
+    where project = :project_name
+    order by deadline, priority
+    """
+
+    cursor.execute(query, {'project_name': project_name})
+
+    for row in cursor.fetchall():
+        task_id, priority, details, status, deadline = row
+        print('{:2d} [{:d}] {:<25} [{:<8}] ({})'.format(
+            task_id, priority, details, status, deadline))
+
+EXPECTED RESULTS: Afer write in CLI: $ python3 sqlite_ex.py pymotw
+1 [1] write about select    [done    ] (2011-04-27)
+2 [1] write about random    [waiting ] (2016-08-22)
+3 [1] write about sqlite3
+'''
+
+#7  update argument
+
+'''
+import sqlite3
+import sys
+
+db_filename = 'todo.db'
+id = int(sys.argv[1])
+status = sys.argv[2]
+
+with sqlite3.connect(db_filename) as conn:
+    cursor = conn.cursor()
+    query = "update task set status = :status where id = :id"
+    cursor.execute(query, {'status': status, 'id': id})
+
+EXPEDTED RESULTS: write: $ python3 sqlite_ex.py 2 done
+                         $ python3 sqlite_ex.py pymotw
+
+1 [1] write about select    [done    ] (2011-04-27)
+2 [1] write about random    [waiting ] (2016-08-22)
+3 [1] write about sqlite3
+'''
+
+#8 executemany()
