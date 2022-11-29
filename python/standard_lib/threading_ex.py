f01b36ac400532ccb1239386473007142ab9e892
maksim-shmat <maksim.shmat@gmail.com>
2022-11-29 04:58:52 +0300

    chore: work with barrier for threading

--- python/standard_lib/threading_ex.py
+++ python/standard_lib/threading_ex.py
@@ -563,9 +563,57 @@ nw = threading.Thread(target=worker_no_with, args=(lock,))
 w.start()
 nw.start()
 
-'''EXPECTED RESULTS:
+''EXPECTED RESULTS:
 (Thread-1  ) Lock acquired via with
 (Thread-2  ) Lock acquired directly
 '''
 
 #16 threading condition
+
+import logging
+import threading
+import time
+
+
+def consumer(cond):
+    """Wait and then use resource."""
+    logging.debug('Starting consumer thread')
+    with cond:
+        cond.wait()
+        logging.debug('Resource is available to consumer')
+
+
+def producer(cond):
+    """Set resource for use."""
+    logging.debug('Starting producer thread')
+    with cond:
+        logging.debug('Making resource available')
+        cond.notifyAll()
+
+logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s (%threadName)-2a) %(message)s',
+)
+
+condition = threading.Condition()
+c1 = threading.Thread(name='c1', target=consumer,
+        args=(condition,))
+c2 = threading.Thread(name='c2', target=consumer,
+        args=(condition,))
+p = threading.Thread(name='p', target=producer,
+        args=(condition,))
+
+c1.start()
+time.sleep(0.2)
+c2.start()
+time.sleep(0.2)
+
+'''EXPECTED RESULTS:
+2016-07-10 10:45:28,170 (c1) Starting consumer thread
+2016-07-10 10:45:28,376 (c2) Starting consumer thread
+2016-07-10 10:45:28,581 (p) Starting producer thread
+2016-07-10 10:45:28,581 (p) Makeing resource available
+                    582 (c1) Resource is available to consumer
+                    582 (c2) Resource is available to consumer
+'''
+
