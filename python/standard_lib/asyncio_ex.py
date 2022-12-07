149e54d180a5aa3b897b6ced3338ec3b7995a613
maksim-shmat <maksim.shmat@gmail.com>
2022-12-07 04:56:38 +0300

    chore: work with Future from asyncio

--- python/standard_lib/asyncio_ex.py
+++ python/standard_lib/asyncio_ex.py
@@ -139,4 +139,155 @@ in phase2
 return value: ('result1', 'result2 derived from result1')
 '''
 
-#5
+#5 asyncio call soon
+
+import asyncio
+import functools
+
+'''
+def callback(arg, *, kwarg='default'):
+    print('callback invoked with {} and {}'.format(arg, kwarg))
+
+
+async def main(loop):
+    print('registering callbacks')
+    loop.call_soon(callback, 1)
+    wrapped = functools.partial(callback, kwarg='not default')
+    loop.call_soon(wrapped, 2)
+
+    await asyncio.sleep(0.1)
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    print('entering event loop')
+    event_loop.run_until_complete(main(event_loop))
+finally:
+    print('closing event loop')
+    event_loop.close()
+
+RESULTS:
+<stdin>:161: DeprecationWarning: There is no current event loop
+entering event loop
+registering callbacks
+callback invoked with 1 and default
+callback invoked with 2 and not default
+closing event loop
+'''
+
+#6 asyncio call later
+
+import asyncio
+
+'''
+def callback(n):
+    print('callback {} invoked'.format(n))
+
+
+async def main(loop):
+    print('registering callbacks')
+    loop.call_later(0.2, callback, 1)
+    loop.call_later(0.1, callback, 2)
+    loop.call_soon(callback, 3)
+
+    await asyncio.sleep(0.4)
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    print('entering event loop')
+    event_loop.run_until_complete(main(event_loop))
+finally:
+    print('closing event loop')
+    event_loop.close()
+
+RESULTS:
+entering event loop
+registering callbacks
+callback 3 invoked
+callback 2 invoked
+callback 1 invoked
+closing event loop
+'''
+
+#7 asyncio call at global clock
+
+import asyncio
+import time
+
+'''
+def callback(n, loop):
+    print('callback {} invoked at {}'.format(n, loop.time()))
+
+
+async def main(loop):
+    now = loop.time()
+    print('clock time: {}'.format(time.time()))
+    print('loop time: {}'.format(now))
+
+    print('registering callbacks')
+    loop.call_at(now + 0.2, callback, 1, loop)
+    loop.call_at(now + 0.1, callback, 2, loop)
+    loop.call_soon(callback, 3, loop)
+
+    await asyncio.sleep(1)
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    print('entering event loop')
+    event_loop.run_until_complete(main(event_loop))
+finally:
+    print('closing event loop')
+    event_loop.close()
+
+RESULTS:
+<stdin>:236: DeprecationWarning: There is no current event loop
+entering event loop
+clock time: 1670377563.727475
+loop time: 256750.725657456
+registering callbacks
+callback 3 invoked at 256750.725708036
+callback 2 invoked at 256750.82597814
+callback 1 invoked at 256750.926417026
+closing event loop
+'''
+
+#8 asyncio <Future> event loop
+
+import asyncio
+
+'''
+def mark_done(future, result):
+    print('setting future result to {!r}'.format(result))
+    future.set_result(result)
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    all_done = asyncio.Future()
+
+    print('scheduling mark_done')
+    event_loop.call_soon(mark_done, all_done, 'the result')
+
+    print('entering event loop')
+    result = event_loop.run_until_complete(all_done)
+    print('returned result: {!r}'.format(result))
+finally:
+    print('closing event loop')
+    event_loop.close()
+
+print('future result: {!r}'.format(all_done.result()))
+
+RESULTS:
+<stdin>:266: DeprecationWarning: There is no current event loop
+<stdin>:268: DeprecationWarning: There is no current event loop
+scheduling mark_done
+entering event loop
+setting future result to 'the result'
+returned result: 'the result'
+closing event loop
+future result: 'the result'
+'''
+
+#9
