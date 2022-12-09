42b5d94890dc3a8832a7766b1e85ef79ec29a0bb
maksim-shmat <maksim.shmat@gmail.com>
2022-12-09 05:00:32 +0300

    chore: work with asyncio lock()

--- python/standard_lib/asyncio_ex.py
+++ python/standard_lib/asyncio_ex.py
@@ -578,4 +578,226 @@ done with phase 2
 results ['phase 1 result', 'phase 0 result', 'phase 2 result']
 '''
 
-#16
+#16 wait() with timeout for don't completed tasks
+
+import asyncio
+
+'''
+async def phase(i):
+    print('in phase {}'.format(i))
+    try:
+        await asyncio.sleep(0.1 * i)
+    except asyncio.CancelledError:
+        print('phase {} canceled'.format(i))
+        raise
+    else:
+        print('done with phase {}'.format(i))
+        return 'phase {} result'.format(i)
+
+
+async def main(num_phases):
+    print('starting main')
+    phases = [
+            phase(i)
+            for i in range(num_phases)
+    ]
+    print('waiting 0.1 for phases to complete')
+    completed, pending = await asyncio.wait(phases, timeout=0.1)
+    print('{} completed and {} pending'.format(
+        len(completed), len(pending),
+    ))
+    # don't completed tasks stop for no errors
+    if pending:
+        print('canceling tasks')
+        for t in pending:
+            t.cancel()
+    print('exiting main')
+
+event_loop = asyncio.get_event_loop()
+try:
+    event_loop.run_until_complete(main(3))
+finally:
+    event_loop.close()
+
+RESULTS:
+<stdin>:616: DeprecationWarning: There is no current event loop
+starting main
+waiting 0.1 for phases to complete
+<stdin>:605: DeprecationWarning: The explicit passing of coroutine objects to asyncio.wait() is deprecated since Python 3.8, and scheduled for removal in Python 3.11.
+in phase 1
+in phase 2
+in phase 0
+done with phase 0
+1 completed and 2 pending
+canceling tasks
+exiting main
+phase 1 canceled
+phase 2 canceled
+'''
+
+#17 gather() for wait both operations
+
+import asyncio
+
+'''
+async def phase1():
+    print('in phase1')
+    await asyncio.sleep(2)
+    print('done with phase1')
+    return 'phase1 result'
+
+
+async def phase2():
+    print('in phase2')
+    await asyncio.sleep(1)
+    print('done with phase2')
+    return 'phase2 result'
+
+
+async def main():
+    print('starting main')
+    print('waiting for phases to complete')
+    results = await asyncio.gather(
+            phase1(),
+            phase2(),
+    )
+    print('results: {!r}'.format(results))
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    event_loop.run_until_complete(main())
+finally:
+    event_loop.close()
+
+RESULTS:
+<stdin>:667: DeprecationWarning: There is no current event loop
+starting main
+waiting for phases to complete
+in phase1
+in phase2
+done with phase2
+done with phase1
+results: ['phase1 result', 'phase2 result']
+'''
+
+#18 asyncio as_completed
+
+import asyncio
+
+'''
+async def phase(i):
+    print('in phase {}'.format(i))
+    await asyncio.sleep(0.5 - (0.1 * i))
+    print('done with phase {}'.format(i))
+    return 'phase {} result'.format(i)
+
+
+async def main(num_phases):
+    print('starting main')
+    phases = [
+            phase(i)
+            for i in range(num_phases)
+    ]
+    print('waiting for phases to complete')
+    results = []
+    for next_to_complete in asyncio.as_completed(phases):
+        answer = await next_to_complete
+        print('received answer {!r}'.format(answer))
+        results.append(answer)
+    print('results: {!r}'.format(results))
+    return results
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    event_loop.run_until_complete(main(3))
+finally:
+    event_loop.close()
+
+RESULTS:
+<stdin>:712: DeprecationWarning: There is no current event loop
+starting main
+waiting for phases to complete
+in phase 2
+in phase 1
+in phase 0
+done with phase 2
+received answer 'phase 2 result'
+done with phase 1
+received answer 'phase 1 result'
+done with phase 0
+received answer 'phase 0 result'
+results: ['phase 2 result', 'phase 1 result', 'phase 0 result']
+'''
+
+#19 asyncio lock
+
+import asyncio
+import functools
+
+'''
+def unlock(lock):
+    print('callback releasing lock')
+    lock.release()
+
+
+async def coro1(lock):
+    print('coro1 waiting for the lock')
+    with await lock:
+        print('coro1 acquired lock')
+    print('coro1 released lock')
+
+
+async def coro2(lock):
+    print('coro2 waiting for the lock')
+    await lock
+    try:
+        print('coro2 acquired lock')
+    finally:
+        print('coro2 released lock')
+        lock.release()
+
+
+async def main(loop):
+    # Make and get different locking
+    lock = asyncio.Lock()
+    print('acquiring the lock before starting coroutines')
+    await lock.acquire()
+    print('lock acquired: {}'.format(lock.locked()))
+
+    # Make func callback for unlock
+    loop.call_later(0.1, functools.partial(unlock, lock))
+
+    # Start programs what need locking
+    print('waiting for coroutines')
+    await asyncio.wait([coro1(lock), coro2(lock)]),
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    event_loop.run_until_complete(main(event_loop))
+finally:
+    event_loop.close()
+
+RESULTS:
+<stdin>:777: DeprecationWarning: There is no current event loop
+acquiring the lock before starting coroutines
+lock acquired: True
+waiting for coroutines
+<stdin>:774: DeprecationWarning: The explicit passing of coroutine objects to asyncio.wait() is deprecated since Python 3.8, and scheduled for removal in Python 3.11.
+coro2 waiting for the lock
+coro1 waiting for the lock
+Task exception was never retrieved
+future: <Task finished name='Task-2' coro=<coro2() done, defined at <stdin>:752> exception=TypeError("object Lock can't be used in 'await' expression")>
+Traceback (most recent call last):
+  File "<stdin>", line 754, in coro2
+TypeError: object Lock can't be used in 'await' expression
+Task exception was never retrieved
+future: <Task finished name='Task-3' coro=<coro1() done, defined at <stdin>:745> exception=TypeError("object Lock can't be used in 'await' expression")>
+Traceback (most recent call last):
+  File "<stdin>", line 747, in coro1
+TypeError: object Lock can't be used in 'await' expression
+'''
+
+#20 asyncio event
