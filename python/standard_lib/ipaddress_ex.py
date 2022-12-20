af8237a1e353f787392a7128716a53c9dbe33536
maksim-shmat <maksim.shmat@gmail.com>
2022-12-20 05:37:14 +0300

    chore: work with socket getserve

--- python/standard_lib/ipaddress_ex.py
+++ python/standard_lib/ipaddress_ex.py
@@ -174,3 +174,47 @@ is not on a known network
 '''
 
 #6 ipaddress interfaces
+
+import ipaddress
+
+'''
+ADDRESSES = [
+        '10.9.0.6/24',
+        'fdfd:87d5:b475:5e3e:b1bc:e121:a8eb:14aa/64',
+]
+
+for ip in ADDRESSES:
+    iface = ipaddress.ip_interface(ip)
+    print('{!r}'.format(iface))
+    print('network:\n ', iface.network)
+    print('ip:\n ', iface.ip)
+    print('IP with prefixlen:\n ', iface.with_prefixlen)
+    print('netmask:\n ', iface.with_netmask)
+    print('hostmask:\n ', iface.with_hostmask)
+    print()
+
+RESULTS:
+IPv4Interface('10.9.0.6/24')
+network:
+  10.9.0.0/24
+ip:
+  10.9.0.6
+IP with prefixlen:
+  10.9.0.6/24
+netmask:
+  10.9.0.6/255.255.255.0
+hostmask:
+  10.9.0.6/0.0.0.255
+
+IPv6Interface('fdfd:87d5:b475:5e3e:b1bc:e121:a8eb:14aa/64')
+network:
+  fdfd:87d5:b475:5e3e::/64
+ip:
+  fdfd:87d5:b475:5e3e:b1bc:e121:a8eb:14aa
+IP with prefixlen:
+  fdfd:87d5:b475:5e3e:b1bc:e121:a8eb:14aa/64
+netmask:
+  fdfd:87d5:b475:5e3e:b1bc:e121:a8eb:14aa/ffff:ffff:ffff:ffff::
+hostmask:
+  fdfd:87d5:b475:5e3e:b1bc:e121:a8eb:14aa/::ffff:ffff:ffff:ffff
+'''
--- python/standard_lib/socket_ex.py
+++ python/standard_lib/socket_ex.py
@@ -5,7 +5,7 @@
 import sys
 import socketserver
 
-
+'''
 class Echo(socketserver.BaseRequestHandler):
 
     def handle(self):
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     s.close()
     server.socket.close()
 
-'''RESULTS:
+RESULTS:
 <stdin>:50: DeprecationWarning: setDaemon() is deprecated, set the daemon attribute instead
 Sending: 'français'
 Writing: b'fran\xc3\xa7ais'
@@ -83,3 +83,111 @@ Reading: b'fran\xc3\xa7ais'
 Reading: b''
 Received: 'français'
 '''
+
+#2 socket gethostname
+
+import socket
+
+print(socket.gethostname())
+
+#3 change hostname to numbers with gethostbyname()
+
+import socket
+
+'''
+HOSTS = [
+        'apy',
+        'pymotw.com',
+        'www.python.com',
+        'nosuchname',
+]
+
+for host in HOSTS:
+    try:
+        print('{} : {}'.format(host, socket.gethostbyname(host)))
+    except socket.error as msg:
+        print('{} : {}'.format(host, msg))
+'''
+
+#4 socket gethostbynamei_ex()
+
+import socket
+
+'''
+HOSTS = [
+        'apu',
+        'pymotw.com',
+        'www.python.org',
+        'nosuchname',
+]
+
+for host in HOSTS:
+    print(host)
+    try:
+        name, aliases, addresses = socket.gethostbyname_ex(host)
+        print('  Hostname:', name)
+        print('   Aliases:', aliases)
+        print(' Addresses:', addresses)
+    except socket.error as msg:
+        print('ERROR:', msg)
+    print()
+'''
+
+#5 socket getfqdn() for get true host name
+
+import socket
+'''
+for host in ['apu', 'pymotw.com']:
+    print('{:>10} : {}'.format(host, socket.getfqdn(host)))
+'''
+#6 socket gethostbyaddr()
+
+import socket
+'''
+hostname, aliases, addresses = socket.gethostbyaddr('10.9.0.10')
+
+print('Hostname :', hostname)
+print('Aliases  :', aliases)
+print('Addresses:', addresses)
+
+EXPECTED RESULTS:
+Hostname  : apu.hellfy.net
+Aliases   : ['apu']
+Addresses : ['10.9.0.10']
+'''
+
+#7 socket getservbyname
+
+'''
+import socket
+from urllib.parse import urlparse
+
+
+URLS = [
+        'http://www.python.org',
+        'https://www.mybank.com',
+        'ftp://prep.ai.mit.edu',
+        'gopher://gopher.micro.umn.edu',
+        'smtp://mail.example.com',
+        'imap://mail.example.com',
+        'pop3://pop.example.com',
+        'pop3s://pop.example.com',
+]
+
+for url in URLS:
+    parsed_url = urlparse(url)
+    port = socket.getservbyname(parsed_url.scheme)
+    print('{:>6} : {}'.format(parsed_url.scheme, port))
+
+RESULTS:
+ http : 80
+ https : 443
+   ftp : 21
+gopher : 70
+  smtp : 25
+  imap : 143
+  pop3 : 110
+ pop3s : 995
+'''
+
+#8 socket getservebyport
