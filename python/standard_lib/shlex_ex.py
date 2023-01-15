d263084a9b04a0bd031036734d520d47d7c99362
maksim-shmat <maksim.shmat@gmail.com>
2023-01-15 17:03:57 +0300

    chore: work with shlex

--- python/standard_lib/shlex_ex.py
+++ python/standard_lib/shlex_ex.py
@@ -142,4 +142,94 @@ for token in lexer:
     print('{!r}'.format(token))
 '''
 
-#7 
+#7  shlex errors
+
+import shlex
+
+'''
+text = """This line is OK.
+This line has an "unfinished quote.
+This line is OK, too.
+"""
+
+print('ORIGINAL: {!r}'.format(text))
+print()
+
+lexer = shlex.shlex(text)
+print('TOKENS:')
+try:
+    for token in lexer:
+        print('{!r}'.format(token))
+except ValueError as err:
+    first_line_of_error = lexer.token.splitlines()[0]
+    print('ERROR: {} {}'.format(lexer.error_leader(), err))
+    print('following {!r}'.format(first_line_of_error))
+'''
+
+#8 shlex possix
+
+import shlex
+
+'''
+examples = [
+        'Do"Not"Separate',
+        '"Do"Separate',
+        'Escaped \e Character not in quotes',
+        'Escaped "\e" Character in double quotes',
+        "Escaped '\e' Character in single quotes",
+        r"Escaped '\'' \"\'\" single quoter",
+        r'Escaped "\"" \'\"\' double quote',
+        "\"'Strip extra layer of quotes'\"",
+]
+
+for s in examples:
+    print('ORIGINAL: {!r}'.format(s))
+    print('non-POSIX: ', end='')
+
+    non_posix_lexer = shlex.shlex(s, posix=False)
+    try:
+        print('{!r}'.format(list(non_posix_lexer)))
+    except ValueError as err:
+        print('error({})'.format(err))
+
+    print('POSIX    :', end='')
+    posix_lexer = shlex.shlex(s, posix=True)
+    try:
+        print('{!r}'.format(list(posix_lexer)))
+    except ValueError as err:
+        print('error({})'.format(err))
+    print()
+
+RESULTS:
+ORIGINAL: 'Do"Not"Separate'
+non-POSIX: ['Do"Not"Separate']
+POSIX    :['DoNotSeparate']
+
+ORIGINAL: '"Do"Separate'
+non-POSIX: ['"Do"', 'Separate']
+POSIX    :['DoSeparate']
+
+ORIGINAL: 'Escaped \\e Character not in quotes'
+non-POSIX: ['Escaped', '\\', 'e', 'Character', 'not', 'in', 'quotes']
+POSIX    :['Escaped', 'e', 'Character', 'not', 'in', 'quotes']
+
+ORIGINAL: 'Escaped "\\e" Character in double quotes'
+non-POSIX: ['Escaped', '"\\e"', 'Character', 'in', 'double', 'quotes']
+POSIX    :['Escaped', '\\e', 'Character', 'in', 'double', 'quotes']
+
+ORIGINAL: "Escaped '\\e' Character in single quotes"
+non-POSIX: ['Escaped', "'\\e'", 'Character', 'in', 'single', 'quotes']
+POSIX    :['Escaped', '\\e', 'Character', 'in', 'single', 'quotes']
+
+ORIGINAL: 'Escaped \'\\\'\' \\"\\\'\\" single quoter'
+non-POSIX: error(No closing quotation)
+POSIX    :['Escaped', '\\ \\"\\"', 'single', 'quoter']
+
+ORIGINAL: 'Escaped "\\"" \\\'\\"\\\' double quote'
+non-POSIX: error(No closing quotation)
+POSIX    :['Escaped', '"', '\'"\'', 'double', 'quote']
+
+ORIGINAL: '"\'Strip extra layer of quotes\'"'
+non-POSIX: ['"\'Strip extra layer of quotes\'"']
+POSIX    :["'Strip extra layer of quotes'"]
+'''
