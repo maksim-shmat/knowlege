58a5747cd3e10b0c2c2a40bc88971d190b416a46
maksim-shmat <maksim.shmat@gmail.com>
2023-02-02 05:31:25 +0300

    chore: work with os wait()

--- python/os_ex.py
+++ python/os_ex.py
@@ -167,4 +167,132 @@ Removed value: None
 Child process:
 '''
 
-#7 
+#7 os cwd example
+
+import os
+
+'''
+print('Starting:', os.getcwd())
+
+print('Moving up one:', os.pardir)
+os.chdir(os.pardir)
+
+print('After move:', os.getcwd())
+
+RESULTS:
+Starting: /home/jack/django2/knowlege/python
+Moving up one: ..
+After move: /home/jack/django2/knowlege
+'''
+
+#8 simple run command, os system example
+
+import os
+
+'''
+# simple cmd
+os.system('pwd')
+
+RESULTS:
+/home/jack/django2/knowlege/python
+'''
+
+#9 os system background
+
+import os
+import time
+
+'''
+print('Calling...')
+time.sleep(5)
+
+print('Sleeping...')
+time.sleep(5)
+'''
+
+#10 os fork example
+
+import os
+
+'''
+pid = os.fork()
+
+if pid:
+    print('Child process id:', pid)
+else:
+    print('I am the child')
+
+RESULTS:
+Child process id: 1329530
+I am the child
+'''
+
+#11 os kill example
+
+import os
+import signal
+import time
+
+'''
+def signal_usr1(sinnum, frame):
+    "Callback function for signal."""
+    pid = os.getpid()
+    print('Received USR1 in process {}'.format(pid))
+
+print('Forking...')
+child_pid = os.fork()
+if child_pid:
+    print('PARENT: Pausing before sending signal...')
+    time.sleep(1)
+    print('PARENT: Signaling {}'.format(child_pid))
+    os.kill(child_pid, signal.SIGUSR1)
+else:
+    print('CHILD: Setting up signal handler')
+    signal.signal(signal.SIGUSR1, signal_usr1)
+    print('CHILD: Pausing to wait for signal')
+    time.sleep(5)
+
+RESULTS:
+Forking...
+PARENT: Pausing before sending signal...
+CHILD: Setting up signal handler
+CHILD: Pausing to wait for signal
+PARENT: Signaling 1329955
+Received USR1 in process 1329955
+'''
+
+#12 os wait() for wait how any daughters process is stopped
+
+import os
+import sys
+import time
+
+'''
+for i in range(2):
+    print('PARENT {}: Forking {}'.format(os.getpid(), i))
+    worker_pid = os.fork()
+    if not worker_pid:
+        print('WORKER {}: Starting'.format(i))
+        time.sleep(2 + i)
+        print('WORKER {}: Finishing'.format(i))
+        sys.exit(i)
+
+for i in range(2):
+    print('PARENT: Waiting for {}'.format(i))
+    done = os.wait()
+    print('PARENT: Child done:', done)
+
+RESULTS:
+PARENT 1330729: Forking 0
+PARENT 1330729: Forking 1
+WORKER 0: Starting
+PARENT: Waiting for 0
+WORKER 1: Starting
+WORKER 0: Finishing
+PARENT: Child done: (1330730, 0)
+PARENT: Waiting for 1
+WORKER 1: Finishing
+PARENT: Child done: (1330731, 256)
+'''
+
+#13 os waitpid example
