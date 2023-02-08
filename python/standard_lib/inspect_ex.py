1abefca4d4821cb2262c72148160e4cbbd5e409a
maksim-shmat <maksim.shmat@gmail.com>
2023-02-08 05:09:52 +0300

    chore: work with inspect getmro

--- python/standard_lib/inspect_ex.py
+++ python/standard_lib/inspect_ex.py
@@ -167,3 +167,296 @@ RESULTS:
 '''
 
 #11 inspect signature function
+
+import inspect
+import example
+
+'''
+sig = inspect.signature(example.module_level_function)
+print('module_level_function{}'.format(sig))
+
+print('\nParameter details:')
+for name, param in sig.parameters.items():
+    if param.kind == inspect.Parameter.POSITIONAL_ONLY:
+        print('  {} (positionsl-only)'.format(name))
+    elif param.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD:
+        if param.default != inspect.Parameter.empty:
+            print(' {}={!r}'.format(name, param.default))
+        else:
+            print(' {}'.format(name))
+    elif param.kind == inspect.Parameter.KEYWORD_ONLY:
+        if param.default != inspect.Parameter.empty:
+            print('  {}={!r} (keyword-only)'.format(
+                name, param.default))
+        else:
+            print('  {} (keyword-only)'.format(name))
+    elif param.kind == inspect.Parameter.VAR_KEYWORD:
+        print(' **{}'.format(name))
+
+RESULTS:
+module_level_function(arg1, arg2='default', *args, **kwargs)
+
+Parameter details:
+ arg1
+ arg2='default'
+ **kwargs
+'''
+
+#12 inspect signature bind
+
+import inspect
+import example
+
+'''
+sig = inspect.signature(example.module_level_function)
+
+bound = sig.bind(
+        'this is arg1',
+        'this is arg2',
+        'this is an extra positional argument',
+        extra_named_arg='value',
+)
+
+print('Arguments:')
+for name, value in bound.arguments.items():
+    print('{} = {!r}'.format(name, value))
+
+print('\nCalling:')
+print(example.module_level_function(*bound.args, **bound.kwargs))
+
+RESULTS:
+Arguments:
+arg1 = 'this is arg1'
+arg2 = 'this is arg2'
+args = ('this is an extra positional argument',)
+kwargs = {'extra_named_arg': 'value'}
+
+Calling:
+this is arg1this is arg1
+'''
+
+#13 inspect signature bind partial
+
+import inspect
+import example
+
+'''
+sig = inspect.signature(example.module_level_function)
+
+partial = sig.bind_partial(
+        'this is arg1',
+)
+
+print('Without defaults:')
+for name, value in partial.arguments.items():
+    print('{} = {!r}'.format(name, value))
+
+print('\nWith defaults:')
+partial.apply_defaults()
+for name, value in partial.arguments.items():
+    print('{} = {!r}'.format(name, value))
+
+RESULTS:
+Without defaults:
+arg1 = 'this is arg1'
+
+With defaults:
+arg1 = 'this is arg1'
+arg2 = 'default'
+args = ()
+kwargs = {}
+'''
+
+#14 inspect getclasstree
+
+import inspect
+import example
+
+'''
+class C(example.B):
+    pass
+
+
+class D(C, example.A):
+    pass
+
+
+def print_class_tree(tree, indent=-1):
+    if isinstance(tree, list):
+        for node in tree:
+            print_class_tree(node, indent + 1)
+    else:
+        print(' ' * indent, tree[0].__name__)
+    return
+
+if __name__ == '__main__':
+    print('A, B, C, D:')
+    print_class_tree(inspect.getclasstree(
+        [example.A, example.B, C, D])
+    )
+
+RESULTS:
+A, B, C, D:
+ object
+  A
+   D
+   B
+    C
+     D
+'''
+
+#15 inspect getclasstree unique
+
+import inspect
+import example
+
+'''
+class C(example.B):
+    pass
+
+
+class D(C, example.A):
+    pass
+
+
+def print_class_tree(tree, indent=-1):
+    if isinstance(tree, list):
+        for node in tree:
+            print_class_tree(node, indent + 1)
+    else:
+        print('  ' * indent, tree[0].__name__)
+    return
+
+if __name__ == '__main__':
+    print('A, B, C, D:')
+    print_class_tree(inspect.getclasstree(
+        [example.A, example.B, C, D],
+        unique = True,
+))
+
+RESULTS:
+A, B, C, D:
+ object
+   A
+     B
+       C
+         D
+'''
+
+#16 inspect getmro
+
+import inspect
+import example
+
+'''
+class C(object):
+    pass
+
+
+class C_First(C, example.B):
+    pass
+
+
+class B_First(example.B, C):
+    pass
+
+
+print('B_First:')
+for c in inspect.getmro(B_First):
+    print(' {}'.format(c.__name__))
+print()
+print('C_First:')
+for c in inspect.getmro(C_First):
+    print('  {}'.format(c.__name__))
+
+RESULTS:
+B_First:
+ B_First
+ B
+ A
+ C
+ object
+
+C_First:
+  C_First
+  C
+  B
+  A
+  object
+'''
+
+#17 inspect currentframe
+
+import inspect
+import pprint
+
+'''
+def recurse(limit, keyword='default', *, kwonly='must be named'):
+    local_variable = '.' * limit
+    keyword = 'changed value of argument'
+    frame = inspect.currentframe()
+    print('line {} of {}'.format(frame.f_lineno,
+                                 frame.f_code.co_filename))
+    print('locals:')
+    pprint.pprint(frame.f_locals)
+    print()
+    if limit <= 0:
+        return
+    recurse(limit - 1)
+    return local_variable
+
+if __name__ == '__main__':
+    recurse(2)
+
+RESULTS:
+line 398 of <stdin>
+locals:
+{'frame': <frame at 0x7f5146f41090, file '<stdin>', line 401, code recurse>,
+ 'keyword': 'changed value of argument',
+ 'kwonly': 'must be named',
+ 'limit': 2,
+ 'local_variable': '..'}
+
+line 398 of <stdin>
+locals:
+{'frame': <frame at 0x563bd0f233a0, file '<stdin>', line 401, code recurse>,
+ 'keyword': 'changed value of argument',
+ 'kwonly': 'must be named',
+ 'limit': 1,
+ 'local_variable': '.'}
+
+line 398 of <stdin>
+locals:
+{'frame': <frame at 0x563bd0f40fe0, file '<stdin>', line 401, code recurse>,
+ 'keyword': 'changed value of argument',
+ 'kwonly': 'must be named',
+ 'limit': 0,
+ 'local_variable': ''}
+'''
+
+#18 inspect stack
+
+import inspect
+import pprint
+
+
+def show_stack():
+    for level in inspect.stack():
+        print('{}[{}]\n -> {}'.format(
+            level.frame.f_code.co_filename,
+            level.lineno,
+            level.code_context[level.index].strip(),
+        ))
+        pprint.pprint(level.frame.f_locals)
+        pritn()
+
+def recurse(limit):
+    local_variable = '.' * limit
+    if limit <= 0:
+        show_stack()
+        return
+    recurse(limit - 1)
+    return local_variable
+
+if __name__ == '__main__':
+    recurse(2)
