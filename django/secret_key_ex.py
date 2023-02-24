a52a8b8c8d12d97fe22ef5792dbc1db47e06a6a8
maksim-shmat <maksim.shmat@gmail.com>
2023-02-24 04:57:51 +0300

    chore: work with git

--- django/secret_key_ex.py
+++ django/secret_key_ex.py
@@ -5,8 +5,22 @@
 import os
 SECRET_KEY = os.environ['SECRET_KEY']
 
+#1.1
+
+import os
+SECTET_KEY = os.environ.get('DJANGO_SECRET_KEY',
+        'cg#p$g+j9tax!#a3cup@1$8abt2__^k3q_pmi)5%asj6yupkag')
 
 #2 from file
 
 with open('/etc/secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
+
+#3 Warn!
+
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DJANGO_DEBUG = ''  # it False
+
+#4 pythohn manage.py check -deploy
+
+check deploy settings
--- python/standard_lib/zipimport_get_data_nozip.py
+++ python/standard_lib/zipimport_get_data_nozip.py
@@ -1,11 +1,28 @@
 """Find dir and make data file."""
 
+#1 import from filesystem
+
 import os
 import example_package
 
-
+'''
 pkg_dir = os.path.dirname(example_package.__file__)
 data_filename = os.path.join(pkg_dir, 'RADME.txt')
 
 print(data_filename, ':')
 print(open(data_filename, 'r').read())
+'''
+
+#2 import from ZIP archive
+
+import sys
+'''
+sys.path.insert(0, 'zipimport_example.zip')
+
+import os
+import example_package
+print(example_package.__file__)
+data = example_package.__loader__.get_data(
+        'example_package/README.txt')
+print(data.decode('utf-8'))
+'''
