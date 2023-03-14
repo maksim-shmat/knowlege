64036d71f4e8dd03d5e8ef8d5a2e4f29933d8f2f
maksim-shmat <maksim.shmat@gmail.com>
2023-03-14 05:26:44 +0300

    chore: work with math examples.

---
+++ math/math_ex.py
@@ -0,0 +1,15 @@
+"""Mathematics about."""
+
+#1 The hammer for sum
+
+(f + g)` = f` + g`.
+
+#2 The hammer for multiplication
+
+(fg)` = f`g + fg`.
+
+#3 The hammer for 
+
+df    ds  df
+--  = --  --.
+dx    dx  ds
