6e1cafa28df6d1f20eee55e470d56ce88a57277c
maksim-shmat <maksim.shmat@gmail.com>
2023-05-10 05:20:57 +0300

    chore: work with Django admin class, set decorator

--- django/12admin.py
+++ django/12admin.py
@@ -9,4 +9,29 @@ class ContactAdmin(admin.ModelAdmin):
     pass
 admin.site.register(models.Contact, ContactAdmin)
 
-######
+#1 Registering models using a statement
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from ..chapter_3.models import Seller
+
+
+class SellerAdmin(UserAdmin):
+    pass
+admin.site.register(Seller, SellerAdmin)
+
+#1.1 Registering models using a decorator
+
+from django.contrib import admin
+from ..chapter_3.modles import (
+        Engine,
+        Seller,
+        Vehicle,
+        VehicleModel
+)
+
+@admin.register(Seller)
+class SellerAdmin(UserAdmin):
+    pass
+
+#2
---
+++ django/loaddata_ex.py
@@ -0,0 +1,4 @@
+"""Django loaddata about."""
+
+#1
+(virtual_env) python manage.py loaddata chapter_3
