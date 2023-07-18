2674b9b3364f2e09815e4644f6fa91d0532a3b33
maksim-shmat <maksim.shmat@gmail.com>
2023-07-18 05:04:39 +0300

    chore: work with inheritance

--- python/inheritance.py
+++ python/inheritance.py
@@ -60,3 +60,33 @@ for car in cars:
 #Starting engine V8Engine for car RaceCar... Wroom, wroom!
 #Starting engine ElectricEngine for car CityCar... Wroom, wroom!
 #Starting engine V8Engine for car F1Car... Wroom, wroom!
+
+#2 Check it in the diff file for those code
+
+from inheritance import Car, RaceCar, F1Car
+
+
+
+car = Car()
+racecar = RaceCar()
+f1car = F1Car()
+cars = [(car, 'car'), (racecar, 'racecar'), (f1car, 'f1car')]
+car_classes = [Car, RaceCar, F1Car]
+
+for car, car_name in cars:
+    for class_ in car_classes:
+        belongs = isinstance(car, class_)
+        msg = 'is a' if belongs else 'is not a'
+        print(car_name, msg, class_.__name__)
+
+""" Prints:
+    car is a Car
+    car is not a RaceCar
+    car is not a F1Car
+    racecar is a Car
+    racecar is a RaceCar
+    racecar is not F1Car
+    f1car is a Car
+    f1car is a RaceCar
+    f1car is a F1Car
+    """
