2baef6d7dcb31eab641037532e686783126f06e1
maksim-shmat <maksim.shmat@gmail.com>
2023-09-26 04:57:30 +0300

    chore: work with Django models

--- django/15models_example.py
+++ django/15models_example.py
@@ -293,4 +293,56 @@ class PersonAdmin(admin.ModelAdmin):
             ("Location", {"fields": ("city", "state")})
     ]
 
-#2
+#2 models.py other
+
+class DroneCategory(models.Model):
+    name = models.Charfield(max_length=250, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Drone(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    drone_category = models.ForeignKey(
+            DroneCategory,
+            related_name='drones',
+            on_delete=models.CASCADE)
+    manufacturing_date = models.DateTimeField()
+    has_it_completed = models.BooleanField(default=False)
+    inserted_timestamp = models.DateTimeField(auto_now_add_True)
+    
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Pilot(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICES = (
+            (MALE, 'Male'),
+            (FEMALE, 'Female'),
+    )
+    name = models.CharField(max_length=150, blank=False, unique=True)
+    gender = models.CharField(
+            max_length=2
+            choices=GENDER_CHOICES,
+            default=MALE,
+    )
+    races_count = models.IntegerField()
+    inserted_timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+#3
