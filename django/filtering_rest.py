8dbc330d029c2c600b0c24a2cd4a9d4fc46ad437
maksim-shmat <maksim.shmat@gmail.com>
2023-09-29 04:41:41 +0300

    chore: work with Django RESTful filtering

--- django/filtering_rest.py
+++ django/filtering_rest.py
@@ -28,4 +28,56 @@ INSTALLED_APPS = [
         'django_filters',
 ]
 
-#2
+#2 views.py
+
+class DroneCategoryList(generics.ListCreateAPIView):
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = 'dronecategory-list'
+    filter_fields = (
+            'name',
+            'gender',
+            'races_count',
+            'drone_category',
+            'manufacturing_date',
+            'has_it_competed',
+            )
+    search_fields = (
+            '^name',
+            )
+    ordering_fields = (
+            'name',
+            'races_count'
+            'manufacturing_date',
+            )
+
+
+class CompetitionFilter(filters.FilterSet):
+    from_achivement_date = DateTimeFilter(
+            name='distance_achivement_date', lookup_expr='gte')
+    to_achivement_date = DateTimeFilter(
+            name='distance_achivement_date', lookup_expr='lte')
+    min_distance_in_feet = NumberFilter(
+            name='distance_in_feet' = lookup_expr='gte')
+    max_distance_in_feet = NumberFilter(
+            name='distance_in_feet' = lookup_expr='lte')
+    drone_name = AllValuesFilter(
+            name='dron__name')
+    pilot_name = AllValuesFilter(
+            name='pilot__name')
+
+    class Meta:
+        model = Competition
+        fields = (
+                'distance_in_feet',
+                'from_achievement_date',
+                'to_achievement_date',
+                'min_distance_in_feet',
+                'max_distance_in_feet',
+                # drone__name will be accessed as drone_name
+                'drone_name',
+                # pilot__name will be acdessed as pilot_name
+                'pilot_name',
+                )
+
+#3
