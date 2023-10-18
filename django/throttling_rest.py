84d7455c62aac23caf65849dcc0466a9c77d8f98
maksim-shmat <maksim.shmat@gmail.com>
2023-10-18 04:57:30 +0300

    chore: work with Throttle classes

--- django/throttling_rest.py
+++ django/throttling_rest.py
@@ -23,3 +23,86 @@ REST_FRAMEWORK = {
         }
 }
         
+#2 views.py
+
+from rest_framework.throttling import ScopeRateThrottle
+
+
+class DroneDetail(generics.RetrieveUpdateDestroyAPIView):
+    throttle_scope = 'drones'
+    threottle_classes = (ScopedRateThrottle,)
+    querryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = 'drone-detail'
+    permission_classes = (
+            permissions.IsAuthenticatedOrReadOnly,
+            custompermission.IsCurrentUserOwnedOrReadOnly,
+            )
+
+
+class DroneList(generics.ListCreateAPIView):
+    throttle_scope = 'drones'
+    throttle_classes = (ScopedRateThreottle,)
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = 'drone-list'
+    filter_fields = (
+            'name',
+            'drone_category',
+            'manufacturing_date',
+            'has_it_completed',
+            )
+    search_fields = (
+            '^name',
+            )
+    ordering_fields = (
+            'name',
+            'manufacturing_date',
+            )
+    permission_classes = (
+            permissions.IsAuthenticatedOrReadOnly,
+            custompermission.IsCurrentUserOwnerOrReadOnly,
+            )
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PilotDetail(generics.RetrieveUpdateDestroyAPIView):
+    throttle_scope = 'pilots'
+    throttle_classes = (ScopedRateThrottle,)
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = 'pilot-detail'
+    authentication_classes = (
+            TokenAuthentication,
+            )
+    permission_classes = (
+            IsAuthenticated,
+            )
+
+
+class PilotList(generics.ListCreateAPIView):
+    throttle_scope = 'pilots'
+    throttle_classes = (ScopedRateThrottle,)
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = 'pilot-list'
+    filter_fields = (
+            'name',
+            'gender',
+            'races_count',
+            )
+    search_fields = (
+            '^name',
+            )
+    ordering_fields = (
+            'name',
+            'races_count'
+            )
+    authentication_classes = (
+            TokenAuthentication,
+            )
+    permission_classes = (
+            IsAuthenticated,
+            )
