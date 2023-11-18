1bb017136c40651564f9719c08839b6e513651ee
maksim-shmat <maksim.shmat@gmail.com>
2023-11-18 17:09:21 +0300

    chore: work with Bootstrap navigation bar

--- django/bootstrap_ex.py
+++ django/bootstrap_ex.py
@@ -1,4 +1,7 @@
-"""Template styling with Bootstrap."""
+"""Template styling with Bootstrap.
+
+https://getbootstrap.com/docs/4.4/getting-started/introduction/.
+"""
 
 
 #1
@@ -18,8 +21,37 @@
     uhOf23Q9Ifjh" crossorigin="anonymous">
   </head>
     <body>
-      Content goes here
+      <h1>Welcome to my Site</h1>
+      <button type="button" class="btn btn-primary">
+      Checkout my Blog!</button>
     </body>
 </html>
 
-#2
+#2 adding template inheritance and a Bootstrap navigation bar
+
+# base.html
+<!doctype html>
+{% load static %}
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,
+  initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+...
+# book_list.html
+{% extends 'base.html' %}
+
+{% block content %}
+<ul class="list-group">
+  {% for item in book_list %}
+  <li class="list-group-item">
+    <span class="text-info">Title: </span> <span>{{
+        item.book.title }}</span>
+    <br>
+    <span class="text-info">Publisher:
+        </span><span>{{
+            ierm.book.publisher }}</span>
+...
+#
