649f36b933ac3b7fd32a91ac6837bc3dec5f51f3
maksim-shmat <maksim.shmat@gmail.com>
2024-01-30 18:15:00 +0300

    chore: work with advanced html tags

--- git/git_ssh.py
+++ git/git_ssh.py
@@ -1,10 +1,13 @@
 """ Connect terminal to github by ssh."""
 
+
 #1 Configure git to use ssh
 
 git config --global gpg.format ssh
 
-added a string is "format = ssh", maybe ssh is a sort of gpg
+It added a string is "format = ssh", maybe ssh is a sort of gpg
+
+git config --global user.signingkey /PATH/TO/.SSH/KEY.PUB
 
 #2 Generate pair keys
 
@@ -18,6 +21,26 @@ $ eval "$(ssh-agent -s)"
 
 ~/.ssh/ id_ed25519 copy how into
 
-#5 Testing your ssh connection
+#5 change user.signingkey in .gitconfig
+
+$ git config --global user.signingkey <gpg-key-id>
+
+#5.1 Or maybe create ~/.ssh/config file
+
+~/.ssh/config
+chmod 600 ~/.ssh/config
+
+Host *  # for all hosts
+    IgnoreUnknown UseKeychain  # ???
+    UseKeychain yes  # ???
+
+#6 Testing your ssh connection
 
 $ ssh -T git@github.com
+
+# Docs
+man ssh_config
+
+#7 If you have change passfrase to no
+
+ssh-keygen -p -f ~/.ssh/id_ed25519
---
+++ html/html_tags.py
@@ -0,0 +1,113 @@
+"""HTML tags about."""
+
+#1 Breack line
+Hello<br>
+World<br>
+!
+
+#2 Horizontal line between the paragraphs
+Hello<hr>  # named "horizontal rule"
+---------
+World!
+
+#3 Navigate on the page
+...
+</header>
+<nav>
+  <a href="/">Home</a>
+  <a href="semantics.html">Semantics</a>
+  <a href="contact.html">Contact</a>
+  <a href="about.html">About</a>
+</nav>
+
+#4 Division
+<div>
+  Requisite social media links:
+</div>
+<div>
+  <a href="https://twikker.com/">Twikker</a>
+  <a href="https://inst.com/">Inst</a>
+</div>
+
+#5 Inline element with <span>
+<p>
+Notice how an <span style="font-variant: small-caps">
+inline element</span> flows right along with the
+rest of the text.
+</p>
+
+#6 Comments
+<!--
+    something-something
+-->
+
+#7 Emphasizing text with italic
+
+You`ll <em>never</em> fit in there with that rdiculous thing on your head!
+or <i> never </i>
+
+#8 Bold text
+
+<strong> Hello </strong> World!
+or <b> Hello </b>
+
+#9 Quoted strings and Quoted blocks, and Cite
+
+Hello <q>friends</q>
+
+<blockquote>
+  Blah
+  blah
+  blah
+</blockquote>
+
+Blah-blah
+  <q cite="http://html.spec.whathever.org/#the-q-element">blah-blah
+  blah.</q>
+
+#10 Links
+
+<a href="https://webdev.mcfedries.com/wb"> Link text</a>  # remote web page
+
+<a href="rutabagas.html">!!!</a>  # local web page
+
+<a href="/wordplay/puns.html"> click this!</a>  # local web page in a different directory
+
+#11 Anchors
+
+<element id="name">
+
+<section id="section1">
+
+<a href="#name">
+
+<a href="#section1">
+
+<a href="chapter57.html#section1">  # anchors to the same web page
+
+#12 Abbreviation
+
+Why, their life becomes a 
+<abbr title="single income, two childern, oppressive mortgage">SITCOM</abbr>, of course.
+
+if you move cursor ont SITCOM, you have info about
+
+#13 Address with italic
+
+<p>
+  Having a problem? Please "contact us" here:
+</p>
+<address>
+  The Complaints Department<br>
+  0 Black Hole Blvd, Suite -23<br>
+  Nowheresville, BC N0N 0N0
+</address>
+
+#14 Cite as a link
+
+<p>
+  Source: <cite><a href="http://html.spec.whatwg.org/#the-cite-element">The cite
+  element</a></cite>, published by CHuchong.
+</p>
+
+#15
