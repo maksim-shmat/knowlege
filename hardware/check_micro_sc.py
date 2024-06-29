a172e23ac0df7d662927551b27acb6b693bca162
maksim-shmat <maksim.shmat@gmail.com>
2024-06-29 12:10:07 +0300

    chore: work with wifi adapter for secure work.

--- hardware/check_micro_sc.py
+++ hardware/check_micro_sc.py
@@ -4,7 +4,12 @@
 
 #1 Format flash to FAT32
 
-mkfs.vfat /dev/sda
+mkfs.vfat /dev/sda  # mkfs.ext4 persistence -L /dev/sda (check it)
+
+# mkfs variants
+fs(5), badblocks(8), fsck(8), mkdosfs(8), mke2fs(8), mkfs.bfs(8),
+       mkfs.ext2(8), mkfs.ext3(8), mkfs.ext4(8), mkfs.minix(8),
+       mkfs.msdos(8), mkfs.vfat(8), mkfs.xfs(8)
 
 #1.1 Init with copy to
 
---
+++ hardware/how_install_drivers.py
@@ -0,0 +1,17 @@
+"""How install drivers for Wi-Fi adapters."""
+
+#0 for kernel 5.15 apt update/upgrade
+
+#1 Make dir /src/
+
+#2 git clone https://github.com/aircrack-ng/rtl8188us.git
+
+cd /rtl818us
+
+#3 sudo make
+
+#4 sudo make install
+
+#5 lsusb
+
+#6 iwconfig
